6aa59b0759f4b03f6f7b059e99e1e516405a4362
tfojo1 <anthony.fojo@jhmi.edu>
2023-06-10 00:23:29 -0400

    code to install from github all required packages

---
+++ initial_installation.R
@@ -0,0 +1,8 @@
+
+# Need to have devtools package installed
+
+install_github("tfojo1/distributions")
+install_github("tfojo1/bayesian_simulations")
+install_github("tfojo1/locations")
+
+install_github("tfojo1/jheem2")
