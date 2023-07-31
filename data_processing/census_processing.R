f07a8b93d6f8f120150941c143a4685f43a422a3
Zoe Thompson <136368115+zoethompson213@users.noreply.github.com>
2023-07-31 16:40:41 -0600

    age changes

--- data_processing/census_processing.R
+++ data_processing/census_processing.R
@@ -42,7 +42,8 @@ census.manager$register.ontology(
   ont = ontology(
     year= NULL,
     location= NULL,
-    age=c('13-24 years', '25-34 years', '35-44 years', '45-54 years','55+ years'),
+    age=c('0-4 years', '5-9 years', '10-14 years', '15-19 years', '20-24 years', '25-29 years', '30-34 years', '35-39 years', '40-44 years',
+           '45-49 years', '50-54 years', '55-59 years', '60-64 years','65-69 years', '70-74 years', '75-79 years', '80-84 years', '85 years and over'),
     race=c('American Indian/Alaska Native', 'Asian', 'Black/African American', 'Hispanic/Latino', 'Multiracial', 'Native Hawaiian/Other Pacific Islander', 'White'),
     sex=c('male','female')
   ))
