ba26e9c276e0d524981e4675bc3a448307fec54c
Zoe Thompson <136368115+zoethompson213@users.noreply.github.com>
2023-08-28 15:28:43 -0600

    Fixing NAs introduced by coercion issue

--- data_processing/msa_reports_processing.R
+++ data_processing/msa_reports_processing.R
@@ -255,90 +255,108 @@ data.list.msa_sex_age.clean = lapply(data.list.msa_sex_age, function(file){
   
   data=file[["data"]] 
   filename = file[["filename"]] 
-
+  
   data$division= ifelse(grepl("Division", data$MSA), "1", "0") #Remove MSA = division
   data= subset(data, data$division != "1")
   
   data$location = as.character(locations::get.location.code(data$MSA,"CBSA"))
   
   #Create Year#
-if(grepl("2009", filename)) {
-  data$year = as.character("2009")
-}
-if(grepl("2010", filename)) {
-  data$year = as.character("2010")
-}
+  if(grepl("2009", filename)) {
+    data$year = as.character("2009")
+  }
+  if(grepl("2010", filename)) {
+    data$year = as.character("2010")
+  }
   if(grepl("2011", filename)) {
     data$year = as.character("2011")
   }
-if(grepl("2012", filename)) {
-  data$year = as.character("2012")
-}
-if(grepl("2013", filename)) {
-  data$year = as.character("2013")
-}
-if(grepl("2014", filename)) {
-  data$year = as.character("2014")
-}
-if(grepl("2015", filename)) {
-  data$year = as.character("2015")
-}
-if(grepl("2016", filename)) {
-  data$year = as.character("2016")
-}
+  if(grepl("2012", filename)) {
+    data$year = as.character("2012")
+  }
+  if(grepl("2013", filename)) {
+    data$year = as.character("2013")
+  }
+  if(grepl("2014", filename)) {
+    data$year = as.character("2014")
+  }
+  if(grepl("2015", filename)) {
+    data$year = as.character("2015")
+  }
+  if(grepl("2016", filename)) {
+    data$year = as.character("2016")
+  }
   if(grepl("2017", filename)) {
     data$year = as.character("2017")
   }
-if(grepl("2018", filename)) {
-  data$year = as.character("2018")
-}
-  
+  if(grepl("2018", filename)) {
+    data$year = as.character("2018")
+  }
   #Create Sex# 
-if(grepl("male", filename)){
+  if(grepl("male", filename)){
     data$sex="male"
   }
-if(grepl("female", filename)){
+  if(grepl("female", filename)){
     data$sex="female"
-}
+  }
   if(grepl("new", filename)){
     
-    data$diagnoses_num_1 = (gsub("[[:punct:]]", NA, data$diagnoses_num_1))
-    data$diagnoses_1 = as.numeric(data$diagnoses_num_1)
+    data$diagnoses_num_1 = (gsub(",", "", data$diagnoses_num_1)) #replace comma with nothing
+    data$diagnoses_num_1 = (gsub(" ", "", data$diagnoses_num_1)) #replace space with nothing
+    data$diagnoses_num_1 = (gsub("[[:punct:]]", NA, data$diagnoses_num_1)) #if there's a dash replace with NA
+    data$diagnoses_1 = as.numeric(data$diagnoses_num_1) #make it a number
     
-    data$diagnoses_num_2 = (gsub("[[:punct:]]", NA, data$diagnoses_num_2))
+    data$diagnoses_num_2 = (gsub(",", "", data$diagnoses_num_2)) 
+    data$diagnoses_num_2 = (gsub(" ", "", data$diagnoses_num_2)) 
+    data$diagnoses_num_2 = (gsub("[[:punct:]]", NA, data$diagnoses_num_2)) 
     data$diagnoses_2 = as.numeric(data$diagnoses_num_2)
     
-    data$diagnoses_num_3 = (gsub("[[:punct:]]", NA, data$diagnoses_num_3))
-    #data$diagnoses_num_3 = (gsub("[^[:alnum:] ]", NA, data$diagnoses_num_3))
+    data$diagnoses_num_3 = (gsub(",", "", data$diagnoses_num_3)) 
+    data$diagnoses_num_3 = (gsub(" ", "", data$diagnoses_num_3)) 
+    data$diagnoses_num_3 = (gsub("[[:punct:]]", NA, data$diagnoses_num_3)) 
     data$diagnoses_3 = as.numeric(data$diagnoses_num_3)
     
-    data$diagnoses_num_4 = (gsub("[[:punct:]]", NA, data$diagnoses_num_4))
+    data$diagnoses_num_4 = (gsub(",", "", data$diagnoses_num_4)) 
+    data$diagnoses_num_4 = (gsub(" ", "", data$diagnoses_num_4)) 
+    data$diagnoses_num_4 = (gsub("[[:punct:]]", NA, data$diagnoses_num_4)) 
     data$diagnoses_4 = as.numeric(data$diagnoses_num_4)
     
-    data$diagnoses_num_5 = (gsub("[[:punct:]]", NA, data$diagnoses_num_5))
+    data$diagnoses_num_5 = (gsub(",", "", data$diagnoses_num_5)) 
+    data$diagnoses_num_5 = (gsub(" ", "", data$diagnoses_num_5)) 
+    data$diagnoses_num_5 = (gsub("[[:punct:]]", NA, data$diagnoses_num_5)) 
     data$diagnoses_5 = as.numeric(data$diagnoses_num_5)
   }
   
   if(grepl("prevalence", filename)){
-    data$prevalence_num_1 = (gsub("[[:punct:]]", NA, data$prevalence_num_1))
+    data$prevalence_num_1 = (gsub(",", "", data$prevalence_num_1)) #replace comma with nothing
+    data$prevalence_num_1 = (gsub(" ", "", data$prevalence_num_1)) #replace space with nothing
+    data$prevalence_num_1 = (gsub("[[:punct:]]", NA, data$prevalence_num_1)) #if there's a dash replace with NA
     data$prevalence_1 = as.numeric(data$prevalence_num_1)
     
-    data$prevalence_num_2 = (gsub("[[:punct:]]", NA, data$prevalence_num_2))
+    data$prevalence_num_2 = (gsub(",", "", data$prevalence_num_2)) 
+    data$prevalence_num_2 = (gsub(" ", "", data$prevalence_num_2)) 
+    data$prevalence_num_2 = (gsub("[[:punct:]]", NA, data$prevalence_num_2)) 
     data$prevalence_2 = as.numeric(data$prevalence_num_2)
     
-    data$prevalence_num_3 = (gsub("[[:punct:]]", NA, data$prevalence_num_3))
+    data$prevalence_num_3 = (gsub(",", "", data$prevalence_num_3)) 
+    data$prevalence_num_3 = (gsub(" ", "", data$prevalence_num_3)) 
+    data$prevalence_num_3 = (gsub("[[:punct:]]", NA, data$prevalence_num_3)) 
     data$prevalence_3 = as.numeric(data$prevalence_num_3)
     
-    data$prevalence_num_4 = (gsub("[[:punct:]]", NA, data$prevalence_num_4))
+    data$prevalence_num_4 = (gsub(",", "", data$prevalence_num_4)) 
+    data$prevalence_num_4 = (gsub(" ", "", data$prevalence_num_4)) 
+    data$prevalence_num_4 = (gsub("[[:punct:]]", NA, data$prevalence_num_4)) 
     data$prevalence_4 = as.numeric(data$prevalence_num_4)
     
-    data$prevalence_num_5 = (gsub("[[:punct:]]", NA, data$prevalence_num_5))
+    data$prevalence_num_5 = (gsub(",", "", data$prevalence_num_5)) 
+    data$prevalence_num_5 = (gsub(" ", "", data$prevalence_num_5)) 
+    data$prevalence_num_5 = (gsub("[[:punct:]]", NA, data$prevalence_num_5)) 
     data$prevalence_5 = as.numeric(data$prevalence_num_5)
   }
   
-   data <- data %>%
-     select(year, location, sex, one_of("diagnoses_1", "diagnoses_2", "diagnoses_3", "diagnoses_4", "diagnoses_5",
-                                        "prevalence_1", "prevalence_2", "prevalence_3", "prevalence_4", "prevalence_5"))
+  data <- data %>%
+    select(year, location, sex, one_of("diagnoses_1", "diagnoses_2", "diagnoses_3", "diagnoses_4", "diagnoses_5",
+                                       "prevalence_1", "prevalence_2", "prevalence_3", "prevalence_4", "prevalence_5"))
   
   data <- data %>%
     pivot_longer(cols=c(one_of("diagnoses_1", "diagnoses_2", "diagnoses_3", "diagnoses_4", "diagnoses_5",
@@ -346,7 +364,7 @@ if(grepl("female", filename)){
                  names_to = c("outcome", "age"),
                  names_sep = "_",
                  values_to = "value")
-
+  
   data$age = age.msa.mappings[data$age]
   
   data= as.data.frame(data)
@@ -354,7 +372,6 @@ if(grepl("female", filename)){
   list(filename, data) 
   
 })
-
 ################################################################################
                         ###BEFORE 2009 FILES###
       ###Note: these files have AIDS and HIV data differentiated######
@@ -962,115 +979,115 @@ data.list.msa_sex_risk.clean = lapply(data.list.msa_sex_risk, function(file){
 ################################################################################
                   ###Put Data into Data Manager###
 ################################################################################
-
-##MSA Deaths
-msa_deaths = lapply(data.list.msa_deaths.clean, `[[`, 2)  
-
-for (data in msa_deaths) {
-  
-  data.manager$put.long.form(
-    data = data,
-    ontology.name = 'cdc msa reports',
-    source = 'cdc',
-    dimension.values = list(),
-    url = 'https://www.cdc.gov/nchhstp/atlas/index.htm',
-    details = 'CDC Reporting')
-}
-
-##MSA total
-msa_total = lapply(data.list.msa_total.clean, `[[`, 2)  
-
-for (data in msa_total) {
-  
-  data.manager$put.long.form(
-    data = data,
-    ontology.name = 'cdc msa reports',
-    source = 'cdc',
-    dimension.values = list(),
-    url = 'https://www.cdc.gov/nchhstp/atlas/index.htm',
-    details = 'CDC Reporting')
-}
-
-##MSA by Sex
-msa_sex = lapply(data.list.msa_sex.clean , `[[`, 2)  
-
-for (data in msa_sex) {
-  
-  data.manager$put.long.form(
-    data = data,
-    ontology.name = 'cdc msa reports',
-    source = 'cdc',
-    dimension.values = list(),
-    url = 'https://www.cdc.gov/nchhstp/atlas/index.htm',
-    details = 'CDC MSA Reports')
-}
-
-##MSA by Sex and age
-msa_sex_age = lapply(data.list.msa_sex_age.clean , `[[`, 2)  
-
-for (data in msa_sex_age) {
-  
-  data.manager$put.long.form(
-    data = data,
-    ontology.name = 'cdc msa reports',
-    source = 'cdc',
-    dimension.values = list(),
-    url = 'https://www.cdc.gov/nchhstp/atlas/index.htm',
-    details = 'CDC MSA Reports')
-}
-
-##MSA Data Before 2009
-msa_2009 = lapply(data.list.msa_2009.clean , `[[`, 2)  
-
-for (data in msa_2009) {
-  
-  data.manager$put.long.form(
-    data = data,
-    ontology.name = 'cdc msa reports',
-    source = 'cdc',
-    dimension.values = list(),
-    url = 'https://www.cdc.gov/nchhstp/atlas/index.htm',
-    details = 'CDC MSA Reports')
-}
-
-##MSA by Sex and Race
-msa_sex_race = lapply(data.list.msa_sex_race.clean , `[[`, 2)  
-
-for (data in msa_sex_race) {
-  
-  data.manager$put.long.form(
-    data = data,
-    ontology.name = 'cdc msa reports',
-    source = 'cdc',
-    dimension.values = list(),
-    url = 'https://www.cdc.gov/nchhstp/atlas/index.htm',
-    details = 'CDC MSA Reports')
-}
-
-##MSA by Race and Risk
-msa_race_risk = lapply(data.list.msa_race_risk.clean , `[[`, 2)  
-
-for (data in msa_race_risk) {
-  
-  data.manager$put.long.form(
-    data = data,
-    ontology.name = 'cdc msa reports',
-    source = 'cdc msa report',
-    dimension.values = list(),
-    url = 'https://www.cdc.gov/nchhstp/atlas/index.htm',
-    details = 'CDC MSA Reports')
-}
-
-##MSA by Sex and Risk
-msa_sex_risk = lapply(data.list.msa_sex_risk.clean, `[[`, 2)  
-
-for (data in msa_sex_risk) {
-  
-  data.manager$put.long.form(
-    data = data,
-    ontology.name = 'cdc msa reports',
-    source = 'cdc',
-    dimension.values = list(),
-    url = 'https://www.cdc.gov/nchhstp/atlas/index.htm',
-    details = 'CDC MSA Reports')
-}
+# 
+# ##MSA Deaths
+# msa_deaths = lapply(data.list.msa_deaths.clean, `[[`, 2)  
+# 
+# for (data in msa_deaths) {
+#   
+#   data.manager$put.long.form(
+#     data = data,
+#     ontology.name = 'cdc msa reports',
+#     source = 'cdc',
+#     dimension.values = list(),
+#     url = 'https://www.cdc.gov/nchhstp/atlas/index.htm',
+#     details = 'CDC Reporting')
+# }
+# 
+# ##MSA total
+# msa_total = lapply(data.list.msa_total.clean, `[[`, 2)  
+# 
+# for (data in msa_total) {
+#   
+#   data.manager$put.long.form(
+#     data = data,
+#     ontology.name = 'cdc msa reports',
+#     source = 'cdc',
+#     dimension.values = list(),
+#     url = 'https://www.cdc.gov/nchhstp/atlas/index.htm',
+#     details = 'CDC Reporting')
+# }
+# 
+# ##MSA by Sex
+# msa_sex = lapply(data.list.msa_sex.clean , `[[`, 2)  
+# 
+# for (data in msa_sex) {
+#   
+#   data.manager$put.long.form(
+#     data = data,
+#     ontology.name = 'cdc msa reports',
+#     source = 'cdc',
+#     dimension.values = list(),
+#     url = 'https://www.cdc.gov/nchhstp/atlas/index.htm',
+#     details = 'CDC MSA Reports')
+# }
+# 
+# ##MSA by Sex and age
+# msa_sex_age = lapply(data.list.msa_sex_age.clean , `[[`, 2)  
+# 
+# for (data in msa_sex_age) {
+#   
+#   data.manager$put.long.form(
+#     data = data,
+#     ontology.name = 'cdc msa reports',
+#     source = 'cdc',
+#     dimension.values = list(),
+#     url = 'https://www.cdc.gov/nchhstp/atlas/index.htm',
+#     details = 'CDC MSA Reports')
+# }
+# 
+# ##MSA Data Before 2009
+# msa_2009 = lapply(data.list.msa_2009.clean , `[[`, 2)  
+# 
+# for (data in msa_2009) {
+#   
+#   data.manager$put.long.form(
+#     data = data,
+#     ontology.name = 'cdc msa reports',
+#     source = 'cdc',
+#     dimension.values = list(),
+#     url = 'https://www.cdc.gov/nchhstp/atlas/index.htm',
+#     details = 'CDC MSA Reports')
+# }
+# 
+# ##MSA by Sex and Race
+# msa_sex_race = lapply(data.list.msa_sex_race.clean , `[[`, 2)  
+# 
+# for (data in msa_sex_race) {
+#   
+#   data.manager$put.long.form(
+#     data = data,
+#     ontology.name = 'cdc msa reports',
+#     source = 'cdc',
+#     dimension.values = list(),
+#     url = 'https://www.cdc.gov/nchhstp/atlas/index.htm',
+#     details = 'CDC MSA Reports')
+# }
+# 
+# ##MSA by Race and Risk
+# msa_race_risk = lapply(data.list.msa_race_risk.clean , `[[`, 2)  
+# 
+# for (data in msa_race_risk) {
+#   
+#   data.manager$put.long.form(
+#     data = data,
+#     ontology.name = 'cdc msa reports',
+#     source = 'cdc msa report',
+#     dimension.values = list(),
+#     url = 'https://www.cdc.gov/nchhstp/atlas/index.htm',
+#     details = 'CDC MSA Reports')
+# }
+# 
+# ##MSA by Sex and Risk
+# msa_sex_risk = lapply(data.list.msa_sex_risk.clean, `[[`, 2)  
+# 
+# for (data in msa_sex_risk) {
+#   
+#   data.manager$put.long.form(
+#     data = data,
+#     ontology.name = 'cdc msa reports',
+#     source = 'cdc',
+#     dimension.values = list(),
+#     url = 'https://www.cdc.gov/nchhstp/atlas/index.htm',
+#     details = 'CDC MSA Reports')
+# }
