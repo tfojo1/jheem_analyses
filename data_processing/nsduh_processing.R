08d094f12108f5fe538a7e34c47914ba38278780
Zoe Dansky <zoedansky@gmail.com>
2023-11-07 14:09:33 -0500

    updating locations into

--- data_processing/nsduh_processing.R
+++ data_processing/nsduh_processing.R
@@ -6,7 +6,6 @@
 ################################################################################
                   ###Read in NSDUH Data###
 ################################################################################ 
-
 DATA.DIR.NSDUH="../../data_raw/nsduh"
 
 nsduh_files <- Sys.glob(paste0(DATA.DIR.NSDUH, '/*.csv'))
@@ -17,7 +16,7 @@ data.list.nsduh <- lapply(nsduh_files, function(x){
 })
 ################################################################################
       ###Read in Mapping File for NSDUH "Regions"###
-      ###Commenting out bc Jeff is working on this piece###
+      ###Commenting out bc Jeff is working on this piece in the locations package###
 ################################################################################
 # 
 # mapping_file <- read_sas("~/JHEEM/data_raw/nsduh/substate_county161718.sas7bdat", 
@@ -38,79 +37,76 @@ data.list.nsduh <- lapply(nsduh_files, function(x){
 ################################################################################
                        ###Clean NSDUH Data###
                           ###REGION###
-            ###Commenting out bc Jeff is working on this piece###
-      ##Once he maps the locations you'll have to add them in here####
 ################################################################################
-# data.list.nsduh.region.clean = lapply(data.list.nsduh, function(file){
-#   
-#   data=file[["data"]] 
-#   filename = file[["filename"]] 
-#   
-#   data= subset(data, data$geography != "South")
-#   data= subset(data, data$geography != "West")
-#   data= subset(data, data$geography != "Midwest")
-#   data= subset(data, data$geography != "Northeast")
-#   
-#   data$estimate = if_else(data$estimate == "suppressed", NA, data$estimate)
-#   
-#   if(grepl("heroin", filename)) {
-#     data$outcome = "heroin"
-#   }
-#   
-#   if(grepl("cocaine", filename)) {
-#     data$outcome = "cocaine"
-#   }
-#   
-#   data <- data%>%
-#   mutate(state_name = str_extract(geography, paste(state.name, collapse = "|")))%>%
-#     mutate(state_name= ifelse(grepl("District of Columbia", geography), "District of Columbia", state_name))%>%
-#     mutate(state_abbrev = state.abb[match(state_name, state.name)]) %>%
-#     mutate(region_name = str_extract(geography, paste(substate_regions, collapse = "|")))
-#   
-#   data$state_name = if_else(data$geography == "United States", "United States", data$state_name)
-#  
-#    if(grepl("02.04", filename)) {
-#     data$year = "2002-2004"
-#   }
-#   if(grepl("04.06", filename)) {
-#     data$year = "2004-2006"
-#   }
-#   if(grepl("06.08", filename)) {
-#     data$year = "2006-2008"
-#   }
-#   if(grepl("08.10", filename)) {
-#     data$year = "2008-2010"
-#   }
-#   if(grepl("10.12", filename)) {
-#     data$year = "2010-2012"
-#   }
-#   if(grepl("12.14", filename)) {
-#     data$year = "2014-2016"
-#   }
-#    if(grepl("14.16", filename)) {
-#       data$year = "2014-2016"
-#     }
-#   if(grepl("16.18", filename)) {
-#     data$year = "2016-2018"
-#     }
-#   
-#   #data= subset(data, data$region_name != "NA")
-#   
-#   data$matching_var = paste(data$region_name, data$state_abbrev, sep=" ")
-#   
-#   #data = merge(data, mapping_file, by = "matching_var")  #in this merge things are being removed#
-#   
-#   data$value = as.numeric(data$estimate)
-#   data$location = data$FIPS
-#   data$age = data$age_group
-#   
-#      # data <- data %>%
-#      #   select(year, outcome, value, location, age)
-#      
-#   data= as.data.frame(data)
-#   
-#   list(filename, data) 
-# })
+data.list.nsduh.region.clean = lapply(data.list.nsduh, function(file){
+
+  data=file[["data"]]
+  filename = file[["filename"]]
+
+  data= subset(data, data$geography != "South")
+  data= subset(data, data$geography != "West")
+  data= subset(data, data$geography != "Midwest")
+  data= subset(data, data$geography != "Northeast")
+  data= subset(data, data$geography != "United States")
+
+  data$estimate = if_else(data$estimate == "suppressed", NA, data$estimate)
+
+  if(grepl("heroin", filename)) {
+    data$outcome = "heroin"
+  }
+
+  if(grepl("cocaine", filename)) {
+    data$outcome = "cocaine"
+  }
+  
+  #Create state abbreviations to get regions by filtering by those w/o a state abbrev
+  data <- data%>%
+    mutate(state_abbrev = state.abb[match(geography, state.name)])%>%
+    mutate(state_abbrev = if_else(geography == "District of Columbia", "DC", state_abbrev ))%>%
+    filter(is.na(state_abbrev))
+
+   if(grepl("02.04", filename)) {
+    data$year = "2002-2004"
+  }
+  if(grepl("04.06", filename)) {
+    data$year = "2004-2006"
+  }
+  if(grepl("06.08", filename)) {
+    data$year = "2006-2008"
+  }
+  if(grepl("08.10", filename)) {
+    data$year = "2008-2010"
+  }
+  if(grepl("10.12", filename)) {
+    data$year = "2010-2012"
+  }
+  if(grepl("12.14", filename)) {
+    data$year = "2014-2016"
+  }
+   if(grepl("14.16", filename)) {
+      data$year = "2014-2016"
+    }
+  if(grepl("16.18", filename)) {
+    data$year = "2016-2018"
+    }
+
+  data$location = locations::get.location.code(data$geography, "NSDUH")
+  data$location = as.character(data$location)
+  data$value = as.numeric(data$estimate)
+  data$age = data$age_group
+  
+  #I think you need to take out NA values from values col in order to put into manager#
+  data = subset(data, !is.na(data$value))
+  #I'm going to remove locations that are NA
+  data = subset(data, !is.na(data$location))
+
+      data <- data %>%
+        select(year, outcome, value, location, age)
+
+  data= as.data.frame(data)
+
+  list(filename, data)
+})
 ################################################################################
                       ###Clean NSDUH Data###
                             ###STATE###
@@ -198,7 +194,6 @@ data.list.nsduh.national.clean = lapply(data.list.nsduh, function(file){
   if(grepl("cocaine", filename)) {
     data$outcome = "cocaine"
   }
-  
   if(grepl("02.04", filename)) {
     data$year = "2002-2004"
   }
@@ -239,18 +234,18 @@ data.list.nsduh.national.clean = lapply(data.list.nsduh, function(file){
 ################################################################################
                     ###Put Data into Data Manager###
 ################################################################################
-# nsduh_region = lapply(data.list.nsduh.region.clean, `[[`, 2)
-# 
-# for (data in nsduh_region) {
-#   
-#   data.manager$put.long.form(
-#     data = data,
-#     ontology.name = 'nsduh',
-#     source = 'nsduh',
-#     dimension.values = list(),
-#     url = 'https://pdas.samhsa.gov/saes/substate',
-#     details = 'NSDUH Substate Estimates')
-# }
+nsduh_region = lapply(data.list.nsduh.region.clean, `[[`, 2)
+
+for (data in nsduh_region) {
+
+  data.manager$put.long.form(
+    data = data,
+    ontology.name = 'nsduh',
+    source = 'nsduh',
+    dimension.values = list(),
+    url = 'https://pdas.samhsa.gov/saes/substate',
+    details = 'NSDUH Substate Estimates')
+}
 
 nsduh_state = lapply(data.list.nsduh.state.clean, `[[`, 2)
 
