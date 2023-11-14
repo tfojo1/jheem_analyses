e57bc745f01fc63d7c352c989b4b955fec62e126
Zoe Dansky <zoedansky@gmail.com>
2023-11-14 17:52:05 -0500

    editing this code to help calculate the proportion of MSM

--- data_processing/brfss_state_weighted.R
+++ data_processing/brfss_state_weighted.R
@@ -288,8 +288,8 @@ data.list.brfss.state.totals = lapply(data.list.brfss.state.clean, function(file
   data$year = as.character(data$year)
   data$value = data$proportion_tested
   
-  data <- data %>%
-    select(outcome, year, location, sum_tested, n_weighted, value, `_LLCPWT`)
+  # data <- data %>%
+  #   select(outcome, year, location, sum_tested, n_weighted, value, `_LLCPWT`)
   data= as.data.frame(data)
   
   list(filename, data) 
