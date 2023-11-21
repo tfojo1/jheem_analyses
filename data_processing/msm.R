d65804f070b922bae79b3024ad5002b515f1e81f
Zoe Dansky <136368115+zoedansky@users.noreply.github.com>
2023-11-21 09:36:27 -0700

    updating dimension values

--- data_processing/msm.R
+++ data_processing/msm.R
@@ -1,16 +1,15 @@
 
 ################################################################################
-                        ##Create Proportion of MSM##
-##Note this code pulls from the BRFSS dataset created in the BRFSS weighted state code
-##This is a weighted measure
-##MSM Estimation using BRFSS Data by State
+                      ##Create Proportion of MSM##
+              ##Note this code pulls from the BRFSS dataset created in the BRFSS weighted state code
+                      ##This is a weighted measure
+                ##MSM Estimation using BRFSS Data by State
 ################################################################################
 #######
 ##Total
-#######       
-
+#######
 #Pulling from the brffs.state.sex data list so that denominator for msm proportion is males only
-data.list.brfss.state.msm = lapply(data.list.brfss.state.clean, function(file){
+data.list.brfss.state.msm = lapply(data.list.brfss.state.sex, function(file){
   
   data=file[[2]] 
   filename = file[[1]] 
@@ -18,16 +17,10 @@ data.list.brfss.state.msm = lapply(data.list.brfss.state.clean, function(file){
   #Create MSM proportion
   data$outcome= "proportion.msm"
   data$msm = as.numeric(if_else(data$risk == "msm", "1", "0"))
-  data$year = as.character(data$year)
   
-  data<- data %>% #create total msm
+  data<- data %>%
     group_by(location, msm) %>%
     mutate(msm_total = sum(msm*`_LLCPWT`))%>%  
-    ungroup()
-  
-  data<- data %>% #create total denominators by location
-    group_by(location) %>%
-    mutate(n_weighted = sum(`_LLCPWT`)) %>% 
     ungroup()
   
   data <- data %>%
@@ -48,7 +41,7 @@ data.list.brfss.state.msm = lapply(data.list.brfss.state.clean, function(file){
 ##Denominator values that are total males of each race and total males of each
 #age group so that MSM proportion can have male only denominator and be stratified by race/age appropriately
 ################################################################################
-data.list.race.male.denom = lapply(data.list.brfss.state.clean, function(file){
+data.list.race.male.denom = lapply(data.list.brfss.state.sex, function(file){
   
   data=file[[2]] 
   filename = file[[1]] 
@@ -60,14 +53,10 @@ data.list.race.male.denom = lapply(data.list.brfss.state.clean, function(file){
     mutate(n_weighted = sum(`_LLCPWT`)) %>% #denominator should be the sum of weights#
     ungroup()
   
-  
   data= as.data.frame(data)
   list(filename, data) 
 })
-
-##
-
-data.list.age.male.denom = lapply(data.list.brfss.state.clean, function(file){
+data.list.age.male.denom = lapply(data.list.brfss.state.sex, function(file){
   
   data=file[[2]] 
   filename = file[[1]] 
@@ -97,21 +86,20 @@ data.list.brfss.state.msm.race = lapply(data.list.race.male.denom, function(file
   #Create MSM proportion
   data$outcome= "proportion.msm"
   data$msm = as.numeric(if_else(data$risk == "msm", "1", "0"))
-  data$year = as.character(data$year)
   
   data<- data %>%
     group_by(location,msm, race) %>%
     mutate(msm_total = sum(msm*`_LLCPWT`))%>% 
     ungroup()
-  
+
   data <- data %>%
     select(outcome, year, location, race, msm, msm_total, n_weighted)%>%
     filter(!is.na(msm_total))%>%
     mutate(value = (msm_total/n_weighted))
-  
+   
   data$value = round(data$value, digits=2)
   
-  data<- data[!duplicated(data), ]
+   data<- data[!duplicated(data), ]
   
   data= as.data.frame(data)
   list(filename, data) 
@@ -121,6 +109,8 @@ data.list.brfss.state.msm.race = lapply(data.list.race.male.denom, function(file
 ##Age
 #######
 
+##this needs to be just males of the age group##
+
 data.list.brfss.state.msm.age = lapply(data.list.age.male.denom, function(file){
   
   data=file[[2]] 
@@ -129,13 +119,12 @@ data.list.brfss.state.msm.age = lapply(data.list.age.male.denom, function(file){
   #Create MSM proportion
   data$outcome= "proportion.msm"
   data$msm = as.numeric(if_else(data$risk == "msm", "1", "0"))
-  data$year = as.character(data$year)
   
   data<- data %>%
     group_by(location,msm,age) %>%
     mutate(msm_total = sum(msm*`_LLCPWT`))%>% 
     ungroup()
-  
+
   data <- data %>%
     select(outcome, year, location, age, msm, msm_total, n_weighted)%>%
     filter(!is.na(msm_total))%>%
@@ -149,7 +138,7 @@ data.list.brfss.state.msm.age = lapply(data.list.age.male.denom, function(file){
   list(filename, data) 
 })
 ###############################################################################
-##Put MSM Proportion into data manager- BRFSS##
+                ##Put MSM Proportion into data manager- BRFSS##
 ###############################################################################
 ##BRFSS MSM State-Total
 msm.state.total = lapply(data.list.brfss.state.msm, `[[`, 2)  
@@ -160,7 +149,7 @@ for (data in msm.state.total) {
     data = data,
     ontology.name = 'brfss',
     source = 'brfss',
-    dimension.values = list(),
+    dimension.values = list(sex = "male"),
     url = 'https://www.cdc.gov/brfss/index.html',
     details = 'Behavioral Risk Factor Surveillance System')
 }
@@ -174,7 +163,7 @@ for (data in msm.state.race) {
     data = data,
     ontology.name = 'brfss',
     source = 'brfss',
-    dimension.values = list(),
+    dimension.values = list(sex = "male"),
     url = 'https://www.cdc.gov/brfss/index.html',
     details = 'Behavioral Risk Factor Surveillance System')
 }
@@ -188,13 +177,13 @@ for (data in msm.state.age) {
     data = data,
     ontology.name = 'brfss',
     source = 'brfss',
-    dimension.values = list(),
+    dimension.values = list(sex = "male"),
     url = 'https://www.cdc.gov/brfss/index.html',
     details = 'Behavioral Risk Factor Surveillance System')
 }
 
 ###############################################################################
-##Emory MSM Data by County for 2013#
+            ##Emory MSM Data by County for 2013#
 ###############################################################################
 DATA.DIR.MSM="../../data_raw/emory"
 
@@ -205,7 +194,7 @@ data.list.emory.msm <- lapply(emory_files, function(x){
 })
 
 ###############################################################################
-##Clean Emory data##
+                ##Clean Emory data##
 ###############################################################################
 ##########
 ##County
@@ -217,7 +206,7 @@ data.list.emory.msm.county = lapply(data.list.emory.msm, function(file){
   
   data$year = "2013"
   data$outcome= "proportion.msm"
-  
+
   data$value = as.numeric(data$MSM12MTH/data$ADULTMEN)
   data$value = round(data$value, digits=2)
   
@@ -225,7 +214,7 @@ data.list.emory.msm.county = lapply(data.list.emory.msm, function(file){
   data$state_code= str_pad(data$STATEFP, width=2, side="left", pad="0")
   data$county_code= str_pad(data$COUNTYFP, width=3, side="left", pad="0")
   data$location = paste(data$state_code, data$county_code, sep="")
-  
+
   data= as.data.frame(data)
   
   list(filename, data)
@@ -245,11 +234,11 @@ data.list.emory.msm.msa = lapply(data.list.emory.msm, function(file){
   data$value = round(data$value, digits=2)
   
   #To create location- for MSA use 'CBSACODE' and also 'METMICSA' ==1 for metropolitian statistical area
-  data = subset(data, data$METMICSA == "1") #Select msas only
-  data$location = sub("^", "C.", data$CBSACODE)
-  data$location_check = locations::is.location.valid(data$location)
+    data = subset(data, data$METMICSA == "1") #Select msas only
+    data$location = sub("^", "C.", data$CBSACODE)
+    data$location_check = locations::is.location.valid(data$location)
   #Removing invalid MSA locations#
-  data=subset(data, data$location_check == 'TRUE')
+    data=subset(data, data$location_check == 'TRUE')
   
   data= as.data.frame(data)
   
@@ -263,12 +252,12 @@ data.list.emory.msm.msa = lapply(data.list.emory.msm, function(file){
 msm.emory.county = lapply(data.list.emory.msm.county, `[[`, 2)
 
 for (data in msm.emory.county) {
-  
+
   data.manager$put.long.form(
     data = data,
     ontology.name = 'emory',
     source = 'emory',
-    dimension.values = list(),
+    dimension.values = list(sex = "male"),
     url = 'https://prismhealth.emory.edu/estimating-the-population-sizes-of-men-who-have-sex-with-men-in-us-states-and-counties-using-data-from-the-american-community-survey/',
     details = 'Emory University MSM Research from American Community Survey')
 }
@@ -277,12 +266,12 @@ for (data in msm.emory.county) {
 msm.emory.msa = lapply(data.list.emory.msm.msa, `[[`, 2)
 
 for (data in msm.emory.msa ) {
-  
+
   data.manager$put.long.form(
     data = data,
     ontology.name = 'emory',
     source = 'emory',
-    dimension.values = list(),
+    dimension.values = list(sex = "male"),
     url = 'https://prismhealth.emory.edu/estimating-the-population-sizes-of-men-who-have-sex-with-men-in-us-states-and-counties-using-data-from-the-american-community-survey/',
     details = 'Emory University MSM Research from American Community Survey')
 }
