3fb36c2cf20794d03b3ea68283411b592e45f2e1
Zoe Dansky <136368115+zoedansky@users.noreply.github.com>
2023-12-14 13:42:07 -0700

    putting birth and death numerator and denominator from cdc wonder

--- data_processing/births_and_deaths.R
+++ data_processing/births_and_deaths.R
@@ -14,6 +14,7 @@ data.list.births <- lapply(birth_files, function(x) {
 })
 ################################################################################
                       ###CLEAN BIRTH DATA###
+#outcome= births
 ################################################################################
 data.list.births.clean = lapply(data.list.births, function(file){
   
@@ -56,6 +57,52 @@ data.list.births.clean = lapply(data.list.births, function(file){
   list(filename, data)  
   
 })
+################################################################################
+###CLEAN BIRTH DATA###
+#outcome= births.denominator
+################################################################################
+data.list.births.denominator = lapply(data.list.births, function(file){
+  
+  data=file[["data"]] 
+  filename = file[["filename"]]
+  
+  data$year = as.character(data$Year)
+  data$outcome= "births.denominator"
+  data$ethnicity = data$`Mother.s.Hispanic.Origin`
+  
+  if(grepl("07.19", filename)) {
+    data$race = data$`Mother.s.Bridged.Race`
+  }
+  if(grepl("20.22", filename)) {
+    data$race = data$`Mother.s.Single.Race`
+  }
+  
+  #Combining racial groups from bridged and single
+  #Todd said for now to leave in Not reported, not available, and unknown and add to the ontology
+  data$race = if_else(data$race == "Native Hawaiian or Other Pacific Islander" | data$race == "Asian", "Asian or Pacific Islander", data$race)
+  
+  ##Removing births = suppressed or Missing County##
+  ##'Missing County' appears when county data is not available for a certain year. 
+  #This occurs because the county did not meet minimum population standards and thus the data for the county was recoded to the "Unidentified Counties"
+  data = subset(data, data$`Total.Population` != "Suppressed") 
+  data = subset(data, data$`Total.Population` != "Missing County")
+  data = subset(data, data$`Total.Population` != "Not Available")
+  data$value = as.numeric(data$`Total.Population`)
+  
+  #Fix Location Codes
+  data$location= str_pad(data$`County.Code`, width=5, side="left", pad="0")
+  data$location = as.character(data$location)
+  #Remove locations that are invalid
+  data$location_flag = locations::is.location.valid(data$location)
+  data = subset(data, data$location_flag != 'FALSE')
+  
+  data <- data %>%
+    select(outcome, year, location, value, race, ethnicity)
+  
+  data = as.data.frame(data)
+  list(filename, data)  
+  
+})
 
 ################################################################################
                   ###Put BIRTHS into CENSUS MANAGER###
@@ -63,6 +110,19 @@ data.list.births.clean = lapply(data.list.births, function(file){
 births_race_eth = lapply(data.list.births.clean , `[[`, 2)
 
 for (data in births_race_eth ) {
+  
+  census.manager$put.long.form(
+    data = data,
+    ontology.name = 'census.cdc.wonder.births.deaths',
+    source = 'cdc_wonder',
+    dimension.values = list(),
+    url = 'https://wonder.cdc.gov/',
+    details = 'CDC Wonder')
+}
+
+births_denom= lapply(data.list.births.denominator , `[[`, 2)
+
+for (data in births_denom ) {
   
   census.manager$put.long.form(
     data = data,
@@ -100,35 +160,32 @@ data.list.deaths.clean = lapply(data.list.deaths, function(file){
   names(state.abb) <- state.name
   data$location =ifelse(data$State == "District of Columbia", "DC", state.abb[data$State]) 
   
-  if(grepl("01.10", filename)) {
-    data$year = "2001-2010"
+  if(grepl("06.10", filename)) {
+    data$year = "2006-2010"
   }
-  if(grepl("11.20", filename)) {
-    data$year = "2011-2020"
+  if(grepl("11.15", filename)) {
+    data$year = "2011-2015"
+  }
+  if(grepl("16.20", filename)) {
+    data$year = "2016-2020"
   }
 
   data$outcome= "metro.deaths"
+  data$race = data$Race
   
   #Remove suppressed death values
   data = subset(data, data$Deaths != "Suppressed")
   data$value = as.numeric(data$'Deaths')
   
-  data$age = data$"Ten.Year.Age.Groups"
+  data$age = data$"Single.Year.Ages"
   data$sex = tolower(data$Gender)
   data$ethnicity = data$'Hispanic.Origin'
   
    data = subset(data, data$ethnicity != "Not Stated")
    data = subset(data, data$age != "Not Stated")
   
-  if(grepl("nh", filename)) {
-    data$race = data$Race
-    data <- data %>%
+     data <- data %>%
       select(outcome, year, location, value, sex, age, race, ethnicity)
-  }
-  if(grepl("HISP", filename)) {
-    data <- data %>%
-      select(outcome, year, location, value, sex, age, ethnicity)
-  }
   
   data = as.data.frame(data)
   list(filename, data)  
@@ -146,36 +203,34 @@ data.list.deaths.denom = lapply(data.list.deaths, function(file){
   names(state.abb) <- state.name
   data$location =ifelse(data$State == "District of Columbia", "DC", state.abb[data$State]) 
   
-  if(grepl("01.10", filename)) {
-    data$year = "2001-2010"
+  if(grepl("06.10", filename)) {
+    data$year = "2006-2010"
   }
-  if(grepl("11.20", filename)) {
-    data$year = "2011-2020"
+  if(grepl("11.15", filename)) {
+    data$year = "2011-2015"
+  }
+  if(grepl("16.20", filename)) {
+    data$year = "2016-2020"
   }
   
   data$outcome= "metro.deaths.denominator"
+  
+  data$race = data$Race
   
   #Remove suppressed  values
    data = subset(data, data$Population != "Not Applicable")
   data = subset(data, data$Population != "Suppressed")
    data$value = as.numeric(data$'Population')
   
-  data$age = data$"Ten.Year.Age.Groups"
+  data$age = data$"Single.Year.Ages"
   data$sex = tolower(data$Gender)
   data$ethnicity = data$'Hispanic.Origin'
   
    data = subset(data, data$ethnicity != "Not Stated")
    data = subset(data, data$age != "Not Stated")
 
-   if(grepl("nh", filename)) {
-     data$race = data$Race
      data <- data %>%
        select(outcome, year, location, value, sex, age, race, ethnicity)
-   }
-   if(grepl("HISP", filename)) {
-     data <- data %>%
-       select(outcome, year, location, value, sex, age, ethnicity)
-   }
   
   data = as.data.frame(data)
   list(filename, data)  
