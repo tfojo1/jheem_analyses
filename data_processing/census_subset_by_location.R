8c37b3cd3eefd8f442754498c9f8a111359ac1f5
Zoe Dansky <136368115+zoedansky@users.noreply.github.com>
2023-12-15 08:05:53 -0700

    changing location of cached folder

--- data_processing/census_subset_by_location.R
+++ data_processing/census_subset_by_location.R
@@ -5,13 +5,13 @@ library(locations)
 source('commoncode/locations_of_interest.R')
 COUNTIES.OF.INTEREST <- unlist(COUNTIES.OF.INTEREST)
 
-census.manager.filepath = "../../cached/census.manager.rdata"
+census.manager.filepath = "cached/census.manager.rdata"
 
 load(census.manager.filepath)
 
 smaller.census.manager = census.manager$subset.data(dimension.values = list(location = COUNTIES.OF.INTEREST))
 
-save(census.manager, file="../../cached/smaller.census.manager.rdata")
+save(census.manager, file="cached/smaller.census.manager.rdata")
 
 # NOTES: Get the COUNTIES.OF.INTEREST from jheem_analyses/commoncode/lcations.of.interest.R
 # Change these filepaths based on what the current working directory is.
