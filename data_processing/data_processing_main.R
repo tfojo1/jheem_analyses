e67292426d715d1f9e60bf4b046aec2d0f59df21
Zoe Dansky <zoedansky@gmail.com>
2023-12-19 16:45:21 -0500

    updating ontologies

--- data_processing/data_processing_main.R
+++ data_processing/data_processing_main.R
@@ -397,7 +397,7 @@ data.manager$register.ontology(
     year= NULL,
     location= NULL,
     age = c("1-4 years", "5-17 years", "18-19 years", "20-24 years", "25-29 years", "30-34 years", "35-39 years", "40-44 years", "45-49 years", "50-54 years", "55-59 years", "60-64 years",
-            "65-69 years", "70-74 years", "75 years+"),
+            "65-69 years", "70-74 years", "75+ years"),
     race=c("white", "hispanic", "black", "other"),
     sex=c('male','female')
   ))
--- data_processing/immigration.R
+++ data_processing/immigration.R
@@ -13,6 +13,28 @@ data.list.move <- lapply(movement_files, function(x){
 })
 
 ################################################################################
+age.mappings.immigration = c('01' = '1-4 years',
+                             '02' = '5-17 years',
+                             '03' = '18-19 years',
+                             '04' = '20-24 years',
+                             '05' = '25-29 years',
+                             '06' = '30-34 years',
+                             '07' = '35-39 years',
+                             '08' = '40-44 years',
+                             '09' = '45-49 years',
+                             '10' = '50-54 years',
+                             '11' = '55-59 years',
+                             '12' = '60-64 years',
+                             '13' = '65-69 years',
+                             '14' = '70-74 years',
+                             '15' = '75+ years')
+
+race.mappings.immigration = c('02' = 'black')
+
+ethnicity.mappings.immigration = c( '01' = 'White, Non-Hispanic',
+                                    '03' = 'Hispanic or Latino')
+
+################################################################################
 #Clean
 #BY SEX
 #outcome=immigration
@@ -38,9 +60,7 @@ data.list.move.clean = lapply(data.list.move, function(file){
     
     data<- data[!duplicated(data), ]
     }
-  
-  
-  #Location conditional formatting
+
   if(grepl("emigration_total", filename)) {
     data$location = data$`Residence 1 Year Ago Metro Code1`
     data$location = paste("C", data$location, sep=".")
@@ -67,13 +87,8 @@ data.list.move.clean = lapply(data.list.move, function(file){
      select(outcome, year, location, sex, value)
     
     data<- data[!duplicated(data), ]
-    }
+  }
   
-##BY AGE##
-  
-##BY RACE##
-  
-  #Location conditional formatting
   if(grepl("emigration_sex", filename)) {
     data$location = data$`Residence 1 Year Ago Metro Code1`
     data$location = paste("C", data$location, sep=".")
@@ -86,6 +101,98 @@ data.list.move.clean = lapply(data.list.move, function(file){
         select(outcome, year, location, sex, value)
       
       data<- data[!duplicated(data), ]
+  }
+  
+  
+  ##BY AGE##
+  if(grepl("immigration_age", filename)) {
+    data$location = data$`Current Residence Metro Code1`
+    data$location = paste("C", data$location, sep=".")
+    data$outcome = "immigration"
+    
+    data$age = age.mappings.immigration[data$`Age Group Code`]
+    
+    data <-data %>%
+      mutate(value = (`Movers from Different Metropolitan Statistical Area3 Estimate` + `Movers from Elsewhere in the U.S. or Puerto Rico Estimate` + `Movers from Abroad4 Estimate`))%>%
+      select(outcome, year, location, age, value)
+    
+    data<- data[!duplicated(data), ]
+  }
+  
+  if(grepl("emigration_age", filename)) {
+    data$location = data$`Residence 1 Year Ago Metro Code1`
+    data$location = paste("C", data$location, sep=".")
+    data$outcome = "emigration"
+    
+    data$age = age.mappings.immigration[data$`Age Group Code`]
+    
+    data <-data %>%
+      mutate(value = (`Movers to Different Metropolitan Statistical Area3 Estimate` + `Movers to Elsewhere in the U.S. or Puerto Rico Estimate` ))%>%
+      select(outcome, year, location, age, value)
+    
+    data<- data[!duplicated(data), ]
+  }
+  
+  ##Race File, race=black##
+  if(grepl("immigration_race", filename)) {
+    data$location = data$`Current Residence Metro Code1`
+    data$location = paste("C", data$location, sep=".")
+    data$outcome = "immigration"
+    
+    data=subset(data, data$`Race Code`=="02")
+    data$race = race.mappings.immigration[data$`Race Code`]
+    
+    data <-data %>%
+      mutate(value = (`Movers from Different Metropolitan Statistical Area3 Estimate` + `Movers from Elsewhere in the U.S. or Puerto Rico Estimate` + `Movers from Abroad4 Estimate`))%>%
+      select(outcome, year, location, race, value)
+    
+    data<- data[!duplicated(data), ]
+  }
+  
+  if(grepl("emigration_race", filename)) {
+    data$location = data$`Residence 1 Year Ago Metro Code1`
+    data$location = paste("C", data$location, sep=".")
+    data$outcome = "emigration"
+    
+    data=subset(data, data$`Race Code`=="02")
+    data$race = race.mappings.immigration[data$`Race Code`]
+    
+    data <-data %>%
+      mutate(value = (`Movers to Different Metropolitan Statistical Area3 Estimate` + `Movers to Elsewhere in the U.S. or Puerto Rico Estimate` ))%>%
+      select(outcome, year, location, race, value)
+    
+    data<- data[!duplicated(data), ]
+  }
+  ##Ethnicity File; eth=white; hispanic##
+
+  if(grepl("immigration_eth", filename)) {
+    data$location = data$`Current Residence Metro Code1`
+    data$location = paste("C", data$location, sep=".")
+    data$outcome = "immigration"
+    
+    data=subset(data, data$`Hispanic Origin Code`!="02")
+    data$race = race.mappings.immigration[data$`Hispanic Origin Code`]
+    
+    data <-data %>%
+      mutate(value = (`Movers from Different Metropolitan Statistical Area3 Estimate` + `Movers from Elsewhere in the U.S. or Puerto Rico Estimate` + `Movers from Abroad4 Estimate`))%>%
+      select(outcome, year, location, race, value)
+    
+    data<- data[!duplicated(data), ]
+  }
+  
+  if(grepl("emigration_eth", filename)) {
+    data$location = data$`Residence 1 Year Ago Metro Code1`
+    data$location = paste("C", data$location, sep=".")
+    data$outcome = "emigration"
+    
+    data=subset(data, data$`Hispanic Origin Code`!="02")
+    data$race = ethnicity.mappings.immigration[data$`Hispanic Origin Code`]
+    
+    data <-data %>%
+      mutate(value = (`Movers to Different Metropolitan Statistical Area3 Estimate` + `Movers to Elsewhere in the U.S. or Puerto Rico Estimate` ))%>%
+      select(outcome, year, location, race, value)
+    
+    data<- data[!duplicated(data), ]
   }
   
   
