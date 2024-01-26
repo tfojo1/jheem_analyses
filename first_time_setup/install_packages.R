386fe0d9667255937f2cd5e1d200482d57321a31
Todd Fojo <tfojo1@jh.edu>
2024-01-26 14:03:48 -0500

    update to install packages

--- first_time_setup/install_packages.R
+++ first_time_setup/install_packages.R
@@ -2,6 +2,16 @@
 
 library(devtools)
 
+# Packages requires for our custom packages
+install.packages('ggmap')
+
+# Our Custom Packages
 install_github('tfojo1/distributions')
 install_github('tfojo1/bayesian.simulations')
 install_github('tfojo1/locations')
+
+# Other packages required for analyses
+
+# Archival packages - I hope to eventually remove these
+install_github('tfojo1/jheem')
+install.packages('sas7bdat')
