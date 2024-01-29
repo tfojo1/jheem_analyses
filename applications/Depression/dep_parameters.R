7b27da4ee196ea898de8cecf3712821726dcc0bf
Lucia Cilloni <lmcilloni@yahoo.co.uk>
2024-01-29 12:41:21 -0500

    updating depression parameter priors

--- applications/Depression/dep_parameters.R
+++ applications/Depression/dep_parameters.R
@@ -1,6 +1,24 @@
 # depression parameters
 
 DEP.PARAMETERS.PRIOR <- join.distributions(
-  depression.proportion.tx <- Logitnormal.Distribution(), 
   
+  #-- Depression characteristics --#
+  depression.proportion.tx <- Lognormal.Distribution(0, log(2)), 
+  depression.length        <- Lognormal.Distribution(log(6/12), log(2)), 
+  
+  #-- Suppression --#
+  rr.suppression.dep.tx   <- Lognormal.Distribution(0, log(2)), 
+  rr.suppression.dep.notx <- Lognormal.Distribution(0, log(2)), 
+  
+  #-- Sexual susceptibility --#
+  rr.sex.sus.dep.hetmale <- Lognormal.Distribution(log(.88), log(2)), 
+  rr.sex.sus.dep.msm     <- Lognormal.Distribution(log(.88), log(2)), 
+  rr.sex.sus.dep.female  <- Lognormal.Distribution(log(.72), log(2)), 
+  
+  #-- IDU susceptibility --#
+  rr.idu.dep.tx   <- Lognormal.Distribution(log(.71), log(2)), 
+  rr.idu.dep.notx <- Lognormal.Distribution(log(1.67), log(2)), 
+  
+  #-- HIV Testing --#
+  rr.testing.depressed <- Lognormal.Distribution(log(1.4), log(2)), 
 )
