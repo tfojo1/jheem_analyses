6224c7f18c6b6537aa306df9cf955cfa818bad15
A-Zalesak <azalesak@alumni.unc.edu>
2024-02-21 15:39:31 -0500

    fix bugs and begin proportion implementation

--- data_processing/put_msa_data_as_new_source_script.R
+++ data_processing/put_msa_data_as_new_source_script.R
@@ -10,29 +10,49 @@ put.msa.data.as.new.source = function(outcome,
                                       geographic.type.to,
                                       details.for.new.data,
                                       data.manager,
+                                      source.for.denominator=NULL,
+                                      ontology.for.denominator=NULL,
                                       maximum.suppressed.value = 4,
                                       tolerable.fraction.suppressed = 0.05) {
-    
+    # browser()
     error.prefix = "Cannot estimate data from contained location data: "
     # validate if desired
     if (!(from.source.name %in% names(data.manager$data[[outcome]][[metric]])))
         stop(paste0(error.prefix, "'", from.source.name, "' is not a registered source for outcome '", outcome, "'"))
+    # check scale
+    scale = data.manager$outcome.info[[outcome]]$metadata$scale
+    denominator.outcome = data.manager$outcome.info[[outcome]]$denominator.outcome
+    if (!(scale %in% c('non.negative.number', 'proportion')))
+        stop(paste0(error.prefix, "this method does not currently support outcomes of scale '", scale, "'"))
+    if (scale == 'proportion' && is.null(source.for.denominator))
+        stop(paste0(error.prefix, "outcomes of scale '", scale, "' must have a 'source.for.denominator' specified"))
+    if (scale == 'proportion' && is.null(denominator.outcome))
+        stop(paste0(error.prefix, "a denominator outcome could not be found"))
+    if (!is.null(source.for.denominator) && !(source.for.denominator %in% names(data.manager$data[[denominator.outcome]][['estimate']]))) #always aggregate with estimate, right?
+        stop(paste0(error.prefix, "'", source.for.denominator, "' is not a registered source for denominator outcome '", denominator.outcome, "'"))
+    if (!is.null(source.for.denominator) && !(ontology.for.denominator %in% names(data.manager$data[[denominator.outcome]][['estimate']][[source.for.denominator]])))
+        stop(paste0(error.prefix, "'", ontology.for.denominator, "' is not a registered ontology for source '", source.for.denominator, "'"))
     
     # print(outcome)
     outcome.data.all.ontologies = data.manager$data[[outcome]][[metric]][[from.source.name]]
     outcome.details.all.ontologies = data.manager$details[[outcome]][[metric]][[from.source.name]]
     outcome.url.all.ontologies = data.manager$url[[outcome]][[metric]][[from.source.name]]
     
+    if (scale=='proportion') {
+        denominator.data.used.ontology = data.manager$data[[denominator.outcome]][['estimate']][[source.for.denominator]][[ontology.for.denominator]] #assumption: one ontology holds all our denominator data
+    }
+    
     for (to.location in to.locations) {
+        # browser()
         from.locations = locations::get.contained.locations(to.location, geographic.type.from)
         for (ont.name in names(outcome.data.all.ontologies)) {
             for (strat.name in names(outcome.data.all.ontologies[[ont.name]])) {
                 strat.data = outcome.data.all.ontologies[[ont.name]][[strat.name]]
                 strat.details = outcome.details.all.ontologies[[ont.name]][[strat.name]]
                 strat.url = outcome.url.all.ontologies[[ont.name]][[strat.name]]
-                
+
                 # We must have data for all counties
-                if(!setequal(dimnames(strat.data)$location, from.locations)) next
+                if(length(setdiff(from.locations, dimnames(strat.data)$location))>0) next
                 
                 if (!is.null(years)) years.in.this.strat.data = intersect(dimnames(strat.data)$year, years)
                 else years.in.this.strat.data = dimnames(strat.data)$year
@@ -46,16 +66,47 @@ put.msa.data.as.new.source = function(outcome,
                 # Aggregate across location
                 non.location.margin = setdiff(names(dim(strat.data.from.locs.only)), 'location')
                 
-                # skip years where >=2/3 of counties are missing from all strata
-                years.with.enough.data = apply(
-                    apply(strat.data.from.locs.only, c('year', 'location'), function(x) {all(is.na(x))}),
-                    MARGIN='year',
-                    function(x) {
-                        sum(is.na(x)) < (2/3) * length(x)
-                    }
-                )
-                strat.data.from.locs.only = array.access(strat.data.from.locs.only, year=names(years.without.any.counties.missing.from.all.strata)[years.without.any.counties.missing.from.all.strata])
-                if (length(strat.data.from.locs.only)==0) next
+                if (scale == 'non.negative.number') {
+                    # skip years where >=2/3 of counties are missing from all strata
+                    years.with.enough.data = apply(
+                        apply(strat.data.from.locs.only, c('year', 'location'), function(x) {all(is.na(x))}),
+                        MARGIN='year',
+                        function(x) {
+                            sum(is.na(x)) < (2/3) * length(x)
+                        }
+                    )
+                    strat.data.from.locs.only = array.access(strat.data.from.locs.only, year=names(years.with.enough.data)[years.with.enough.data])
+                    if (length(strat.data.from.locs.only)==0) next
+                    
+                }
+                
+                
+                
+                if (scale == 'proportion') {
+                    
+                    if (!(strat.name %in% names(denominator.data.used.ontology))) next
+                    denominator.data = denominator.data.used.ontology[[strat.name]]
+                    
+                    # We must have data for all counties
+                    if(!setequal(dimnames(denominator.data)$location, from.locations)) next
+                    
+                    years.in.this.denom.data = intersect(dimnames(denominator.data), years.in.this.strat.data)
+                    if (length(years.in.this.denom.data)==0) next
+                    
+                    denominator.data.from.locs.only = do.call('[', get.subset.arguments(denominator.data, years.in.this.denom.data, from.locations))
+                    if (is.null(denominator.data.from.locs.only)) next
+                    if (all(is.na(denominator.data.from.locs.only))) next
+                    
+                    # skip years where >=2/3 of counties are missing from all strata of the denominator
+                    years.with.enough.data = apply(
+                        apply(denominator.data.from.locs.only, c('year', 'location'), function(x) {all(is.na(x))}),
+                        MARGIN='year',
+                        function(x) {
+                            sum(is.na(x)) < (2/3) * length(x)
+                        }
+                    )
+                    
+                }
                 
                 aggregated.data = apply.robust(strat.data.from.locs.only, non.location.margin, function(x) {
                     
@@ -139,8 +190,9 @@ aggregate.details.or.url = function(data, keep.dimensions, post.agg.dimnames) {
 #                            data.manager = SURVEILLANCE.MANAGER)
 
 # for testing
-# ss=SURVEILLANCE.MANAGER
-# ss$register.source(source = 'cdc.aggregated.county', parent.source='NHSS', full.name = 'CDC Aggregated County', short.name = 'cdc aggd county')
+
+# ss=SURVEILLANCE.MANAGER$clone()
+# ss$register.source(source = 'cdc.agg.county', parent.source='NHSS', full.name = 'CDC Aggregated County', short.name = 'cdc aggd county')
 # put.msa.data.as.new.source(outcome = 'diagnosed.prevalence',
 #                            from.source.name = 'cdc.hiv',
 #                            to.source.name = 'cdc.agg.county',
