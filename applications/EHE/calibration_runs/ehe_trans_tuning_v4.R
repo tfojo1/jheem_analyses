ea00f1c9164653001001cc7e235f4197099327b6
mschnure <mschnur3@jhmi.edu>
2024-02-21 15:42:09 -0500

    more tuning

---
+++ applications/EHE/calibration_runs/ehe_trans_tuning_v4.R
@@ -0,0 +1,89 @@
+source('../jheem_analyses/applications/EHE/ehe_specification.R')
+
+load("../jheem_analyses/prelim_results/init.transmission.sim_2024-02-21_C.12580.Rdata")
+
+params = sim$parameters[,1]
+
+params.manual = params
+engine = create.jheem.engine('ehe',sim$location,end.year = 2030) 
+
+# I am having a hard time making any improvements with these - going to just give them to be sampled instead 
+params.manual["msm.incident.idu.multiplier.0"] = 0.9*params["msm.incident.idu.multiplier.0"]
+params.manual["black.incident.idu.multiplier.0"] = 1*params["black.incident.idu.multiplier.0"]
+params.manual["other.incident.idu.multiplier.0"] = 0.75*params["other.incident.idu.multiplier.0"]
+
+params.manual["msm.vs.heterosexual.male.idu.susceptibility.rr.0"] = 1*params["msm.vs.heterosexual.male.idu.susceptibility.rr.0"]
+params.manual["msm.vs.heterosexual.male.idu.susceptibility.rr.1"] = 1*params["msm.vs.heterosexual.male.idu.susceptibility.rr.1"]
+params.manual["msm.vs.heterosexual.male.idu.susceptibility.rr.peak"] = 1.05*params["msm.vs.heterosexual.male.idu.susceptibility.rr.peak"]
+
+sim.manual = engine$run(parameters = params.manual) 
+
+# EHE.PARAMETERS.PRIOR@var.names[grepl('incident.idu', EHE.PARAMETERS.PRIOR@var.names)]
+# and msm.vs.het...
+
+# One-way plots (by risk or race): prevalence, new, AIDS diagnoses 
+simplot(sim,
+        sim.manual,
+        facet.by = "risk", # idu and msm-idu undershooting 
+        outcomes = c("diagnosed.prevalence"), 
+        dimension.values = list(year = 2000:2020))
+
+simplot(sim,
+        sim.manual,
+        facet.by = "race", # hispanic undershooting (and black historically)
+        outcomes = c("diagnosed.prevalence"),
+        dimension.values = list(year = 2000:2020)) 
+
+simplot(sim,
+        sim.manual,
+        facet.by = "risk", # pretty good 
+        outcomes = c("new"),
+        dimension.values = list(year = 2000:2020)) 
+
+simplot(sim,
+        sim.manual,
+        facet.by = "race", # hispanic undershooting (but small #s), other overshooting
+        outcomes = c("new"),
+        dimension.values = list(year = 2000:2020)) 
+
+simplot(sim,
+        sim.manual,
+        facet.by = "risk", # heterosexual/msm overshooting
+        outcomes = c("aids.diagnoses"),
+        dimension.values = list(year = 1981:2020))
+
+simplot(sim,
+        sim.manual,
+        facet.by = "race", # hispanic undershooting (but small #s)
+        outcomes = c("aids.diagnoses"),
+        dimension.values = list(year = 1981:2020))
+
+# Two-way plots; prevalence, new (race/risk); population (age/race)
+simplot(sim,
+        sim.manual,
+        facet.by = "risk", split.by = "race", # black msm overshooting; idu and msm-idu all undershooting
+        outcomes = c("diagnosed.prevalence"),
+        dimension.values = list(year = 2000:2020)) 
+
+simplot(sim,
+        facet.by = "risk", split.by = "race", # not bad 
+        outcomes = c("new"),
+        dimension.values = list(year = 2000:2020)) 
+
+simplot(sim,
+        facet.by = "age", split.by = "race",
+        outcomes = c("diagnosed.prevalence"), 
+        dimension.values = list(year = 2000:2020))
+
+simplot(sim,
+        facet.by = "age", split.by = "race",
+        outcomes = c("new"),
+        dimension.values = list(year = 2000:2020)) 
+
+simplot(sim,
+        facet.by = "age", split.by = "race", # good 
+        outcomes = c("population"),
+        dimension.values = list(year = 2000:2020)) 
+
+
+
