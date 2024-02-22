5dfed23c553cc15bc7f3c088870b1b6384d9c9f9
Padmashri Saravanan <psarava1@jhu.edu>
2024-02-22 12:16:33 -0500

    plotting incidence reduction

--- applications/prep_upscale/eg_systematic_interventions.R
+++ applications/prep_upscale/eg_systematic_interventions.R
@@ -112,46 +112,118 @@ for (i in 1:length(prep.upscale.intervention.names)) {
     print(upscale_table(PREP.UPSCALE.INTERVENTION.CODES[i], prep.upscale.intervention.names[i]))
 }
 
-# Reduction in HIV incidence among MSM from 2025 to 2035 in Black and Hispanic communities
+year_inc <- function(int.year, race, sex = "msm", intervention.code){
+  # returns average incidence race for int.year x race x sex group across simulations for the locations
+  # results.year = collection$get(outcomes = c('incidence','population'),
+  #                               dimension.values = list(year=c(int.year)),
+  #                               keep.dimensions = c('race', 'sex'))
+  if(int.year == 2025){
+    return(colMeans(results.2025[race,sex,,"incidence",,intervention.code]/
+                      results.2025[race,sex,,"population",,intervention.code]))
+  }
+  if(int.year == 2035){
+    return(colMeans(results.2035[race,sex,,"incidence",,intervention.code]/
+                      results.2035[race,sex,,"population",,intervention.code]))
+  }
+}
 
-# incidence difference
+# to plot reduction in incidence by location
+plotinc_byloc <- function(race, intervention.code, intervention.name){
+  inc_df <- as.data.frame(year_inc(2025, race, "msm", intervention.code))
+  inc_df["year_2035"] <- as.data.frame(year_inc(2035, race, "msm", intervention.code))
+  colnames(inc_df) <- c("year_2025", "year_2035")
+  inc_df$location <- rownames(inc_df)
+  
+  inc_long <- pivot_longer(inc_df, 
+                                cols = starts_with("year_"), 
+                                names_to = c(".value", "Year"),  # separate entity and year
+                                names_pattern = "^(.*?)_(.*)$",  # regex pattern to separate entity and year
+                                values_to = "Value") %>% dplyr::rename(ir = year, year = Year)
+  
+  if(race == "other"){
+    race <- "Non-Black non-Hispanic"
+  }
+  
+  # print(inc_long)
+  
+  return(ggplot(data = inc_long, aes(year,ir*100000)) +
+    geom_point(aes(color = location)) +
+    geom_line(aes(group = location, color = location)) +
+    labs(x = "Year", y = "Incidence Rate (per 100,000 population)",
+         title = paste("Reduction in HIV incidence from 2025 to 2035 with",
+                       intervention.name), 
+         subtitle = paste("Among the", race, "population")) +
+    theme_minimal())
+}
+
+blackinc_plots <- list()
+hispinc_plots <- list()
+otherinc_plots <- list()
+
+for (i in 1:length(prep.upscale.intervention.names)) {
+  blackinc_plots[[prep.upscale.intervention.names[i]]] <- plotinc_byloc("black",PREP.UPSCALE.INTERVENTION.CODES[i],
+                                                         prep.upscale.intervention.names[i])
+  hispinc_plots[[prep.upscale.intervention.names[i]]] <- plotinc_byloc("hispanic",PREP.UPSCALE.INTERVENTION.CODES[i], prep.upscale.intervention.names[i])
+  otherinc_plots[[prep.upscale.intervention.names[i]]] <- plotinc_byloc("other",PREP.UPSCALE.INTERVENTION.CODES[i], prep.upscale.intervention.names[i])
+}
+
+library(patchwork)
+(blackinc_plots[[1]]+blackinc_plots[[2]]) / ((blackinc_plots[[3]]+blackinc_plots[[4]])+(blackinc_plots[[5]]))
+
+
+
 # 
-# ## black
-# black.id <- colMeans((results.2025["black","msm",,"incidence",,"msmprepuse10"] - 
-#            results.2035["black","msm",,"incidence",,"msmprepuse10"])/
-#            results.2035["black","msm",,"incidence",,"msmprepuse10"])
+# # to plot reduction in incidence by location
+# plotinc_bycode <- function(race, intervention.code, intervention.name, location){
+#   inc_df <- as.data.frame(year_inc(2025, race, "msm", intervention.code))
+#   inc_df["year_2035"] <- as.data.frame(year_inc(2035, race, "msm", intervention.code))
+#   colnames(inc_df) <- c("year_2025", "year_2035")
+#   inc_df$location <- rownames(inc_df)
+#   
+#   inc_long <- pivot_longer(inc_df, 
+#                            cols = starts_with("year_"), 
+#                            names_to = c(".value", "Year"),  # separate entity and year
+#                            names_pattern = "^(.*?)_(.*)$",  # regex pattern to separate entity and year
+#                            values_to = "Value") %>% dplyr::rename(ir = year, year = Year)
+#   
+#   if(race == "other"){
+#     race <- "Non-Black non-Hispanic"
+#   }
+#   
+#   # print(inc_long)
+#   
+#   inc_long <- inc_long |> dplyr::filter(location == location)
+#   
+#   return(ggplot(data = inc_long, aes(year,ir*100000)) +
+#            geom_point(aes(color = location)) +
+#            geom_line(aes(group = location, color = location)) +
+#            labs(x = "Year", y = "Incidence Rate (per 100,000 population)",
+#                 title = paste("Reduction in HIV incidence from 2025 to 2035 with",
+#                               intervention.name), 
+#                 subtitle = paste("Among the", race, "population")) +
+#            theme_minimal())
+# }
+
+#   
+# blackinc_df <- as.data.frame(year_inc(2025, "black", intervention.code = "msmprepuse10")) %>% 
+#   dplyr::rename(year_2025 = `year_inc(2025, "black", intervention.code = "msmprepuse10")`)
+# blackinc_df["year_2035"] <- as.data.frame(year_inc(2035, "black", intervention.code = "msmprepuse10"))
+# blackinc_df$location <- rownames(blackinc_df)
 # 
-# print(paste("Reduction in Incidence among Black MSM population with", intervention.name))
-# black.id
+# blackinc_long <- pivot_longer(blackinc_df, 
+#                         cols = starts_with("year_"), 
+#                         names_to = c(".value", "Year"),  # separate entity and year
+#                         names_pattern = "^(.*?)_(.*)$",  # regex pattern to separate entity and year
+#                         values_to = "Value") %>% dplyr::rename(ir = year, year = Year)         
 # 
-# ## hispanic
-# print(paste("Reduction in Incidence among Hispanic MSM population with", intervention.name))
-# hisp.id <- colMeans((results.2025["hispanic","msm",,"incidence",,"msmprepuse10"] - 
-#            results.2035["hispanic","msm",,"incidence",,"msmprepuse10"])/
-#   results.2035["hispanic","msm",,"incidence",,"msmprepuse10"])
+# ggplot(data = blackinc_long, aes(year,ir*100000)) +
+#   geom_point(aes(color = location)) +
+#   geom_line(aes(group = location, color = location)) +
+#   labs(x = "Year", y = "Incidence Rate (per 100,000 population)",
+#        title = paste("Reduction in HIV incidence from 2025 to 2035 with",
+#                      prep.upscale.intervention.names[1]), 
+#        subtitle = paste("Among the", race, "population")) +
+#   theme_minimal()
 # 
-# ## both 
-# print(paste("Reduction in Incidence among Black and Hispanic MSM population with", intervention.name))
-# black.hisp.id <- colMeans(colSums(results.2025[c("black","hispanic"),"msm",,"incidence",,"msmprepuse10"])-
-#            colSums(results.2035[c("black","hispanic"),"msm",,"incidence",,"msmprepuse10"])/
-#            colSums(results.2025[c("black","hispanic"),"msm",,"incidence",,"msmprepuse10"]))
 # 
-# # incidence rate among MSM
 # 
-# # calculating IRR 
-# 
-# black.msm.ir <- results.2035["black","msm",,"incidence",,"msmprepuse10"] / 
-#   results.2035["black","msm",,"population",,"msmprepuse10"] 
-# 
-# hispanic.msm.ir <- results.2035["hispanic","msm",,"incidence",,"msmprepuse10"] / 
-#   results.2035["hispanic","msm",,"population",,"msmprepuse10"] 
-# 
-# other.msm.ir <- results.2035["other","msm",,"incidence",,"msmprepuse10"] / 
-#   results.2035["other","msm",,"population",,"msmprepuse10"] 
-# 
-# black.hisp.msm.ir <- colSums(results.2025[c("black","hispanic"),"msm",,"incidence",,"msmprepuse10"])/
-#   colSums(results.2025[c("black","hispanic"),"msm",,"population",,"msmprepuse10"])
-# 
-# colMeans(black.msm.ir/other.msm.ir) # black msm vs other msm IRR
-# colMeans(hispanic.msm.ir/other.msm.ir) # hisp msm vs other msm IRR
-# colMeans(black.hisp.msm.ir/other.msm.ir) # black & hisp msm vs other msm IRR
