5b1f2665fcaab8f7b6aba1ef27f58e777d322dd8
Zoe Dansky <136368115+zoedansky@users.noreply.github.com>
2024-02-26 11:00:41 -0700

    cleaning updates

--- data_processing/nhanes.processing.R
+++ data_processing/nhanes.processing.R
@@ -10,7 +10,12 @@ nhanes.files.data.list <- lapply(nhanes.files, function(x) {
   list(filename=x, data=read_xpt(x))
 })
 ################################################################################
-
+age.mappings = c('9' = '2015-2016',
+                 '8' = '2013-2014',
+                 '7' = '2011-2012',
+                 '6' = '2009-2010',
+                 '5' = '2007-2008')
+################################################################################
 nhanes.clean = lapply(nhanes.files.data.list, function(file){
   
   data=file[["data"]]
@@ -18,67 +23,101 @@ nhanes.clean = lapply(nhanes.files.data.list, function(file){
   
   if(grepl("demographics", filename)) {
     data <- data %>%
-      select(SEQN, WTINT2YR, WTMEC2YR, SDDSRVYR, RIDAGEYR, RIDRETH1, RIAGENDR)
+      select(SEQN, WTINT2YR, WTMEC2YR, SDDSRVYR, RIDAGEYR, RIDRETH1, RIAGENDR)%>%
+      rename(survey.year = SDDSRVYR)%>%
+      rename(ID = SEQN)%>%
+      rename(age = RIDAGEYR)%>%
+      rename(gender = RIAGENDR)
   }
   
   if(grepl("sexual_behavior_ok", filename)) {
     data <- data %>%
-      select(SEQN, SXD021,  SXD031, SXQ251, SXQ270, SXQ294)
+      select(SEQN, SXD021, SXQ809, SXD031, SXQ251, SXQ270, SXQ292, SXQ294)%>%
+      rename(ID = SEQN)%>%
+      rename(ever.had.sex =SXD021)%>%
+      rename(men.ever.sex.with.men =SXQ809)%>%
+      rename(age.at.first.sex = SXD031)%>%
+      rename(number.sex.wo.condom.past.year = SXQ251)%>%
+      rename(hx.gonorrhea = SXQ270)%>%
+      rename(sexual.orientation.male = SXQ292)%>%
+      rename(sexual.orientation.female = SXQ294)
+
   }
   
   if(grepl("sexual_behavior_fix1", filename)) { #NEED TO ADD IN HERE SOMETHNG FOR CALCULATE SEX WITH MEN SXQ101
     data <- data %>%
-      select(SEQN, SXD021,  SXD031, SXQ251, SXQ270, SXQ294)
+      select(SEQN, SXQ021,  SXQ101, SXD031, SXQ251, SXQ270, SXQ292, SXQ294)%>%
+      rename(ID = SEQN)%>%
+      rename(ever.had.sex =SXQ021)%>%
+      rename(men.ever.sex.with.men =SXQ101)%>%    #You will need to use gender from demos to re-calculate this bc it's different from other vars in other years but I'm relabeling for the purpose of merge
+      rename(age.at.first.sex = SXD031)%>%
+      rename(number.sex.wo.condom.past.year = SXQ251)%>%
+      rename(hx.gonorrhea = SXQ270)%>%
+      rename(sexual.orientation.male = SXQ292)%>%
+      rename(sexual.orientation.female = SXQ294)
   }
   
   if(grepl("sexual_behavior_fix2", filename)) {
     data <- data %>%
-      rename(SXQ292 = SXQ296)%>%
-      rename(SXQ294 = SXQ296)%>%
-      select(SEQN, SXD021,  SXD031, SXQ251, SXQ270, SXQ294)
+      select(SEQN, SXD021,  SXQ809, SXD031, SXQ251, SXQ270, SXQ295, SXQ296)%>%
+      rename(ID = SEQN)%>%
+      rename(ever.had.sex =SXD021)%>%
+      rename(men.ever.sex.with.men =SXQ809)%>%
+      rename(age.at.first.sex = SXD031)%>%
+      rename(number.sex.wo.condom.past.year = SXQ251)%>%
+      rename(hx.gonorrhea = SXQ270)%>%
+      rename(sexual.orientation.male = SXQ296)%>%
+      rename(sexual.orientation.female = SXQ295)
   }
+  
+  data$survey.year = outcome.mappings[data$Isurvey.year]
+  
   list(filename, data) 
   
 })
+
 ################################################################################
-##Merge datasets
-demo = nhanes.files.data.list[[1]]
+#Merge all the years for demo and sexual behavior
+################################################################################
+
+demo = nhanes.clean[[1]]
 demo = demo [[2]]
-sex = nhanes.files.data.list[[2]]
+sex = nhanes.clean[[2]]
 sex = sex [[2]]
-total.07.08= merge(demo, sex, by="SEQN")
+total.07.08= merge(demo, sex, by="ID")
 
-demo = nhanes.files.data.list[[3]]
+demo = nhanes.clean[[3]]
 demo = demo [[2]]
-sex = nhanes.files.data.list[[4]]
+sex = nhanes.clean[[4]]
 sex = sex [[2]]
-total.09.10= merge(demo, sex, by="SEQN")
+total.09.10= merge(demo, sex, by="ID")
 
-demo = nhanes.files.data.list[[5]]
+demo = nhanes.clean[[5]]
 demo = demo [[2]]
-sex = nhanes.files.data.list[[6]]
+sex = nhanes.clean[[6]]
 sex = sex [[2]]
-total.11.12= merge(demo, sex, by="SEQN")
+total.11.12= merge(demo, sex, by="ID")
 
-demo = nhanes.files.data.list[[7]]
+demo = nhanes.clean[[7]]
 demo = demo [[2]]
-sex = nhanes.files.data.list[[8]]
+sex = nhanes.clean[[8]]
 sex = sex [[2]]
-total.13.14= merge(demo, sex, by="SEQN")
+total.13.14= merge(demo, sex, by="ID")
 
-demo = nhanes.files.data.list[[9]]
+demo = nhanes.clean[[9]]
 demo = demo [[2]]
-sex = nhanes.files.data.list[[10]]
+sex = nhanes.clean[[10]]
 sex = sex [[2]]
-total.15.16= merge(demo, sex, by="SEQN")
+total.15.16= merge(demo, sex, by="ID")
 
-nhanes.1 = merge(total.07.08, total.09.10, by= "SEQN")
-nhanes.2 = merge(nhanes.1, total.11.12, by= "SEQN")
-nhanes.3 = merge(nhanes.2, total.13.14, by= "SEQN")
-nhanes.total = merge(nhanes.3, total.15.16, by= "SEQN")
+nhanes.total = rbind(total.07.08, total.09.10, total.11.12, total.13.14, total.15.16)
 
+################################################################################
+#Now wrangle data for subset
+################################################################################
 
-nhanes.total = rbind(total.07.08, total.09.10)
+nhanes.total.clean <- nhanes.total%>%
+  mutate()
 
 
 
