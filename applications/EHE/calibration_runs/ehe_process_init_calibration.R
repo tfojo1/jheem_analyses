67262d7352f02d182826d2cf879a1d3fd2572ce1
Melissa Schnure <mschnur3@jh.edu>
2024-02-27 12:42:06 -0500

    simset code

--- applications/EHE/calibration_runs/ehe_process_init_calibration.R
+++ applications/EHE/calibration_runs/ehe_process_init_calibration.R
@@ -10,10 +10,12 @@ mcmc = assemble.mcmc.from.calibration(version = 'ehe',
 
 # Pull all the simulations into a simset
 simset = join.simulation.sets((mcmc@simulations[mcmc@simulation.indices]))
-# simset.new = simset$burn(keep = 0.5)
-# simset.new = simset.new$thin(keep = 100)
+simset = simset$burn(keep = 0.5)
+simset = simset$thin(keep = 100)
+
+save(simset,file=paste0("prelim_results/init.transmission.SIMSET_",Sys.Date(),"_",ONE.LOCATION,".Rdata"))
 
 # Pull just the last simulation
-sim = mcmc@simulations[[length(mcmc@simulations)]]
-
-save(sim,file=paste0("prelim_results/init.transmission.sim_",Sys.Date(),"_",ONE.LOCATION,".Rdata"))
+# sim = mcmc@simulations[[length(mcmc@simulations)]]
+# 
+# save(sim,file=paste0("prelim_results/init.transmission.sim_",Sys.Date(),"_",ONE.LOCATION,".Rdata"))
