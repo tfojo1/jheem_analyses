d5166761abfaaa416a168c6f0d06684e6d4103cd
Todd Fojo <tfojo1@jh.edu>
2024-03-04 23:38:27 -0500

    get.cached.object.for.version now pulls from ancestor versions too

--- commoncode/cache_object_for_version_functions.R
+++ commoncode/cache_object_for_version_functions.R
@@ -13,9 +13,20 @@ cache.object.for.version <- function(object, name, version, overwrite=F)
 
 get.cached.object.for.version <- function(name, version)
 {
-    error.prefix = "Error getting cached object for version: "
     BASE.FILE.PATH = '../jheem_analyses/commoncode/object_for_version_cache'
-    FILE.PATH = paste0(BASE.FILE.PATH, "/", version, "_", name, ".Rdata")
-    load(file=FILE.PATH) # will be called "object"
-    return (object)
+    for (version.to.try in c(version, get.prior.versions(version)))
+    {
+        error.prefix = "Error getting cached object for version: "
+        FILE.PATH = paste0(BASE.FILE.PATH, "/", version.to.try, "_", name, ".Rdata")
+    
+        if (file.exists(FILE.PATH))
+        {
+            load(file=FILE.PATH) # will be called "object"
+            return (object)
+        }
+    }
+    
+    # if we got here, we didn't find anything
+    stop(paste0("Cannot get cached '", name, "' for version '", version, 
+                "' - no object by this name has been cached for the version or any ancestor versions"))
 }
