124ce670df4f0d3e22942886da0157840315059c
Zoe Dansky <136368115+zoedansky@users.noreply.github.com>
2024-03-12 11:04:05 -0600

    pushing update

--- data_processing/nsduh_fixing_age_groups.R
+++ data_processing/nsduh_fixing_age_groups.R
@@ -1,3 +1,4 @@
+#census.manager = load.data.manager(name="census.manager", file="../../cached/census.manager.rdata")
 
 ages.of.interest.13.17 = c("13 years", "14 years", "15 years", "16 years", "17 years")
 ages.of.interest.18.25 = c("18 years", "19 years", "20 years", "21 years", "22 years", "23 years", "24 years", "25 years")
@@ -14,19 +15,27 @@ population.2017.12.17 = as.data.frame(rowSums(census.manager$data$population$est
 
 lower.age.list <- list(population.2005.12.17, population.2007.12.17, population.2009.12.17, population.2011.12.17, population.2013.12.17, population.2015.12.17, population.2017.12.17)
             
-
 aids.data.clean = lapply(lower.age.list, function(file){
   
   data=file[["data"]]
   filename = file[["filename"]]    
   
-  data$population.age.12.17 = rowSums(data)
+  #data$population.age.12.17 = rowSums(data)
 
   data = rownames_to_column(data, var = 'location')
   
   data <- data %>%
   select(location, population.age.12.17)
 
-
 list(filename, data) 
 })
+
+
+#NSDUH has national, states, and substate regions.  Census has county
+
+counties.in.states = lapply(states, function(state){
+  counties.in.this.state = locations::get.contained.locations(state, "county")
+})
+
+#Could you make a variable for eah county that corresponds to each 'state' or 'substate region' and then group_by + sum to get values for locations
+#Then join to NSDUH data
