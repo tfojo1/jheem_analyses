ae845d3fe56fbf1346e9121a505319958a865278
Zoe Dansky <zoedansky@gmail.com>
2024-03-12 16:06:51 -0400

    pushing updates

--- data_processing/nsduh_fixing_age_groups.R
+++ data_processing/nsduh_fixing_age_groups.R
@@ -1,4 +1,4 @@
-#census.manager = load.data.manager(name="census.manager", file="../../cached/census.manager.rdata")
+census.manager = load.data.manager(name="census.manager", file="../../cached/census.manager.rdata")
 
 ages.of.interest.13.17 = c("13 years", "14 years", "15 years", "16 years", "17 years")
 ages.of.interest.18.25 = c("18 years", "19 years", "20 years", "21 years", "22 years", "23 years", "24 years", "25 years")
@@ -70,36 +70,42 @@ population.2017.12.17 = as.data.frame(rowSums(census.manager$data$population$est
 ##Messy formatting bc idk how else to do this##
 #####################################################################################################################
 population.2005.12.17 <- population.2005.12.17%>%
-  rename(population.13.17 = `rowSums(census.manager$data$population$estimate$census.population$census$year__location__age__race__ethnicity__sex["2005", , ages.of.interest.13.17, , , ])`)
-population.2007.12.17 <- population.2007.12.17%>%
-  rename(population.13.17 = `rowSums(census.manager$data$population$estimate$census.population$census$year__location__age__race__ethnicity__sex["2007", , ages.of.interest.13.17, , , ])`)
+  rename(population.13.17 = `rowSums(census.manager$data$population$estimate$census.population$census$year__location__age__race__ethnicity__sex["2005", , ages.of.interest.13.17, , , ])`)%>%
+mutate(year = "2005")
+  population.2007.12.17 <- population.2007.12.17%>%
+  rename(population.13.17 = `rowSums(census.manager$data$population$estimate$census.population$census$year__location__age__race__ethnicity__sex["2007", , ages.of.interest.13.17, , , ])`)%>%
+  mutate(year = "2007")
 population.2009.12.17  <- population.2009.12.17 %>%
-  rename(population.13.17 = `rowSums(census.manager$data$population$estimate$census.population$census$year__location__age__race__ethnicity__sex["2009", , ages.of.interest.13.17, , , ])`)
+  rename(population.13.17 = `rowSums(census.manager$data$population$estimate$census.population$census$year__location__age__race__ethnicity__sex["2009", , ages.of.interest.13.17, , , ])`)%>%
+  mutate(year = "2009")
 population.2011.12.17 <- population.2011.12.17%>%
-  rename(population.13.17 = `rowSums(census.manager$data$population$estimate$census.population$census$year__location__age__race__ethnicity__sex["2011", , ages.of.interest.13.17, , , ])`)
+  rename(population.13.17 = `rowSums(census.manager$data$population$estimate$census.population$census$year__location__age__race__ethnicity__sex["2011", , ages.of.interest.13.17, , , ])`)%>%
+  mutate(year = "2011")
 population.2013.12.17 <- population.2013.12.17%>%
-  rename(population.13.17 = `rowSums(census.manager$data$population$estimate$census.population$census$year__location__age__race__ethnicity__sex["2013", , ages.of.interest.13.17, , , ])`)
+  rename(population.13.17 = `rowSums(census.manager$data$population$estimate$census.population$census$year__location__age__race__ethnicity__sex["2013", , ages.of.interest.13.17, , , ])`)%>%
+  mutate(year = "2013")
 population.2015.12.17  <- population.2015.12.17 %>%
-  rename(population.13.17 = `rowSums(census.manager$data$population$estimate$census.population$census$year__location__age__race__ethnicity__sex["2015", , ages.of.interest.13.17, , , ])`)
+  rename(population.13.17 = `rowSums(census.manager$data$population$estimate$census.population$census$year__location__age__race__ethnicity__sex["2015", , ages.of.interest.13.17, , , ])`)%>%
+  mutate(year = "2015")
 population.2017.12.17 <- population.2017.12.17%>%
-  rename(population.13.17 = `rowSums(census.manager$data$population$estimate$census.population$census$year__location__age__race__ethnicity__sex["2017", , ages.of.interest.13.17, , , ])`)
+  rename(population.13.17 = `rowSums(census.manager$data$population$estimate$census.population$census$year__location__age__race__ethnicity__sex["2017", , ages.of.interest.13.17, , , ])`)%>%
+  mutate(year = "2017")
 #####################################################################################################################
 #This creates a list of census population values for ages 12-17 by county
 
 lower.age.list <- list(population.2005.12.17, population.2007.12.17, population.2009.12.17, population.2011.12.17, population.2013.12.17, population.2015.12.17, population.2017.12.17)
             
-younger.population.by.county = lapply(lower.age.list, function(file){
+younger.population = lapply(lower.age.list, function(file){
   
   data=file[1]
   
    data= as.data.frame(data)
-   data = rownames_to_column(data, var = 'location.county')
-   
+    data = rownames_to_column(data, var = 'location.county')
+
    data$location.state.fips = substr(data$location.county, start = 1, stop = 2)
    data$location.state = state.to.fips.mappings[data$location.state.fips]
-   
-   data$test = locations::get.location.code()
 
+  
 list(data) 
 })
 
