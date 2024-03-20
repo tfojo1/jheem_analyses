7112384f5a00cfe19e52a317e79afcc7d04e5223
Melissa Schnure <mschnur3@jh.edu>
2024-03-20 15:30:05 -0400

    changes for running on remote

--- applications/EHE/calibration_runs/ehe_setup_init_calibration.R
+++ applications/EHE/calibration_runs/ehe_setup_init_calibration.R
@@ -8,33 +8,33 @@ LOCATIONS = c(# NYC.MSA, #'C.35620' # non.idu.general.mortality error
               #DALLAS.MSA, #'C.19100'
               #CHICAGO.MSA, #'C.16980' # non.idu.general.mortality error
               #DC.MSA, #'C.47900'
-              PHILADELPHIA.MSA, #'C.37980'
+              PHILADELPHIA.MSA #'C.37980'
               #ORLANDO.MSA, #'C.36740'
               #SF.MSA, #'C.41860'
               #PHOENIX.MSA, #'C.38060' # proportion.msm.of.male error
               #TAMPA.MSA, #'C.45300'
               #RIVERSIDE.MSA, #'C.40140'
               #DETROIT.MSA, #'C.19820' # "control file for the 1st chain is missing"
-              #BALTIMORE.MSA #'C.12580'
+              #BALTIMORE.MSA, #'C.12580'
               #VEGAS.MSA, #'C.29820' # proportion.msm.of.male error
               #BOSTON.MSA, #'C.14460' # non.idu.general.mortality error
               #SAN.DIEGO.MSA, #'C.41740' # proportion.msm.of.male error
               #CHARLOTTE.MSA, #'C.16740' # non.idu.general.mortality error
               #SAN.ANTONIO.MSA, #'C.41700'
               #JACKSONVILLE.MSA, #'C.27260'
-              NEW.ORLEANS.MSA, #'C.35380'
+              #NEW.ORLEANS.MSA, #'C.35380'
               #MEMPHIS.MSA, #'C.32820' # non.idu.general.mortality error
-              SEATTLE.MSA, #'C.42660'
-              AUSTIN.MSA, #'C.12420'
+              #SEATTLE.MSA, #'C.42660'
+              #AUSTIN.MSA, #'C.12420'
               #INDIANAPOLIS.MSA, #'C.26900'
               #CINCINATTI.MSA, #'C.17140' # non.idu.general.mortality error
-              COLUMBUS.MSA, #'C.18140'
+              #COLUMBUS.MSA, #'C.18140'
               #BATON.ROUGE.MSA, #'C.12940'
-              SACRAMENTO.MSA #'C.40900'
+              #SACRAMENTO.MSA #'C.40900'
               #CLEVELAND.MSA #'C.17460'
 )
 
-CALIBRATION.CODE.TO.RUN = CALIBRATION.CODE.POPULATION # or CALIBRATION.CODE.TRANSMISSION
+CALIBRATION.CODE.TO.RUN = CALIBRATION.CODE.TRANSMISSION  # or CALIBRATION.CODE.POPULATION
 
 for(location in LOCATIONS){
   clear.calibration.cache(version='ehe',
