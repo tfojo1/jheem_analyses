909804668dcfb8f802febc038110c9916fed9e63
Melissa Schnure <mschnur3@jh.edu>
2024-04-08 09:30:13 -0400

    for running on remote

--- applications/EHE/calibration_runs/ehe_process_init_calibration.R
+++ applications/EHE/calibration_runs/ehe_process_init_calibration.R
@@ -1,40 +1,43 @@
 source('../jheem_analyses/applications/EHE/calibration_runs/ehe_register_calibrations.R')
 
 LOCATIONS = c(#NYC.MSA, #'C.35620' # non.idu.general.mortality error
-              MIAMI.MSA, #'C.33100'
+              #MIAMI.MSA, #'C.33100'
               #LA.MSA, #'C.31080'
-              ATLANTA.MSA, #'C.12060'
+              #ATLANTA.MSA, #'C.12060'
               #HOUSTON.MSA, #'C.26420'
               #DALLAS.MSA, #'C.19100'
               #CHICAGO.MSA, #'C.16980' # non.idu.general.mortality error
               #DC.MSA, #'C.47900'
-              PHILADELPHIA.MSA, #'C.37980'
+              #PHILADELPHIA.MSA, #'C.37980'
               #ORLANDO.MSA, #'C.36740'
               #SF.MSA, #'C.41860'
               #PHOENIX.MSA, #'C.38060' # proportion.msm.of.male error
               #TAMPA.MSA, #'C.45300'
               #RIVERSIDE.MSA, #'C.40140'
               #DETROIT.MSA, #'C.19820' # "control file for the 1st chain is missing"
-              #BALTIMORE.MSA, #'C.12580'
+              BALTIMORE.MSA #'C.12580'
               #VEGAS.MSA, #'C.29820' # proportion.msm.of.male error
               #BOSTON.MSA, #'C.14460' # non.idu.general.mortality error
               #SAN.DIEGO.MSA, #'C.41740' # proportion.msm.of.male error
               #CHARLOTTE.MSA, #'C.16740' # non.idu.general.mortality error
               #SAN.ANTONIO.MSA, #'C.41700'
               #JACKSONVILLE.MSA, #'C.27260'
-              NEW.ORLEANS.MSA, #'C.35380'
+              #NEW.ORLEANS.MSA, #'C.35380'
               #MEMPHIS.MSA, #'C.32820' # non.idu.general.mortality error
-              SEATTLE.MSA, #'C.42660'
-              AUSTIN.MSA, #'C.12420'
+              #SEATTLE.MSA, #'C.42660'
+              #AUSTIN.MSA, #'C.12420'
               #INDIANAPOLIS.MSA, #'C.26900'
               #CINCINATTI.MSA, #'C.17140' # non.idu.general.mortality error
-              COLUMBUS.MSA, #'C.18140'
+              #COLUMBUS.MSA, #'C.18140'
               #BATON.ROUGE.MSA, #'C.12940'
-              SACRAMENTO.MSA #'C.40900'
+              #SACRAMENTO.MSA #'C.40900'
               #CLEVELAND.MSA #'C.17460'
 )
 
-CALIBRATION.CODE.TO.PROCESS = CALIBRATION.CODE.POPULATION # or CALIBRATION.CODE.POPULATION
+CALIBRATION.CODE.TO.PROCESS = CALIBRATION.CODE.FULL # CALIBRATION.CODE.POPULATION, 
+                                                    # CALIBRATION.CODE.TRANSMISSION
+                                                    # CALIBRATION.CODE.FULL
+                                                    # CALIBRATION.CODE.FULL.WITHOUT.SUPPRESSION
 
 for(location in LOCATIONS){
   mcmc = assemble.mcmc.from.calibration(version = 'ehe',
@@ -47,13 +50,14 @@ for(location in LOCATIONS){
   # Pull just the last simulation
   sim = mcmc@simulations[[length(mcmc@simulations)]]
   
-  save(sim,file=paste0("prelim_results/init.pop.migration.sim_",Sys.Date(),"_",location,".Rdata"))
-  
-  # Pull all the simulations into a simset
-  # simset = join.simulation.sets((mcmc@simulations[mcmc@simulation.indices]))
-  # simset = simset$burn(keep = 0.5)
-  # simset = simset$thin(keep = 100)
-  # 
-  # save(simset,file=paste0("prelim_results/init.transmission.SIMSET_",Sys.Date(),"_",location,".Rdata"))
+  if(CALIBRATION.CODE.TO.RUN==CALIBRATION.CODE.TRANSMISSION){
+    save(sim,file=paste0("prelim_results/init.transmission.sim_",Sys.Date(),"_",LOCATION,".Rdata"))
+  } else if(CALIBRATION.CODE.TO.RUN==CALIBRATION.CODE.POPULATION){
+    save(sim,file=paste0("prelim_results/init.pop.migration.sim_",Sys.Date(),"_",LOCATION,".Rdata"))
+  } else if(CALIBRATION.CODE.TO.RUN==CALIBRATION.CODE.FULL){
+    save(sim,file=paste0("prelim_results/init.full.sim_",Sys.Date(),"_",LOCATION,".Rdata"))
+  } else if(CALIBRATION.CODE.TO.RUN==CALIBRATION.CODE.FULL.WITHOUT.SUPPRESSION){
+    save(sim,file=paste0("prelim_results/init.full.minus.two.sim_",Sys.Date(),"_",LOCATION,".Rdata"))
+  } else stop("invalid calibration code")
 
 }
