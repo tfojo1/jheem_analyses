480604b46893c0b22476b3b8559bbde6b2750d15
Lucia Cilloni <lmcilloni@yahoo.co.uk>
2024-04-11 10:41:26 -0400

    Depression test run

--- applications/Depression/depression_test.R
+++ applications/Depression/depression_test.R
@@ -9,3 +9,20 @@ params["global.trate"] = 0.075
 
 engine=create.jheem.engine("dep", "C.12580", end.year = 2025)
 sim = engine$run(params)
+simplot(sim,"new")
+
+
+## intervention: example get 90% on depression tx
+treatment90 <- create.intervention.effect(quantity.name = 'depression.proportion.tx', start.time = 2025, 
+                                        effect.values = 0.9, 
+                                        times = 2030, apply.effects.as = 'value', scale = 'proportion',
+                                        allow.values.less.than.otherwise = F, allow.values.greater.than.otherwise = T)
+
+intdep90  <- create.intervention(treatment90, WHOLE.POPULATION, code = 'depTx90')
+sim.int   <- intdep90$run(sim,2025, 2035)
+sim.noint <- get.null.intervention()$run(sim, 2025, 2035)
+
+simplot(sim.noint, sim.int, 'incidence')
+
+sim.noint$get('incidence', year=2035)
+sim.int$get('incidence', year=2035)
