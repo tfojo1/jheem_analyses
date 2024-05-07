07ec37f12ce7e3730613b495c04c0115594baca4
Lauren Zalla <zalla@unc.edu>
2024-05-07 15:29:50 -0400

    new simset

--- applications/ehe_disparities/run.R
+++ applications/ehe_disparities/run.R
@@ -7,10 +7,36 @@ CALIBRATION.CODE=NULL
 LOCATIONS=c("C.12580","C.33100") #Baltimore & Miami
 INTERVENTIONS=c("noint","testdisp")
 
-collection=create.simset.collection(version="ehe", calibration.code = CALIBRATION.CODE, locations = LOCATIONS, interventions = INTERVENTIONS)
+collection=create.simset.collection(version="ehe", calibration.code = CALIBRATION.CODE, locations = LOCATIONS, interventions = INTERVENTIONS, n.sim=2)
 
 collection$run(2025, 2035, verbose=TRUE)
 
 results = collection$get(outcomes = c("incidence", "population"),
                          dimension.values = list(year=2035),
                          keep.dimensions = c("race"))
+
+
+
+
+source("applications/EHE/ehe_specification.R")
+source("../jheem_analyses/applications/ehe_disparities/ehe_disparities_interventions.R")
+
+CALIBRATION.CODE=NULL
+LOCATIONS=c("C.12580")
+LOCATIONS=c("C.12580","C.33100") #Baltimore & Miami
+INTERVENTIONS=c("noint","testdisp")
+
+load("../jheem_analyses/applications/ehe_disparities/simset_2024-05-06_C.12580.Rdata")
+simset$save()
+simset
+
+simplot(simset, "new", split.by="race")
+
+collection=create.simset.collection(version="ehe", calibration.code = CALIBRATION.CODE, locations = LOCATIONS, interventions = INTERVENTIONS, n.sim=50)
+
+collection$run(2025, 2035, verbose=TRUE)
+
+results = collection$get(outcomes = c("new", "population"),
+                         dimension.values = list(year=2035),
+                         keep.dimensions = c("race"))
+
