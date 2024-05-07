a54edb4516c5a38a6394a373931b07ae77a57bed
mschnure <mschnur3@jhmi.edu>
2024-05-07 16:31:00 -0400

    corrected calibration code

--- applications/ehe_disparities/run.R
+++ applications/ehe_disparities/run.R
@@ -19,18 +19,21 @@ results = collection$get(outcomes = c("incidence", "population"),
 source("applications/EHE/ehe_specification.R")
 source("../jheem_analyses/applications/ehe_disparities/ehe_disparities_interventions.R")
 
-CALIBRATION.CODE=NULL
+CALIBRATION.CODE="full.with.aids"
 LOCATIONS=c("C.12580")
-LOCATIONS=c("C.12580","C.33100") #Baltimore & Miami
-INTERVENTIONS=c("noint","testdisp")
+#LOCATIONS=c("C.12580","C.33100") #Baltimore & Miami
+INTERVENTIONS=c("noint",
+                "testdisp")
 
 load("../jheem_analyses/applications/ehe_disparities/simset_2024-05-06_C.12580.Rdata")
+#load("../jheem_analyses/prelim_results/simset_2024-05-06_C.12580.Rdata") # for Melissa to run on her computer 
 simset$save()
 simset
 
 simplot(simset, "new", split.by="race")
 
-collection=create.simset.collection(version="ehe", calibration.code = CALIBRATION.CODE, locations = LOCATIONS, interventions = INTERVENTIONS, n.sim=50)
+collection=create.simset.collection(version="ehe", calibration.code = CALIBRATION.CODE, 
+                                    locations = LOCATIONS, interventions = INTERVENTIONS, n.sim=50)
 
 collection$run(2025, 2035, verbose=TRUE)
 
