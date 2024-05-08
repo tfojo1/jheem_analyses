5b8d7322e548a55d6f6c83429d7ae5e38129f8c1
Zoe Dansky <136368115+zoedansky@users.noreply.github.com>
2024-05-08 09:10:52 -0600

    updates

--- data_processing/outliers_diagnosed_prevalence.R
+++ data_processing/outliers_diagnosed_prevalence.R
@@ -26,24 +26,26 @@ dx.prev.ehe <- run.outlier.process(outcome= 'diagnosed.prevalence',
 all.dx.prev.source1 = as.data.frame.table(surveillance.manager$data$diagnosed.prevalence$estimate$cdc.surveillance.reports$cdc.msa.reports$year__location)
 
 #Plot1- Washington, DC
-plot.one <- all.dx.prevsource1 %>% filter(location == "C.47900")
+plot.one <- all.dx.prev.source1 %>% filter(location == "C.47900")
 ggplot(data = plot.one)+                   
   geom_point(                             
     mapping = aes(x = year, y = Freq),    
     color = "turquoise4")+                       
   labs()+                                 
   theme()+
-  ylim(0, NA)
+  ylim(0, NA)+
+  ggtitle("Diagnosed Prevalence, Washington, DC (C.47900), cdc.surveillance.reports")
 
 #Plot2- Riverside-San Bernardino, CA
-plot.two <- all.dx.prevsource1 %>% filter(location == "C.40140")
+plot.two <- all.dx.prev.source1 %>% filter(location == "C.40140")
 ggplot(data = plot.two)+                   
   geom_point(                             
     mapping = aes(x = year, y = Freq),    
     color = "turquoise4")+                       
   labs()+                                 
   theme()+
-  ylim(0, NA)
+  ylim(0, NA)+
+  ggtitle("Diagnosed Prevalence, Riverside-San Bernardino, CA (C.40140), cdc.surveillance.reports")
 
 
 #Source = cdc.aggregated.county
@@ -75,8 +77,8 @@ ggplot(data = plot.three)+
 dx.prev.ehe <- run.outlier.process(outcome= 'diagnosed.prevalence',
                                    stratifications= list(c()), 
                                    data.manager= surveillance.manager,
-                                   phi=0.2, #default is phi=0.15 (percent change from year to year)
-                                   theta=0.08, #default is theta=0.05 (a multiplier that produces a percent change)
+                                   phi=0.3, #default is phi=0.15 (percent change from year to year)
+                                   theta=0.1, #default is theta=0.05 (a multiplier that produces a percent change)
                                    locations= c(EHE.MSAS))
 
 #Here I adjusted the values with gives 3 total outliers- but looking at them I don't think they
