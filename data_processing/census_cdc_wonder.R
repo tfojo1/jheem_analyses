ce8007fa71f89da268e1485148eef83efbf19105
Zoe Dansky <zoedansky@gmail.com>
2024-05-16 15:30:48 -0400

    updating dummy data to have NAs at every level of stratification

--- data_processing/census_cdc_wonder.R
+++ data_processing/census_cdc_wonder.R
@@ -72,22 +72,107 @@ for (data in county_single_year_age) {
 #we decided to put them here as NA so then we won't run into the dimnames
 #related error
 
-dummy.counties  <- data.frame(
+# need 9 data frames -----------------------------------------------------
+
+#year_location
+dummy.counties.one<- data.frame(
+  outcome = c('population'),
+  year = c('2018'),
+  location = c('12025', '51560', '51780', '51123', '29193'), #These are the counties that have changed#
+  value = as.numeric(NA)
+)
+#year_location_age
+dummy.counties.two <- data.frame(
+  outcome = c('population'),
+  year = c('2018'),
+  location = c('12025', '51560', '51780', '51123', '29193'), #These are the counties that have changed#
+  value = as.numeric(NA),
+  age = c('< 1 year')
+)
+#year_location_race
+dummy.counties.three <- data.frame(
+  outcome = c('population'),
+  year = c('2018'),
+  location = c('12025', '51560', '51780', '51123', '29193'), #These are the counties that have changed#
+  value = as.numeric(NA),
+  race = c('American Indian or Alaska Native')
+)
+#year_location_ethnicity
+dummy.counties.four  <- data.frame(
+  outcome = c('population'),
+  year = c('2018'),
+  location = c('12025', '51560', '51780', '51123', '29193'), #These are the counties that have changed#
+  value = as.numeric(NA),
+  ethnicity = c('Hispanic or Latino')
+)
+
+#year_location_race_ethnicity
+dummy.counties.five  <- data.frame(
+  outcome = c('population'),
+  year = c('2018'),
+  location = c('12025', '51560', '51780', '51123', '29193'), #These are the counties that have changed#
+  value = as.numeric(NA),
+  race = c('American Indian or Alaska Native'),
+  ethnicity = c('Hispanic or Latino')
+)
+#year_location_sex
+dummy.counties.six <- data.frame(
+  outcome = c('population'),
+  year = c('2018'),
+  location = c('12025', '51560', '51780', '51123', '29193'), #These are the counties that have changed#
+  value = as.numeric(NA),
+  sex = c('male')
+)
+#year_location_age_race_ethnicity
+dummy.counties.seven  <- data.frame(
+  outcome = c('population'),
+  year = c('2018'),
+  location = c('12025', '51560', '51780', '51123', '29193'), #These are the counties that have changed#
+  value = as.numeric(NA),
+  race = c('American Indian or Alaska Native'),
+  ethnicity = c('Hispanic or Latino'),
+  age = c('< 1 year')
+)
+#year_location_age_sex
+dummy.counties.eight  <- data.frame(
+  outcome = c('population'),
+  year = c('2018'),
+  location = c('12025', '51560', '51780', '51123', '29193'), #These are the counties that have changed#
+  value = as.numeric(NA),
+  sex = c('male'),
+  age = c('< 1 year')
+)
+#year_location_race_ethnicity_sex
+dummy.counties.nine  <- data.frame(
   outcome = c('population'),
   year = c('2018'),
   location = c('12025', '51560', '51780', '51123', '29193'), #These are the counties that have changed#
   value = as.numeric(NA),
   sex = c('male'),
   race = c('American Indian or Alaska Native'),
-  ethnicity = c('Hispanic or Latino'),
-  age = c('< 1 year')
+  ethnicity = c('Hispanic or Latino')
 )
 
+#Put into list and put into data manage
+
+dummy.data.all = list (dummy.counties.one, 
+                       dummy.counties.two,
+                       dummy.counties.three,
+                       dummy.counties.four,
+                       dummy.counties.five,
+                       dummy.counties.six,
+                       dummy.counties.seven,
+                       dummy.counties.eight,
+                       dummy.counties.nine)
+
+for (data in dummy.data.all) {
+  
 census.manager$put.long.form(
-  data = dummy.counties,
+  data = data,
   ontology.name = 'census.cdc.wonder.population',
   source = 'cdc_wonder',
   dimension.values = list(),
   url ='https://wonder.cdc.gov/',
   details = 'CDC Wonder',
   allow.na.to.overwrite = T )
+}
