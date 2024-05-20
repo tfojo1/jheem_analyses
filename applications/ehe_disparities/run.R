d3f34822722cc6417979b33e1f82f1b7c9b921f4
Lauren Zalla <zalla@unc.edu>
2024-05-20 18:33:42 -0400

    add simset for Atlanta

--- applications/ehe_disparities/run.R
+++ applications/ehe_disparities/run.R
@@ -20,12 +20,15 @@ source("applications/EHE/ehe_specification.R")
 source("../jheem_analyses/applications/ehe_disparities/ehe_disparities_interventions.R")
 
 CALIBRATION.CODE= "init.transmission.ehe" # "full.with.aids" or "init.transmission.ehe"
-LOCATIONS=c("C.12580")
-#LOCATIONS=c("C.12580","C.33100", "C.XXXXX") #Baltimore, Miami & Atlanta
+LOCATIONS=c("C.12580","C.12060") #Baltimore & Atlanta
+#LOCATIONS=c("C.12580","C.12060","C.33100") #Baltimore, Atlanta & Miami
 INTERVENTIONS=c("noint",
                 "testdisp")
 
+noint=get.null.intervention()
+
 load("../jheem_analyses/applications/ehe_disparities/simset_2024-05-08_C.12580.Rdata")
+load("../jheem_analyses/applications/ehe_disparities/simset_2024-05-19_C.12060.Rdata")
 #load("../jheem_analyses/prelim_results/init.transmission.ehe_simset_2024-05-08_C.12580.Rdata") # for Melissa 
 #rerun.simulations(simset) # this line takes ~5 min to run; don't need to run this on the "init.transmission.ehe" one 
 simset$save()
