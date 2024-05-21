f3e6f78f40ea48502d5a5faff1d9aa87220c5946
Zoe Dansky <136368115+zoedansky@users.noreply.github.com>
2024-05-21 14:08:35 -0600

    correcting values

--- data_processing/outliers/adjusting_theta_phi.R
+++ data_processing/outliers/adjusting_theta_phi.R
@@ -1,4 +1,4 @@
-#May 9th- Adjust values of theta and phi for each outcome to determine best set up for finding true outliers
+#Adjust values of theta and phi for each outcome to determine best set up for finding true outliers
 
 #outcomes = diagnosed.prevalence; diagnoses; hiv.deaths
 #stratification = total
@@ -7,96 +7,82 @@
 #phi: a percent change from one year to another (default value is 0.15)
 #theta: a multiplier that produces a percent change based on how many years apart the two samples are (it has a maximum value) (default value is 0.05)
 
-#Questions: how do I plan this across sources, strata, locations?
-#Do I need to review tens of obs?
-
 #True = it is an outlier
 # False = the data point is fine as is
 
 
+# Source outlier finder ---------------------------------------------------
+
+surveillance.manager = load.data.manager(name="surveillance.manager", file="../../cached/surveillance.manager.rdata")
+source('data_processing/outlier_finder.R')
+source('commoncode/locations_of_interest.R')
+source('commoncode/additional_locations_of_interest.R')
+
+#will Need to fix these after talking to Andrew
+national = "US"
+states = c(state.abb)
+MSAs = MSAS.OF.INTEREST
+counties = unlist(COUNTIES.OF.INTEREST) #why does this work
+substate.region = locations::get.all.for.type("NSDUH")
+  
 # diagnoses ---------------------------------------------------------------
 
-diagnoses.original <- run.outlier.process(outcome= 'diagnoses',
-                                          stratifications= list(c()), 
-                                          data.manager= surveillance.manager,
-                                          locations= c(EHE.MSAS))
-
 diagnoses.adjusted <- run.outlier.process(outcome= 'diagnoses',
-                                              stratifications= list(c()), 
+                                              stratifications= list(c()), #but there's more strata
                                               data.manager= surveillance.manager,
                                               phi = 0.3,
                                               theta = 0.1,
-                                              locations= c(EHE.MSAS))
+                                              locations= c(national, states, counties, MSAs))
 
 #Use diagnoses.adjusted; removing 2 outliers (DC + Baltimore)
-diagnoses.adjusted$adjudication <- c('TRUE', 'TRUE')
-
-run.outlier.process(outcome= 'diagnoses',
-                    stratifications= list(c()),
-                    data.manager= surveillance.manager,
-                    locations= EHE.MSAS,
-                    adjudication.data.frame = diagnoses.adjusted)
+# diagnoses.adjusted$adjudication <- c('TRUE', 'TRUE')
+# 
+# run.outlier.process(outcome= 'diagnoses',
+#                     stratifications= list(c()),
+#                     data.manager= surveillance.manager,
+#                     locations= EHE.MSAS,
+#                     adjudication.data.frame = diagnoses.adjusted)
 
 
 # diagnosed.prevalence ----------------------------------------------------
 
-
-diagnosed.prevalence.original <- run.outlier.process(outcome= 'diagnosed.prevalence',
-                                    stratifications= list(c()), 
-                                    data.manager= surveillance.manager,
-                                    locations= c(EHE.MSAS))
-
 diagnosed.prevalence.adjusted <- run.outlier.process(outcome= 'diagnosed.prevalence',
                                           stratifications= list(c()), 
                                           data.manager= surveillance.manager,
-                                          phi = 0.2,
-                                          theta = 0.05,
-                                          locations= c(EHE.MSAS))
+                                          phi = 0.6, #this just seems like a huge value
+                                          theta = 0.4,
+                                          locations= c(national, states, counties, MSAs))
 
 #using the adjusted dataframe- this removes 10 values
-diagnosed.prevalence.adjusted$adjudication <- replace(diagnosed.prevalence.adjusted$adjudication, 1:10, 'TRUE')
-
-run.outlier.process(outcome= 'diagnosed.prevalence',
-                    stratifications= list(c()),
-                    data.manager= surveillance.manager,
-                    locations= EHE.MSAS,
-                    adjudication.data.frame = diagnosed.prevalence.adjusted)
+# diagnosed.prevalence.adjusted$adjudication <- replace(diagnosed.prevalence.adjusted$adjudication, 1:10, 'TRUE')
+# 
+# run.outlier.process(outcome= 'diagnosed.prevalence',
+#                     stratifications= list(c()),
+#                     data.manager= surveillance.manager,
+#                     locations= EHE.MSAS,
+#                     adjudication.data.frame = diagnosed.prevalence.adjusted)
 
 
 # hiv.deaths --------------------------------------------------------------
-#I don't know how to assess these- it's like there's data for 2019 and 20212
-hiv.deaths.original <- run.outlier.process(outcome= 'hiv.deaths',
-                                                     stratifications= list(c()), 
-                                                     data.manager= surveillance.manager,
-                                                     locations= c(EHE.MSAS))
-
 hiv.deaths.adjusted <- run.outlier.process(outcome= 'hiv.deaths',
                                                      stratifications= list(c()), 
                                                      data.manager= surveillance.manager,
-                                                     phi = 0.2,
-                                                     theta = 0.05,
-                                                     locations= c(EHE.MSAS))
+                                                     phi = 0.3,
+                                                     theta = 0.1,
+                                                     locations= c(states, MSAs))
 
 
-#Adding this to compare to other hiv death data:
-# hiv.deaths.to.graph.one = as.data.frame.table(surveillance.manager$data$hiv.deaths$estimate$cdc.hiv$cdc$year__location)
-# hiv.deaths.to.graph.two = as.data.frame.table(surveillance.manager$data$hiv.deaths$estimate$cdc.surveillance.reports$cdc.msa.reports$year__location__sex)
-# 
-# 
-# new <- hiv.deaths.to.graph.two%>%
-#   group_by(year, location)%>%
-#   mutate(sum = sum(Freq))%>%
-#   select(-sex, -Freq)%>%
-#   rename(Freq= sum)
-# 
-# combo = rbind(new, hiv.deaths.to.graph.one)
-# 
-# 
-# graph <- combo %>% filter(location == "C.29820")
-# ggplot(data = graph)+                   
-#   geom_point(                             
-#     mapping = aes(x = year, y = Freq),    
-#     color = "purple")+                       
-#   labs()+                                 
-#   theme()+
-  ylim(0, NA)
+
+# Gonorrhea -----------------------------------------------------------------
+
+#This is an example - i started with a lot of GC outliers, adjusted values to get 1 at total level, but then at strata that becomes 110 for race, 18 for age
+#Like how do I do this without making the values absurd?
+
+gonorrhea.adjusted <- run.outlier.process(outcome= 'gonorrhea',
+                                           stratifications= list(c()), 
+                                           data.manager= surveillance.manager,
+                                           phi = 0.5,
+                                           theta = 0.1,
+                                           locations= c(states))
+
+
