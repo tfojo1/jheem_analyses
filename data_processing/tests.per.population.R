88e491e128dc6fb2ea5d987ae5ac955f64315477
Zoe Dansky <136368115+zoedansky@users.noreply.github.com>
2024-06-05 15:41:18 -0600

    adding cities to hiv.tests.per.population

--- data_processing/tests.per.population.R
+++ data_processing/tests.per.population.R
@@ -57,3 +57,46 @@ tests.per.pop <- combined %>%
     url = 'https://www.cdc.gov/hiv/library/reports/testing/2021/index.html#:~:text=Among%20the%201%2C736%2C850%20CDC%2Dfunded,demographic%20characteristics%20and%20population%20groups.',
     details = 'CDC Annual HIV Testing Report')
 
+  
+
+# Adding in Cities --------------------------------------------------------
+
+  #Add cities to hiv.tests.per.population
+  
+  cities <- c("C.12060", "C.12580", "C.16980" ,"C.26420", "C.31080", "C.35620", "C.37980", "C.41860")
+  
+  #hiv.tests
+  cities.numerator = as.data.frame.table(surveillance.manager$data$hiv.tests$estimate$cdc.testing$cdc$year__location[, cities]) %>% rename(hiv.test.value = Freq)%>% filter(hiv.test.value != "NaN") #2011-2021
+  
+  
+  #Adult.pop
+  denominator.cities.1 = as.data.frame.table(surveillance.manager$data$adult.population$estimate$census.population$census$year__location[, cities]) %>% rename(adult.population.value = Freq)
+  denominator.cities.2 = as.data.frame.table(surveillance.manager$data$adult.population$estimate$cdc_wonder$census.cdc.wonder.population$year__location[, cities]) %>% rename(adult.population.value = Freq)
+  
+  denominator = rbind(denominator.cities.1, denominator.cities.2)
+  
+  final.denominator <- denominator %>%
+    filter(year != "2005")%>%
+    filter(year != "2006")%>%
+    filter(year != "2007")%>%
+    filter(year != "2008")%>%
+    filter(year != "2009")%>%
+    filter(year != "2010")%>%
+    filter(adult.population.value != "NaN")
+  
+  #combine
+  combined.cities = left_join(cities.numerator, final.denominator, join_by(location, year))%>% filter(!is.na(adult.population.value))
+  
+  combined.cities <- combined.cities %>%
+    mutate(value = (hiv.test.value/adult.population.value))%>%
+    select(year, location, value)%>%
+    mutate(outcome = 'hiv.tests.per.population')
+  
+  #Put into data manager (but at this point surveillance manager)
+  surveillance.manager$put.long.form(
+    data = combined.cities,
+    ontology.name = 'cdc',
+    source = 'cdc.testing',
+    dimension.values = list(),
+    url = 'https://www.cdc.gov/hiv/library/reports/testing/2021/index.html#:~:text=Among%20the%201%2C736%2C850%20CDC%2Dfunded,demographic%20characteristics%20and%20population%20groups.',
+    details = 'CDC Annual HIV Testing Report')  
