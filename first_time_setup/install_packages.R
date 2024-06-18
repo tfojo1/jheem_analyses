96aec19ced5347d2d96a7bc6aaf530f66701ed1d
Todd Fojo <tfojo1@jh.edu>
2024-06-18 13:24:47 -0400

    updates to init set up

--- first_time_setup/install_packages.R
+++ first_time_setup/install_packages.R
@@ -1,25 +1,29 @@
 
 
-library(devtools)
 
 # Packages required for our custom packages
 if (nchar(system.file(package='ggmap'))==0)
   install.packages('ggmap')
 
 # Our Custom Packages
-install_github('tfojo1/distributions')
-install_github('tfojo1/bayesian.simulations')
-install_github('tfojo1/locations')
+devtools::install_github('tfojo1/distributions')
+devtools::install_github('tfojo1/bayesian.simulations')
+devtools::install_github('tfojo1/locations')
 
 # JULIA
-install.packages('JuliaCall')
-julia_setup(installJulia = TRUE)
-install.packages('diffeqr')
+# install.packages('JuliaCall')
+# julia_setup(installJulia = TRUE)
+# install.packages('diffeqr')
 # On a mac - navigate to the directory where Julia is installed, right click on Julia icon, select open, and override Mac safety to allow it to open
 
 
 
 # Other packages required for analyses
+
+
+devtools::install_github('thk686/odeintr')
+
+
 
 # Archival packages - I hope to eventually remove these
 #install_github('tfojo1/jheem')
--- initial_installation.R
+++
@@ -1,8 +0,0 @@
-
-# Need to have devtools package installed
-
-install_github("tfojo1/distributions")
-install_github("tfojo1/bayesian.simulations")
-install_github("tfojo1/locations")
-
-install_github("tfojo1/jheem2")
