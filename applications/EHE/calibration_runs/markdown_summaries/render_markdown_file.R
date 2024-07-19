ad74802888a5a85f5b8fb297499fb5269a843fc2
mschnure <mschnur3@jhmi.edu>
2024-07-19 09:27:40 -0400

    updates to markdown files

--- applications/EHE/calibration_runs/markdown_summaries/render_markdown_file.R
+++ applications/EHE/calibration_runs/markdown_summaries/render_markdown_file.R
@@ -1,8 +1,14 @@
 library(stringr)
 
-## This is the only line you need to change before sourcing this file to create an R Markdown summary 
-#file = "prelim_results/full.with.covid_simset_2024-07-15_C.12580.Rdata"
- file = "prelim_results/init.transmission.ehe_simset_2024-07-12_C.12580.Rdata"
+## These are the only two lines you need to change before sourcing this file to create an R Markdown summary. 
+file = "prelim_results/full.with.covid_simset_2024-07-17_C.12580.Rdata"
+notes = "In this calibration, I added testing slopes by age and race (+6 parameters)."
+
+# file = "prelim_results/full.with.covid_simset_2024-07-15_C.12580.Rdata"
+# notes = "In this calibration, we made age4 and age5 msm susceptibility time-varying."
+
+# file = "prelim_results/init.transmission.ehe_simset_2024-07-12_C.12580.Rdata"
+# notes = ""
 
 load(file)
 if(grepl("transmission",simset$calibration.code)){
@@ -21,8 +27,9 @@ render_subject <- function(calibration,
     input = "applications/EHE/calibration_runs/markdown_summaries/MCMC_summary.Rmd",
     params = list(set_title = calibration,
                   file = file,
-                  file.date = file.date),
-    output_file = paste0(calibration, '_output_', Sys.Date(), '.html')
+                  file.date = file.date,
+                  notes = notes),
+    output_file = paste0('outputs/',calibration, '_output_', Sys.Date(), '.html')
   )
 }
 
