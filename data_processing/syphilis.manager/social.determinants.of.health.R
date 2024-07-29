b023a46c22f0f0537bc8b5366c76c09e09193ed9
Zoe Dansky <136368115+zoedansky@users.noreply.github.com>
2024-07-29 14:47:54 -0600

    Updating ontologies and SDH data

---
+++ data_processing/syphilis.manager/social.determinants.of.health.R
@@ -0,0 +1,86 @@
+options(error=NULL)
+
+# Read in SDH Data --------------------------------------------------------
+
+DATA.DIR.SDH="../../data_raw/syphilis.manager/sdh"
+
+sdh_files <- Sys.glob(paste0(DATA.DIR.SDH, '/*.csv'))
+
+data.list.sdh <- lapply(sdh_files, function(x){
+  skip=7
+  list(filename=x, data=read.csv(x, skip=skip, header=TRUE, colClasses=c(FIPS="character")))
+})
+
+
+# SHD Outcome Mappings ----------------------------------------------------
+
+shd.mappings = c('Uninsured'='uninsured',
+                     'Vacant housing' = 'vacant.housing',
+                     'Population 25 years and older w/o HS diploma' = 'not.highschool.graduate',
+                     'Households living below the federal poverty level' = 'below.fpl',
+                     'Population living in a rural area' = 'rural.area',
+                     'Urbanization level' = 'urbanization.level')
+
+
+year.mappings = c('5-year estimates (2018-2022)'='2018-2022',
+                 '2010' = '2010',
+                 '2013' = '2013', 
+                 '2020 (COVID-19 Pandemic)' = '2020')
+
+
+# Cleaning ----------------------------------------------------------------
+
+shd.clean  = lapply(data.list.sdh, function(file){
+  
+  data=file[["data"]]
+  filename = file[["filename"]]
+
+  data$outcome = shd.mappings[data$Indicator]
+  data = data[!is.na(data$outcome),]
+  data$location = as.character(data$FIPS)
+  data$Year = as.character(data$Year)
+  data$year = year.mappings[data$Year]
+  
+  # data$Cases[data$Cases %in% c("Data suppressed")] = NA    
+  # data$Cases[data$Cases %in% c("Data not available")] = NA  
+  # data$value = as.numeric(gsub(",", '', data$Cases))   
+  
+  
+  #Outcome conditionals#
+   if(grepl("prop", filename)) {
+     data = subset(data, !is.na(data$Percent)) #Need to remove Percent values that are character values
+     data = subset(data, data$Percent != "Data not available")
+     data$formatted.percent = as.numeric(data$Percent)
+     data$value = (data$formatted.percent/100)
+   }
+  
+  if(grepl("urbanization", filename)) {     #This is categorical- need to figure out what to do with it
+    data$value = data$Urbanization.Level
+  }
+
+   data <- data %>%
+     select(outcome, year, location, value)
+  
+  data= as.data.frame(data)
+  
+  list(filename, data) 
+  
+})
+
+
+# Put SHD Data ------------------------------------------------------------
+
+##Update once you know how to structure the ontologoes#
+
+# prep_atlas_all = lapply(data.list.clean.atlas.prep, `[[`, 2)  
+# 
+# for (data in prep_atlas_all) {
+#   
+#   data.manager$put.long.form(
+#     data = data,
+#     ontology.name = 'cdc',
+#     source = 'cdc.prep',
+#     dimension.values = list(),
+#     url = 'https://www.cdc.gov/nchhstp/atlas/index.htm',
+#     details = 'CDC Atlas Plus data')
+# }
--- data_processing/syphilis.manager/syphilis.manager.processing.main.R
+++ data_processing/syphilis.manager/syphilis.manager.processing.main.R
@@ -7,41 +7,85 @@ library(tidyverse)
 
 # Create Syphilis Manager -------------------------------------------------
 
-
-
-data.manager = create.data.manager('surveillance', description='surveillance data manager')
+data.manager = create.data.manager('syphilis', description='syphilis data manager')
 
 data.manager$register.outcome(
-  'adult.population',
+  'uninsured',
   metadata = create.outcome.metadata(
     scale = 'non.negative.number',
-    display.name = 'Adult Population',
-    axis.name = 'Adult Population',
-    units = 'population',
-    description = "Adult Population Estimate, Ages 13 and over"))
+    display.name = 'Uninsured',
+    axis.name = 'Uninsured',
+    units = 'count',
+    description = "Uninsured"))
+
+data.manager$register.outcome(
+  'vacant.housing',
+  metadata = create.outcome.metadata(
+    scale = 'non.negative.number',
+    display.name = 'Vacant Housing Units',
+    axis.name = 'Vacant Housing Units',
+    units = 'count',
+    description = "Vacant Housing Units"))
+
+data.manager$register.outcome(
+  'not.highschool.graduate',
+  metadata = create.outcome.metadata(
+    scale = 'non.negative.number',
+    display.name = 'Population 25 years and older without HS diploma',
+    axis.name = 'Population 25 years and older without HS diploma',
+    units = 'count',
+    description = "Population 25 years and older without HS diploma"))
+
+data.manager$register.outcome(
+  'below.fpl',
+  metadata = create.outcome.metadata(
+    scale = 'non.negative.number',
+    display.name = 'Households living below the federal poverty level',
+    axis.name = 'Households living below the federal poverty level',
+    units = 'count',
+    description = "Households living below the federal poverty level"))
+
+data.manager$register.outcome(
+  'rural.area',
+  metadata = create.outcome.metadata(
+    scale = 'non.negative.number',
+    display.name = 'Population living in a rural area',
+    axis.name = 'Population living in a rural area',
+    units = 'count',
+    description = "Population living in a rural area"))
+
+data.manager$register.outcome(
+  'urbanization.level',
+  metadata = create.outcome.metadata(
+    scale = 'non.negative.number',
+    display.name = 'Urbanization level: rural, suburban, urban',
+    axis.name = 'Urbanization level: rural, suburban, urban',
+    units = 'count',
+    description = "Urbanization level: rural, suburban, urban"))
+
 
 
 # Create Sources + Parent Sources -----------------------------------------
 
 
-data.manager$register.parent.source('IQVIA', full.name = 'IQVIA', short.name= "IQVIA")
-
-data.manager$register.source('aidsvu', parent.source= "IQVIA", full.name = "AIDS Vu", short.name='aidsvu')
+# data.manager$register.parent.source('IQVIA', full.name = 'IQVIA', short.name= "IQVIA")
+# 
+# data.manager$register.source('aidsvu', parent.source= "IQVIA", full.name = "AIDS Vu", short.name='aidsvu')
 
 
 # Establish Ontologies ----------------------------------------------------
 
 
-data.manager$register.ontology(
-  'cdc',
-  ont = ontology(
-    year= NULL,
-    location= NULL,
-    age=c('13-24 years', '25-34 years', '35-44 years', '45-54 years','55+ years'),
-    race=c('American Indian/Alaska Native', 'Asian', 'Black/African American', 'Hispanic/Latino', 'Multiracial', 'Native Hawaiian/Other Pacific Islander', 'White'),
-    sex=c('male','female'),
-    risk=c('msm','idu','msm_idu','heterosexual','other')
-  ))
+# data.manager$register.ontology(
+#   'cdc',
+#   ont = ontology(
+#     year= NULL,
+#     location= NULL,
+#     age=c('13-24 years', '25-34 years', '35-44 years', '45-54 years','55+ years'),
+#     race=c('American Indian/Alaska Native', 'Asian', 'Black/African American', 'Hispanic/Latino', 'Multiracial', 'Native Hawaiian/Other Pacific Islander', 'White'),
+#     sex=c('male','female'),
+#     risk=c('msm','idu','msm_idu','heterosexual','other')
+#   ))
 
 
 # Source Data Cleaning and Processing Files -------------------------------
