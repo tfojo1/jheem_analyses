9654c7fba602a5e5a8dcfb8ccd1b462e60c18096
Zoe Dansky <136368115+zoedansky@users.noreply.github.com>
2024-08-06 15:39:36 -0600

    updating for sdh denominator values

--- data_processing/syphilis.manager/social.determinants.of.health.R
+++ data_processing/syphilis.manager/social.determinants.of.health.R
@@ -11,26 +11,22 @@ data.list.sdh <- lapply(sdh_files, function(x){
   list(filename=x, data=read.csv(x, skip=skip, header=TRUE, colClasses=c(FIPS="character")))
 })
 
-
 # SHD Outcome Mappings ----------------------------------------------------
 
 shd.mappings = c('Uninsured'='uninsured',
                      'Vacant housing' = 'vacant.housing',
                      'Population 25 years and older w/o HS diploma' = 'not.highschool.graduate',
                      'Households living below the federal poverty level' = 'below.fpl',
-                     'Population living in a rural area' = 'rural.area',
-                     'Urbanization level' = 'urbanization.level')
-
+                     'Population living in a rural area' = 'rural.area')
 
 year.mappings = c('5-year estimates (2018-2022)'='2018-2022',
                  '2010' = '2010',
                  '2013' = '2013', 
                  '2020 (COVID-19 Pandemic)' = '2020')
 
+# Cleaning (proportions) ----------------------------------------------------------------
 
-# Cleaning ----------------------------------------------------------------
-
-shd.clean  = lapply(data.list.sdh, function(file){
+sdh.clean  = lapply(data.list.sdh, function(file){
   
   data=file[["data"]]
   filename = file[["filename"]]
@@ -49,19 +45,33 @@ shd.clean  = lapply(data.list.sdh, function(file){
      data$value = (data$formatted.percent/100)
    }
   
-  if(grepl("urbanization", filename)) {     #This is categorical- need to figure out what to do with it
-    data$value = data$Urbanization.Level
-  }
-
    data <- data %>%
-     select(outcome, year, location, value)
-  
+     select(outcome, year, location, value, Denominator)
+
   data= as.data.frame(data)
   
   list(filename, data) 
   
 })
 
+
+# Put denominators --------------------------------------------------------
+sdh.denominators = lapply(sdh.clean, function(file){
+  
+  data=file[[2]]
+  filename = file[[1]]
+  
+  data <- data %>%
+    select(-value)%>%
+    mutate(value = as.numeric(gsub(',', '', Denominator)))%>%
+    mutate(outcome = paste( outcome, 'denominator', sep = '.'))%>%
+    select(outcome, year, location, value)
+  
+data= as.data.frame(data)
+
+list(filename, data) 
+
+})
 
 # Put SHD Data ------------------------------------------------------------
 
