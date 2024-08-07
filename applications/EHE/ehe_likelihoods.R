33fafc9fe6981cb0bbc4695a7255818d5e652e24
mschnure <mschnur3@jhmi.edu>
2024-08-07 15:05:40 -0400

    population likelihood with weights by year

--- applications/EHE/ehe_likelihoods.R
+++ applications/EHE/ehe_likelihoods.R
@@ -95,7 +95,9 @@ population.likelihood.instructions =
                                        # downweight because large population size; 
                                        # can get more specific with create.likelihood.weights 
                                        #(e.g., different weight for age X)
-                                       weights = 1, 
+                                       weights = list(create.likelihood.weights(
+                                         total.weight = 0.5,
+                                         dimension.values = list(year = as.character(2007:2014)))), 
                                        
                                        # if there are more datapoints for certain years, this will normalize
                                        # e.g., if there are a few years with only the totals 
@@ -103,35 +105,23 @@ population.likelihood.instructions =
                                        equalize.weight.by.year = F
   )
 
-population.heavy.likelihood.instructions = 
-  create.basic.likelihood.instructions(outcome.for.data = "adult.population", 
-                                       outcome.for.sim = "population",
-                                       dimensions = c("age","sex","race"),
-                                       levels.of.stratification = c(0,1,2), # 0 = totals, 1 = 1-way stratification
-                                       from.year = 2007,
-                                       correlation.different.years = 0.5, # this is the default
-                                       correlation.different.strata = 0.1, # this is the default
-                                       correlation.different.sources = 0.3, # default
-                                       correlation.same.source.different.details = 0.3, # default
-                                       
-                                       # assumes correlation between all combos of years is the same
-                                       observation.correlation.form = 'compound.symmetry', 
-                                       
-                                       # should always be specified; describes how precise the estimates are; 
-                                       # e.g., estimates can be off by 3% each year
-                                       error.variance.term = 0.03,
-                                       error.variance.type = 'cv',
-                                       
-                                       # downweight because large population size; 
-                                       # can get more specific with create.likelihood.weights 
-                                       #(e.g., different weight for age X)
-                                       weights = 2, 
-                                       
-                                       # if there are more datapoints for certain years, this will normalize
-                                       # e.g., if there are a few years with only the totals 
-                                       # before the stratifications are available
-                                       equalize.weight.by.year = F 
-  )
+# population.heavy.likelihood.instructions = 
+#   create.basic.likelihood.instructions(outcome.for.data = "adult.population", 
+#                                        outcome.for.sim = "population",
+#                                        dimensions = c("age","sex","race"),
+#                                        levels.of.stratification = c(0,1,2), # 0 = totals, 1 = 1-way stratification
+#                                        from.year = 2007,
+#                                        correlation.different.years = 0.5, # this is the default
+#                                        correlation.different.strata = 0.1, # this is the default
+#                                        correlation.different.sources = 0.3, # default
+#                                        correlation.same.source.different.details = 0.3, # default
+#                                        observation.correlation.form = 'compound.symmetry', 
+#                                        error.variance.term = 0.03,
+#                                        error.variance.type = 'cv',
+#                                        
+#                                        weights = 2, 
+#                                        equalize.weight.by.year = F 
+#   )
 
 #-- IMMIGRATION  --#
 immigration.likelihood.instructions = 
@@ -852,7 +842,7 @@ race.ps.syphilis.year.on.year.change.likelihood.instructions =
 
 #-- JOIN THE POPULATION-RELATED LIKELIHOODS  --#
 joint.pop.migration.total.trans.likelihood.instructions = 
-  join.likelihood.instructions(population.heavy.likelihood.instructions,
+  join.likelihood.instructions(population.likelihood.instructions,
                                immigration.likelihood.instructions,
                                general.mortality.likelihood.instructions,
                                emigration.likelihood.instructions,
@@ -860,13 +850,13 @@ joint.pop.migration.total.trans.likelihood.instructions =
                                total.new.diagnoses.likelihood.instructions)
 
 # joint.pop.migration.likelihood.instructions = 
-#   join.likelihood.instructions(population.heavy.likelihood.instructions,
+#   join.likelihood.instructions(population.likelihood.instructions,
 #                                immigration.likelihood.instructions,
 #                                emigration.likelihood.instructions,
 #                                general.mortality.likelihood.instructions)
 
 # joint.pop.migration.total.trans.aids.likelihood.instructions = 
-#   join.likelihood.instructions(population.heavy.likelihood.instructions,
+#   join.likelihood.instructions(population.likelihood.instructions,
 #                                immigration.likelihood.instructions,
 #                                emigration.likelihood.instructions,
 #                                general.mortality.likelihood.instructions,
