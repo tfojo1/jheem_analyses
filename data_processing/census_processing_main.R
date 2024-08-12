5cfde262ca46805b49492e01e7dcdd2b18537dc1
Zoe Dansky <zoedansky@gmail.com>
2024-08-12 16:05:23 -0400

    sourcing population 2010-2019

--- data_processing/census_processing_main.R
+++ data_processing/census_processing_main.R
@@ -180,13 +180,13 @@ source('data_processing/births_and_deaths.R')
 #This pulls the stratified census data by county for 2020-2022
 #source('data_processing/stratified_census.R')
 
-#UPDATE 7-16: Temporarily commenting out 181 to source the newer stratified data
-#I'll use this to decide if we want age groups or single year
-source('data_processing/census.population.20.23.R')
-
 #Sourcing more recent census population data that is stratified but not
 #by single year age just by age group which is why we added it so much later
 source('data_processing/census.population.10.19.R')
+
+#UPDATE 7-16: Temporarily commenting out 181 to source the newer stratified data
+#I'll use this to decide if we want age groups or single year
+source('data_processing/census.population.20.23.R')
 
 ################################################################################
           ###COUNTY POPULATION ESTIMATES 2000-2022###
