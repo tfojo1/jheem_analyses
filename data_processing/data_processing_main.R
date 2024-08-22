fb34b8af9ff6fe485c90d812a8ad0a032ea01f41
Zoe Dansky <zoedansky@gmail.com>
2024-08-22 13:36:56 -0400

    updating ontology

--- data_processing/data_processing_main.R
+++ data_processing/data_processing_main.R
@@ -606,7 +606,7 @@ data.manager$register.ontology(
   ont = ontology(
     year= NULL,
     location= NULL,
-    age=c('13-19 years', '20-24 years', '25-29 years', '30-34 years', '35-39 years',
+    age=c('13-17 years', '18-19 years', '20-24 years', '25-29 years', '30-34 years', '35-39 years',
           '40-44 years', '45-49 years', '50-54 years', '55-59 years', '60-64 years', '65-69 years', '70-74 years', 
           '75-79 years', '80-84 years', '85+ years'),
     race=c('white', 'black', 'american indian or alaska native', 'asian or pacific islander'),
