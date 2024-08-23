f527bef26e91db62e84d2027086bd0865294ece1
Zoe Dansky <136368115+zoedansky@users.noreply.github.com>
2024-08-23 15:23:25 -0600

    updating outlier files

---
+++ data_processing/outliers/outlier.remover.two.way.strata.R
@@ -0,0 +1,16 @@
+#Look into the two way strata- assuming most problematic locations have been
+#removed at this point via getting flagged the total or one way 
+#But Todd said to check manually and just see if there is any blatant issue
+#Look at larger cities
+
+msas = locations::get.all.for.type("CBSA")
+
+#Note: Total Prevalence isn't stratified
+
+# Diagnoses ---------------------------------------------------------------
+
+# Diagnosed Prevalence ----------------------------------------------------
+
+
+
+
