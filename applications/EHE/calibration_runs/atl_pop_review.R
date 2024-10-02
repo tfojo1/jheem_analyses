c6ad27e3bb98b3d4063956d8f03b3674504e0a29
mschnure <mschnur3@jhmi.edu>
2024-10-02 14:04:04 -0400

    mcmc review

--- applications/EHE/calibration_runs/atl_pop_review.R
+++ applications/EHE/calibration_runs/atl_pop_review.R
@@ -6,34 +6,46 @@ location.style.manager = create.style.manager(color.data.by = "location.type")
 source.style.manager = create.style.manager(color.data.by = "source") 
 stratum.style.manager = create.style.manager(color.data.by = "stratum")
 
-load("../jheem_analyses/prelim_results/init.pop.ehe_simset_2024-09-24_C.12060.Rdata")
-sim.mcmc = simset$last.sim()
+load("../jheem_analyses/prelim_results/init.pop.ehe_simset_2024-09-30_C.12060.Rdata")
+simset.pop = simset
 
+# full.with.aids has no significant differences in population - going to work with population calibration instead 
+# load("../jheem_analyses/prelim_results/full.with.aids_simset_2024-10-01_C.12060.Rdata")
+# sim.full = simset
+ 
 atl.pop.lik = joint.pop.migration.total.trans.likelihood.instructions$instantiate.likelihood('ehe','C.12060')
-
+ 
 engine = create.jheem.engine('ehe','C.12060',end.year = 2025)
-params.mcmc = sim.mcmc$params
+params.mcmc = simset.pop$last.sim()$params
 sim.mcmc = engine$run(params.mcmc)
-
-cbind(sim.mcmc$get.params("birth"))
-cbind(sim.mcmc$get.params("mortality"))
-cbind(sim.mcmc$get.params("migration"))
-cbind(sim.mcmc$get.params("hispanic.immigration.multiplier.time"))
-
+ 
+# cbind(sim.mcmc$get.params("birth"))
+# cbind(sim.mcmc$get.params("mortality"))
+# cbind(sim.mcmc$get.params("migration"))
+cbind(sim.mcmc$get.params("other.age2"))
+# 
 
 params.manual = params.mcmc
-#sim.manual = engine$run(params.manual)
-params.manual["hispanic.birth.rate.multiplier"] = 3 # 1.0143249
-#params.manual["hispanic.emigration.multiplier.time.2"] = 1 # 0.04113772
-#params.manual["hispanic.emigration.multiplier.time.1"] = 1 # 0.04695921
+sim.manual = engine$run(params.manual)
+params.manual["other.birth.rate.multiplier"] = 1.07 # 0.8792656
+# params.manual["other.age1.migration.multiplier.time.1"] = 1 # 1.060586
+params.manual["other.age1.migration.multiplier.time.2"] = .75 # 1.021673
+# params.manual["other.age1.aging.multiplier.1"] = 1 # 1.296845
+params.manual["other.age1.aging.multiplier.2"] = 1.3 # 1.000605
+params.manual["other.age2.aging.multiplier.1"] = 1 # 0.9872116
+params.manual["other.age2.aging.multiplier.2"] = 1.2 # 0.9825717
+params.manual["other.age2.migration.multiplier.time.1"] = 1.5 # 0.9866867
+params.manual["other.age2.migration.multiplier.time.2"] = .8 # 1.0198221
+params.manual["other.age4.migration.multiplier.time.2"] = .8 # 1.0343240
 
 sim.manual = engine$run(params.manual)
+# 
+atl.pop.lik$compare.sims(sim.mcmc,sim.manual)
+#exp(atl.pop.lik$compute(sim.manual) - atl.pop.lik$compute(sim.mcmc))
 
-atl.pop.lik$compare.sims(sim.mcmc,sim.manual) 
-exp(atl.pop.lik$compute(sim.manual) - atl.pop.lik$compute(sim.mcmc))
+# qplot(1:simset.pop$n.sim, simset.pop$parameters["other.immigration.multiplier.time.1",]) 
 
-simplot(sim.engine.test,
-        sim.mcmc,
+simplot(sim.mcmc,
         sim.manual,
         facet.by = "age", split.by = "race",
         outcomes = c("population"), 
--- applications/EHE/calibration_runs/full_calibration_review_v19.R
+++ applications/EHE/calibration_runs/full_calibration_review_v19.R
@@ -6,14 +6,31 @@ location.style.manager = create.style.manager(color.data.by = "location.type")
 source.style.manager = create.style.manager(color.data.by = "source") 
 stratum.style.manager = create.style.manager(color.data.by = "stratum")
 
-LOCATION = CHICAGO.MSA
+LOCATION = NYC.MSA
 
+if(LOCATION==BALTIMORE.MSA){
+  load(paste0("../jheem_analyses/prelim_results/full.with.covid2_simset_2024-09-27_",LOCATION,".Rdata"))
+  simset.old = simset
+  
+  load(paste0("../jheem_analyses/prelim_results/full.with.covid2_simset_2024-10-02_",LOCATION,".Rdata"))
+  simset.new = simset
+  
+} else {
+  load(paste0("../jheem_analyses/prelim_results/full.with.aids_simset_2024-09-27_",LOCATION,".Rdata"))
+  simset.old = simset
+  
+  load(paste0("../jheem_analyses/prelim_results/full.with.aids_simset_2024-10-01_",LOCATION,".Rdata"))
+  simset.new = simset  
+}
 
-load(paste0("../jheem_analyses/prelim_results/full.with.aids_simset_2024-09-25_",LOCATION,".Rdata"))
-simset.old = simset
+balt.lik = FULL.likelihood.instructions.with.covid$instantiate.likelihood('ehe','C.12580')
+nyc.lik = FULL.likelihood.instructions.with.aids$instantiate.likelihood('ehe','C.35620')
+houston.lik = FULL.likelihood.instructions.with.aids$instantiate.likelihood('ehe','C.26420')
+chicago.lik = FULL.likelihood.instructions.with.aids$instantiate.likelihood('ehe','C.16980')
+atl.lik = FULL.likelihood.instructions.with.aids$instantiate.likelihood('ehe','C.12060')
 
-load(paste0("../jheem_analyses/prelim_results/full.with.aids_simset_2024-09-27_",LOCATION,".Rdata"))
-simset.new = simset
+#atl.pop.lik$compare.sims(simset.old$last.sim(),simset.new$last.sim()) 
+#exp(atl.pop.lik$compute(simset.new$last.sim()) - atl.pop.lik$compute(simset.old$last.sim()))
 
 simplot(simset.old$last.sim(),
         simset.new$last.sim(),
@@ -21,7 +38,8 @@ simplot(simset.old$last.sim(),
         outcomes = c("population"), 
         dimension.values = list(year = 2000:2030)) 
 
-simplot(simset.old$last.sim(),
+simplot(#simset.old$last.sim(),
+        simset.new,
         simset.new$last.sim(),
         outcomes = c("diagnosed.prevalence"), 
         style.manager = source.style.manager,
@@ -38,7 +56,6 @@ simplot(simset.old$last.sim(),
         facet.by = "age", split.by = "sex", 
         outcomes = c("diagnosed.prevalence"), 
         dimension.values = list(year = 2000:2030)) 
-
 
 simplot(simset.old$last.sim(),
         simset.new$last.sim(),
@@ -80,6 +97,25 @@ simplot(simset.old$last.sim(),
 
 simplot(simset.old$last.sim(),
         simset.new$last.sim(),
+        outcomes = c("cdc.hiv.test.positivity"), # totals only 
+        style.manager = location.style.manager,
+        dimension.values = list(year = 2000:2030)) 
+
+simplot(simset.old$last.sim(),
+        simset.new$last.sim(),
         outcomes = c("aids.diagnoses"), 
         style.manager = source.style.manager,
         dimension.values = list(year = 1980:2030)) 
+
+simplot(simset.old$last.sim(),
+        simset.new$last.sim(),
+        facet.by = "sex", # sex; 1-way 
+        outcomes = c("hiv.mortality"),
+        style.manager = source.style.manager,
+        dimension.values = list(year = 2000:2030)) 
+
+simplot(simset.old$last.sim(),
+        simset.new$last.sim(),
+        outcomes = c("total.mortality"), # totals only 
+        style.manager = location.style.manager,
+        dimension.values = list(year = 2000:2030)) 
