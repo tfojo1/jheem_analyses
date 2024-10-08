7e6d027e624815b4c8b0a9277b3094b7984787c1
Zoe Dansky <136368115+zoedansky@users.noreply.github.com>
2024-10-08 10:51:59 -0600

    adding national level births+ births.denominator

--- data_processing/births_and_deaths.R
+++ data_processing/births_and_deaths.R
@@ -278,3 +278,145 @@ for (data in deaths_denom ) {
     url = 'https://wonder.cdc.gov/',
     details = 'CDC Wonder')
 }
+
+
+################################################################################
+#Read in National Level Birth Data
+################################################################################
+DATA.DIR.BIRTH.NATIONAL="../../data_raw/births_deaths/births/national"
+
+birth_files_national <- list.files(DATA.DIR.BIRTH.NATIONAL, pattern = ".txt", full.names = "TRUE")
+
+data.list.births.national <- lapply(birth_files_national, function(x) {
+  list(filename=x, data=read.delim2(x))
+  
+})
+################################################################################
+#Clean National Level Birth Data
+################################################################################
+births.national.clean = lapply(data.list.births.national, function(file){
+  
+  data=file[["data"]] 
+  filename = file[["filename"]]
+  
+  data$year = as.character(data$Year)
+  data$outcome= "births"
+  data$ethnicity = data$`Mother.s.Hispanic.Origin`
+  data$location = "US"
+  
+  ##Removing births = suppressed or Missing County##
+  ##'Missing County' appears when county data is not available for a certain year. 
+  #This occurs because the county did not meet minimum population standards and thus the data for the county was recoded to the "Unidentified Counties"
+  data = subset(data, data$Births != "Suppressed") 
+  data = subset(data, data$Births != "Missing County")
+  
+  data$value = as.numeric(data$Births)
+  
+  if(grepl("bridged.race", filename)) {
+    data$race = data$`Mother.s.Bridged.Race`
+  }
+  if(grepl("single.race", filename)) {
+    data$race = data$`Mother.s.Single.Race`
+    #Combining racial groups from bridged and single
+    #Todd said for now to leave in Not reported, not available, and unknown and add to the ontology
+    data$race = if_else(data$race == "Native Hawaiian or Other Pacific Islander" | data$race == "Asian", "Asian or Pacific Islander", data$race)
+  }
+  
+  data <- data %>%
+    select(outcome, year, location, value, race, ethnicity, Births)
+  
+  if(grepl("single.race", filename)) {
+    
+    #Now re-calculate the rate for the combined asian + native hawaiian pacific islander group
+    data <- data %>%
+      group_by(year, location, ethnicity, race)%>%
+      mutate(combined.asian.births = sum(Births))%>%
+      select(-value)%>%
+      rename(value = combined.asian.births)%>%
+      ungroup()
+  }
+  
+  data = as.data.frame(data)
+  list(filename, data)  
+  
+})
+################################################################################
+#National Level Birth Denominator Cleaning
+################################################################################
+options(error=NULL)
+births.denominator.national.clean = lapply(data.list.births.national, function(file){
+  
+  data=file[["data"]] 
+  filename = file[["filename"]]
+  
+  data$year = as.character(data$Year)
+  data$outcome= "births.denominator"
+  data$ethnicity = data$`Mother.s.Hispanic.Origin`
+  data$location = "US"
+  
+  ##Removing births = suppressed or Missing County##
+  ##'Missing County' appears when county data is not available for a certain year. 
+  #This occurs because the county did not meet minimum population standards and thus the data for the county was recoded to the "Unidentified Counties"
+  data = subset(data, data$`Total.Population` != "Suppressed") 
+  data = subset(data, data$`Total.Population` != "Missing County")
+  data = subset(data, data$`Total.Population` != "Not Available")
+  
+  data$value = as.numeric(data$`Total.Population`)
+  data$`Total.Population` = as.numeric(data$`Total.Population`)
+  
+  if(grepl("bridged.race", filename)) {
+    data$race = data$`Mother.s.Bridged.Race`
+  }
+  if(grepl("single.race", filename)) {
+    data$race = data$`Mother.s.Single.Race`
+    #Combining racial groups from bridged and single
+    #Todd said for now to leave in Not reported, not available, and unknown and add to the ontology
+    data$race = if_else(data$race == "Native Hawaiian or Other Pacific Islander" | data$race == "Asian", "Asian or Pacific Islander", data$race)
+  }
+  
+  #data <- data %>%
+  #select(outcome, year, location, value, race, ethnicity, `Total.Population`)
+  
+  if(grepl("single.race", filename)) {
+    
+    #Now re-calculate the rate for the combined asian + native hawaiian pacific islander group
+    data <- data %>%
+      group_by(year, location, ethnicity, race)%>%
+      mutate(combined.asian.population = sum(`Total.Population`))%>%
+      select(-value)%>%
+      rename(value = combined.asian.population)%>%
+      ungroup()
+  }
+  
+  data = as.data.frame(data)
+  list(filename, data)  
+  
+})
+################################################################################
+#Put National Births into Census Manager#
+################################################################################
+births.national.clean.put = lapply(births.national.clean , `[[`, 2)
+
+for (data in births.national.clean.put ) {
+  
+  census.manager$put.long.form(
+    data = data,
+    ontology.name = 'census.cdc.wonder.births.deaths',
+    source = 'cdc_wonder',
+    dimension.values = list(),
+    url = 'https://wonder.cdc.gov/',
+    details = 'CDC Wonder')
+}
+
+births_denom_national= lapply(births.denominator.national.clean , `[[`, 2)
+
+for (data in births_denom_national ) {
+  
+  census.manager$put.long.form(
+    data = data,
+    ontology.name = 'census.cdc.wonder.births.deaths',
+    source = 'cdc_wonder',
+    dimension.values = list(),
+    url = 'https://wonder.cdc.gov/',
+    details = 'CDC Wonder')
+}
