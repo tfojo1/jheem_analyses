d2a55f20eb53a1455ac4973dd3625d4e36a53328
mschnure <mschnur3@jhmi.edu>
2024-10-11 12:39:32 -0400

    mcmc review

--- applications/EHE/calibration_runs/full_calibration_review_v19.R
+++ applications/EHE/calibration_runs/full_calibration_review_v19.R
@@ -6,7 +6,7 @@ location.style.manager = create.style.manager(color.data.by = "location.type")
 source.style.manager = create.style.manager(color.data.by = "source") 
 stratum.style.manager = create.style.manager(color.data.by = "stratum")
 
-LOCATION = CHICAGO.MSA
+LOCATION = NYC.MSA
 
 if(LOCATION==BALTIMORE.MSA){
   load(paste0("../jheem_analyses/prelim_results/full.with.covid2_simset_2024-10-03_",LOCATION,".Rdata"))
@@ -15,7 +15,7 @@ if(LOCATION==BALTIMORE.MSA){
   load(paste0("../jheem_analyses/prelim_results/full.with.covid2_simset_2024-10-07_",LOCATION,".Rdata"))
   simset.old = simset
   
-  load(paste0("../jheem_analyses/prelim_results/full.with.covid2_simset_2024-10-09_",LOCATION,".Rdata"))
+  load(paste0("../jheem_analyses/prelim_results/full.with.covid2_simset_2024-10-11_",LOCATION,".Rdata"))
   simset.new = simset
   
 } else {
@@ -25,19 +25,46 @@ if(LOCATION==BALTIMORE.MSA){
   load(paste0("../jheem_analyses/prelim_results/full.with.aids_simset_2024-10-05_",LOCATION,".Rdata"))
   simset.old = simset  
   
-  load(paste0("../jheem_analyses/prelim_results/full.with.aids_simset_2024-10-09_",LOCATION,".Rdata"))
+  load(paste0("../jheem_analyses/prelim_results/full.with.aids_simset_2024-10-11_",LOCATION,".Rdata"))
   simset.new = simset  
 }
 
 # balt.lik = FULL.likelihood.instructions.with.covid$instantiate.likelihood('ehe','C.12580')
-#nyc.lik = FULL.likelihood.instructions.with.aids$instantiate.likelihood('ehe','C.35620')
+# nyc.lik = FULL.likelihood.instructions.with.aids$instantiate.likelihood('ehe','C.35620')
 # houston.lik = FULL.likelihood.instructions.with.aids$instantiate.likelihood('ehe','C.26420')
-#chicago.lik = FULL.likelihood.instructions.with.aids$instantiate.likelihood('ehe','C.16980')
+# chicago.lik = FULL.likelihood.instructions.with.aids$instantiate.likelihood('ehe','C.16980')
 # atl.lik = FULL.likelihood.instructions.with.aids$instantiate.likelihood('ehe','C.12060')
 
-#chicago.lik$compare.sims(simset.old$last.sim(),simset.new$last.sim()) 
-#exp(atl.pop.lik$compute(simset.new$last.sim()) - atl.pop.lik$compute(simset.old$last.sim()))
+# COMPARING TO 10/7 MCMC
 
+## CHICAGO ## 
+# New simset is worse overall, including prevalence, but suppression improved 
+chicago.lik$compare.sims(simset.old$last.sim(),simset.new$last.sim()) 
+exp(chicago.lik$compute(simset.new$last.sim()) - chicago.lik$compute(simset.old$last.sim()))
+
+## NYC ##
+# New simset is worse overall, including prevalence
+# Suppression looks better but scored worse (probably because of trend of line)
+nyc.lik$compare.sims(simset.old$last.sim(),simset.new$last.sim()) 
+exp(nyc.lik$compute(simset.new$last.sim()) - nyc.lik$compute(simset.old$last.sim()))
+
+## HOUSTON ## 
+# New simset is better overall, including suppression
+houston.lik$compare.sims(simset.old$last.sim(),simset.new$last.sim()) 
+exp(houston.lik$compute(simset.new$last.sim()) - houston.lik$compute(simset.old$last.sim()))
+
+## ATLANTA ## 
+# New simset is better overall, including suppression and prevalence (new creeping up a bit)
+atl.lik$compare.sims(simset.old$last.sim(),simset.new$last.sim()) 
+exp(atl.lik$compute(simset.new$last.sim()) - atl.lik$compute(simset.old$last.sim()))
+
+## BALTIMORE ## 
+# New simset is better overall, but new diagnoses taking off and awareness trending down
+balt.lik$compare.sims(simset.old$last.sim(),simset.new$last.sim()) 
+exp(balt.lik$compute(simset.new$last.sim()) - balt.lik$compute(simset.old$last.sim()))
+
+plot(1:simset.new$n.sim,simset.new$parameters["black.msm.trate.0",])
+plot(1:simset.new$n.sim,simset.new$parameters["age1.black.msm.susceptibility.rr.01",])
 
 simplot(simset.old$last.sim(),
         #simset.new,
@@ -46,14 +73,15 @@ simplot(simset.old$last.sim(),
         outcomes = c("population"), 
         dimension.values = list(year = 2000:2030)) 
 
-simplot(simset.old$last.sim(),
-        #simset.new,
+simplot(#simset.old$first.sim(),
+        simset.old$last.sim(),
+        #simset.new$first.sim(),
         simset.new$last.sim(),
         outcomes = c("diagnosed.prevalence"), 
         style.manager = source.style.manager,
         dimension.values = list(year = 2000:2030)) 
 
-simplot(simset.old$last.sim(),
+simplot(#simset.old$last.sim(),
         simset.new$last.sim(),
         facet.by = "risk", split.by = "race",
         outcomes = c("diagnosed.prevalence"), 
@@ -65,19 +93,20 @@ simplot(simset.old$last.sim(),
         outcomes = c("diagnosed.prevalence"), 
         dimension.values = list(year = 2000:2030)) 
 
-simplot(simset.old$last.sim(),
+simplot(#simset.old$last.sim(),
+        simset.new,      
         simset.new$last.sim(),
         outcomes = c("new"), 
         style.manager = source.style.manager, 
         dimension.values = list(year = 2000:2030)) 
 
-simplot(simset.old$last.sim(),
+simplot(#simset.old$last.sim(),
         simset.new$last.sim(),
         facet.by = "risk", split.by = "race", 
         outcomes = c("new"), 
         dimension.values = list(year = 2000:2030)) 
 
-simplot(simset.old$last.sim(),
+simplot(#simset.old$last.sim(),
         simset.new$last.sim(),
         facet.by = "age", split.by = "sex", 
         outcomes = c("new"), 
@@ -85,7 +114,7 @@ simplot(simset.old$last.sim(),
 
 simplot(simset.old$last.sim(),
         simset.new$last.sim(),
-        facet.by = "age", 
+       # facet.by = "age", 
         outcomes = c("testing"),
         style.manager = location.style.manager,
         dimension.values = list(year = 2000:2030)) 
@@ -128,10 +157,9 @@ simplot(simset.old$last.sim(),
         style.manager = location.style.manager,
         dimension.values = list(year = 2000:2030)) 
 
-simplot(simset.older$last.sim(),
-        simset.old$last.sim(),
+simplot(simset.old$last.sim(),
         simset.new$last.sim(),
-        facet.by = "sex", # age, sex, race, risk; 1-way 
+        #facet.by = "age", # age, sex, race, risk; 1-way 
         outcomes = c("suppression"), 
         style.manager = location.style.manager,
         dimension.values = list(year = 2000:2030)) 
