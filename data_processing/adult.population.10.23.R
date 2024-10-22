fe5a89a1c3be6c38f5bdd19423f0d9469df5c83d
Zoe Dansky <zoedansky@gmail.com>
2024-10-22 16:36:05 -0400

    Removing copied functions

--- data_processing/adult.population.10.23.R
+++ data_processing/adult.population.10.23.R
@@ -3,151 +3,6 @@
 #Estimate the adult.population from age grouped data
 #Then find adult.population from the single year ages
 
-#census.manager = load.data.manager(name="census.manager", file="../../cached/census.manager.rdata")
-
-# Source Helper Code Todd Wrote -------------------------------------------
-
-##This is the function that Todd wrote; More code included to run the functions that his function references
-##This is from the HELPERS_age_year_helpers.R file (this is in JHEEM2 dev branch I think)
-
-source('data_processing/HELPERS_age_year_helpers_COPY.R')
-
-#'@title Re-distribute counts according to one set of age brackets into a different set of age brackets
-#'
-#'@param counts Either (a) a dimensionless numeric vector, or (b) a numeric array, with  one dimension named "age". The "given" age brackets according to which these counts are distributed must be continuous - with no gaps between them
-#'@param desired.age.brackets,given.age.brackets The age brackets according to which the ages in counts are given or desired to be restratified to. Either (1) a character vector giving names of brackets, with one name for each age bracket value or (b) a numeric vector of age endpoints, with one more element than the number of age bracket values. given.age.brackets should be present only if names(counts) is absent (if counts is a vector) or if dimnames(counts)$age is null (if counts is an array)
-#'@param smooth.infinite.age.to In building a spline over counts, what value should an infinite bound in an age range be replaced with
-#'@param allow.extrapolation A logical value indicating whether extrapolation should be allowed in restratifying. Extrapolating is required when one of the desired age brackets includes ages not present in any of the given age brackets
-#'@param na.rm Whether NAs should be ignored in counts
-#'@param method The method to be passed to \code{\link{splinefun}}. Must be a method that can produce a monotone spline (ie, either 'monoH.FC' or 'hyman')
-#'@param error.prefix A character value to be prepended to any error messages
-#'
-#'@export
-restratify.age.counts <- function(counts,
-                                  desired.age.brackets,
-                                  given.age.brackets = NULL,
-                                  smooth.infinite.age.to = Inf,
-                                  allow.extrapolation = F,
-                                  na.rm = F,
-                                  method=c('monoH.FC','hyman')[1],
-                                  error.prefix = '')
-{
-  #-- Validate error.prefix --#
-  if (!is.character(error.prefix) || length(error.prefix)!=1 || is.na(error.prefix))
-    stop("Error in restratify.age.counts: 'error.prefix' must be a single, non-NA character value")
-  error.prefix = paste0(error.prefix, "Error in restratify.age.counts: ")
-  #-- Validate counts --#
-  if (!is.numeric(counts))
-    stop(paste0(error.prefix, "'counts' must be either a dimensionless numeric vector or numeric array"))
-  given.brackets.from = "'given.age.brackets'"
-  n.brackets = length(counts)
-  n.brackets.from = "length(counts)"
-  if (is.array(counts))
-  {
-    if (is.null(names(dim(counts))))
-      stop(paste0(error.prefix, "If 'counts' is an array, it must have named dimensions"))
-    if (sum(names(dim(counts))=='age')!=1)
-      stop(paste0(error.prefix, "If 'counts' is an array, it must have one (and only one) dimension named 'age'"))
-    if (is.null(dimnames(counts)$age))
-    {
-      if (is.null(given.age.brackets))
-        stop(paste0(error.prefix, "If the dimnames for the age dimension of 'counts' are  not set, then given.age.brackets must be specified (ie, cannot be NULL)"))
-    }
-    else
-    {
-      if (!is.null(given.age.brackets))
-        stop(paste0(error.prefix, "If the dimnames for the age dimension of 'counts' are set, then given.age.brackets cannot be specified (the given age brackets will be derived from the dimnames of 'counts')"))
-      given.age.brackets = dimnames(counts)$age
-      given.brackets.from = "the dimnames for the age dimension of 'counts'"
-    }
-    n.brackets = dim(counts)['age']
-    n.brackets.from = "the length of the 'age' dimension of 'counts'"
-    if (n.brackets < 2)
-      stop(paste0(error.prefix, "The 'age' dimension of 'counts' must have at least two values"))
-  }
-  else
-  {
-    if (is.null(names(counts)))
-    {
-      if (is.null(given.age.brackets))
-        stop(paste0(error.prefix, "If the names of 'counts' is not set, then given.age.brackets must be specified (ie, cannot be NULL)"))
-    }
-    else
-    {
-      if (!is.null(given.age.brackets))
-        stop(paste0(error.prefix, "If the names of 'counts' are set, then given.age.brackets cannot be specified (the given age brackets will be derived from the names of 'counts')"))
-      given.age.brackets = names(counts)
-      given.brackets.from = "the names of 'counts'"
-    }
-    if (n.brackets < 2)
-      stop(paste0(error.prefix, "'counts' must have at least two values"))
-  }
-  #-- Parse/Validate given.age.brackets --#
-  parsed.given.brackets = parse.age.brackets(age.brackets = given.age.brackets,
-                                             n.brackets = n.brackets,
-                                             require.contiguous = T,
-                                             smooth.infinite.age.to = smooth.infinite.age.to,
-                                             require.non.infinite.ages = T,
-                                             error.prefix = error.prefix,
-                                             required.length.name.for.error = n.brackets.from,
-                                             age.brackets.name.for.error = given.brackets.from)
-  #-- Parse/Validate desired.age.brackets --#
-  parsed.desired.brackets = parse.age.brackets(age.brackets = desired.age.brackets,
-                                               n.brackets = NA,
-                                               require.contiguous = F,
-                                               smooth.infinite.age.to = smooth.infinite.age.to,
-                                               require.non.infinite.ages = T,
-                                               error.prefix = error.prefix,
-                                               required.length.name.for.error = NA,
-                                               age.brackets.name.for.error = 'desired.age.brackets')
-  if (!allow.extrapolation &&
-      (any(parsed.desired.brackets$lower < parsed.given.brackets$lower[1]) ||
-       any(parsed.desired.brackets$upper > parsed.given.brackets$upper[n.brackets])))
-    stop(paste0(error.prefix, "Cannot extrapolate counts for ages beyond the given ages (ie, less than ",
-                parsed.given.brackets$lower[1], " or greater than ", parsed.given.brackets$upper[n.brackets], ")"))
-  #-- Make the new values --#
-  if (is.array(counts))
-  {
-    orig.dim = dim(counts)
-    orig.dim.names = dimnames(counts)
-    if (is.null(orig.dim.names))
-      orig.dim.names = lapply(orig.dim, function(d){NULL})
-    non.age.dimensions = setdiff(names(orig.dim), 'age')
-    raw = apply(counts, non.age.dimensions, function(val){
-      smoother = do.get.cumulative.age.counts.smoother(counts = val[parsed.given.brackets$order],
-                                                       endpoints = parsed.given.brackets$endpoints,
-                                                       method = method)
-      smoother(parsed.desired.brackets$upper) - smoother(parsed.desired.brackets$lower)
-    })
-    raw.dim = c(age = length(parsed.desired.brackets$names),
-                orig.dim[non.age.dimensions])
-    raw.dim.names = c(list(age = parsed.desired.brackets$names),
-                      orig.dim.names[non.age.dimensions])
-    dim(raw) = raw.dim
-    dimnames(raw) = raw.dim.names
-    if (names(orig.dim)[1] != 'age')
-    {
-      new.dim.names = raw.dim.names[names(orig.dim)]
-      new.dim = raw.dim[names(orig.dim)]
-      rv = apply(raw, names(orig.dim), function(x){x})
-      dim(rv) = new.dim
-      dimnames(rv) = new.dim.names
-      rv
-    }
-    else
-      raw
-  }
-  else
-  {
-    smoother = do.get.cumulative.age.counts.smoother(counts = counts[parsed.given.brackets$order],
-                                                     endpoints = parsed.given.brackets$endpoints,
-                                                     method = method)
-    rv = smoother(parsed.desired.brackets$upper) - smoother(parsed.desired.brackets$lower)
-    names(rv) = parsed.desired.brackets$names
-    rv
-  }
-}
-
 
 # ESTIMATED DATA ----------------------------------------------------------
 
@@ -165,7 +20,7 @@ race.mappings.to.census = c('White' = 'white',
 #ESTIMATED DATA: adult.population 2010-2023 by RACE
 population.by.race.array = census.manager$data$population$estimate$census.population$stratified.census$year__location__age__race
 
-restratify.age.race <- restratify.age.counts(population.by.race.array, desired.age.brackets= desired.ages.for.census, smooth.infinite.age.to =100)
+restratify.age.race <- jheem2::restratify.age.counts(population.by.race.array, desired.age.brackets= desired.ages.for.census, smooth.infinite.age.to =100)
 
 restratify.adult.pop.race.10.23 = restratify.age.race[ , ,3:18,] #subset by only adult age groups
 adult.pop.race.10.23 = apply(restratify.adult.pop.race.10.23, MARGIN = c("year","location", "race"), sum) #sum the adult age groups to get adult.population for 2020-2023
@@ -191,7 +46,7 @@ adult.pop.race.10.23<- as.data.frame(adult.pop.race.10.23[!duplicated(adult.pop.
 #ESTIMATED DATA: adult.population 2010-2023 by ETHNICITY
 population.by.ethnicity.array = census.manager$data$population$estimate$census.population$stratified.census$year__location__age__ethnicity
 
-restratify.age.ethnicity <- restratify.age.counts(population.by.ethnicity.array, desired.age.brackets= desired.ages.for.census, smooth.infinite.age.to =100)
+restratify.age.ethnicity <- jheem2::restratify.age.counts(population.by.ethnicity.array, desired.age.brackets= desired.ages.for.census, smooth.infinite.age.to =100)
 
 restratify.adult.pop.ethnicity.10.23 = restratify.age.ethnicity[ , , 3:18,] #subset by only adult age groups
 adult.pop.ethnicity.10.23 = apply(restratify.adult.pop.ethnicity.10.23, MARGIN = c("year","location", "ethnicity"), sum) #sum the adult age groups to get adult.population for 2020-2023
@@ -208,7 +63,7 @@ adult.pop.ethnicity.10.23 <- as.data.frame.table(adult.pop.ethnicity.10.23)%>%
 
 array.race.eth = census.manager$data$population$estimate$census.population$stratified.census$year__location__age__race__ethnicity
 
-restratify.race.eth <- restratify.age.counts(array.race.eth, desired.age.brackets= desired.ages.for.census, smooth.infinite.age.to =100)
+restratify.race.eth <- jheem2::restratify.age.counts(array.race.eth, desired.age.brackets= desired.ages.for.census, smooth.infinite.age.to =100)
 
 restrat.race.eth = restratify.race.eth[ , ,3:18, ,] #subset by only adult age groups
 fixed.race.eth = apply(restrat.race.eth, MARGIN = c("year","location", "race", 'ethnicity'), sum) #sum the adult age groups to get adult.population for 2020-2023
@@ -237,7 +92,7 @@ fixed.race.eth<- as.data.frame(fixed.race.eth[!duplicated(fixed.race.eth), ])
 #ESTIMATED DATA: adult.population 2010-2023 by RACE+ETHNICITY+AGE
 array.race.eth.age = census.manager$data$population$estimate$census.population$stratified.census$year__location__age__race__ethnicity
 
-restratify.race.eth.age <- restratify.age.counts(array.race.eth.age, desired.age.brackets= desired.ages.for.census, smooth.infinite.age.to =100)
+restratify.race.eth.age <- jheem2::restratify.age.counts(array.race.eth.age, desired.age.brackets= desired.ages.for.census, smooth.infinite.age.to =100)
 
 restrat.race.eth.age = restratify.race.eth.age[ , ,3:18, ,] #subset by only adult age groups
 fixed.race.eth.age = apply(restrat.race.eth.age, MARGIN = c("year","location", "race", 'ethnicity', 'age'), sum) #sum the adult age groups to get adult.population for 2020-2023
@@ -269,7 +124,7 @@ fixed.race.eth.age<- as.data.frame(fixed.race.eth.age[!duplicated(fixed.race.eth
 # Race+Ethnicity+Sex 2010-2023--------------------------------------------------
 array.race.eth.sex = census.manager$data$population$estimate$census.population$stratified.census$year__location__age__race__ethnicity__sex
 
-restratify.race.eth.sex <- restratify.age.counts(array.race.eth.sex, desired.age.brackets= desired.ages.for.census, smooth.infinite.age.to =100)
+restratify.race.eth.sex <- jheem2::restratify.age.counts(array.race.eth.sex, desired.age.brackets= desired.ages.for.census, smooth.infinite.age.to =100)
 
 restrat.race.eth.sex = restratify.race.eth.sex[ , ,3:18, , ,] #subset by only adult age groups
 fixed.race.eth.sex = apply(restrat.race.eth.sex, MARGIN = c("year","location", "race", 'ethnicity', 'sex'), sum) #sum the adult age groups to get adult.population for 2020-2023
@@ -421,7 +276,7 @@ for (data in adult.pop.by.single.year.age) {
 #Total 2010-2019
 total.array.10.19 = census.manager$data$population$estimate$census.population$stratified.census$year__location__age
 
-total.10.19 <- restratify.age.counts(total.array.10.19, desired.age.brackets= desired.ages.for.census, smooth.infinite.age.to =100)
+total.10.19 <- jheem2::restratify.age.counts(total.array.10.19, desired.age.brackets= desired.ages.for.census, smooth.infinite.age.to =100)
 
 fixed.total.10.19 = total.10.19[ , ,3:18] #subset by only adult age groups
 fixed.total.10.19 = apply(fixed.total.10.19, MARGIN = c("year","location"), sum) 
@@ -436,7 +291,7 @@ fixed.total.10.19 <- as.data.frame.table(fixed.total.10.19)%>%
 #Sex 2010-2019
 sex.array.10.19 = census.manager$data$population$estimate$census.population$stratified.census$year__location__age__sex
 
-sex.10.19 <- restratify.age.counts(sex.array.10.19, desired.age.brackets= desired.ages.for.census, smooth.infinite.age.to =100)
+sex.10.19 <- jheem2::restratify.age.counts(sex.array.10.19, desired.age.brackets= desired.ages.for.census, smooth.infinite.age.to =100)
 
 fixed.sex.10.19 = sex.10.19[ , ,3:18, ] #subset by only adult age groups
 fixed.sex.10.19 = apply(fixed.sex.10.19, MARGIN = c("year","location", "sex"), sum) 
@@ -452,7 +307,7 @@ fixed.sex.10.19 <- as.data.frame.table(fixed.sex.10.19)%>%
 #Age 2010-2019
 age.array.10.19 = census.manager$data$population$estimate$census.population$stratified.census$year__location__age
 
-age.10.19 <- restratify.age.counts(age.array.10.19, desired.age.brackets= desired.ages.for.census, smooth.infinite.age.to =100)
+age.10.19 <- jheem2::restratify.age.counts(age.array.10.19, desired.age.brackets= desired.ages.for.census, smooth.infinite.age.to =100)
 
 fixed.age.10.19 = age.10.19[ , ,3:18] #subset by only adult age groups
 fixed.age.10.19 = apply(fixed.age.10.19, MARGIN = c("year","location", "age"), sum) 
@@ -468,7 +323,7 @@ fixed.age.10.19 <- as.data.frame.table(fixed.age.10.19)%>%
 #Age+Sex 2010-2019
 age.sex.array.10.19 = census.manager$data$population$estimate$census.population$stratified.census$year__location__age__sex
 
-age.sex.10.19 <- restratify.age.counts(age.sex.array.10.19, desired.age.brackets= desired.ages.for.census, smooth.infinite.age.to =100)
+age.sex.10.19 <- jheem2::restratify.age.counts(age.sex.array.10.19, desired.age.brackets= desired.ages.for.census, smooth.infinite.age.to =100)
 
 fixed.age.sex.10.19 = age.sex.10.19[ , ,3:18, ] #subset by only adult age groups
 fixed.age.sex.10.19 = apply(fixed.age.sex.10.19, MARGIN = c("year","location", "sex", 'age'), sum) 
