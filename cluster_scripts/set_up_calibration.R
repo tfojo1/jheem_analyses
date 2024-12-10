7d069545c1cc5d707ef4aeb361f6e85c4bc4b4af
A-Zalesak <azalesak@alumni.unc.edu>
2024-12-10 13:33:09 -0500

    create command line script for testing

---
+++ cluster_scripts/set_up_calibration.R
@@ -0,0 +1,29 @@
+#-- CHECK ARGUMENTS --#
+args = commandArgs(trailingOnly=TRUE)
+if (length(args)<1) {
+    stop("One argument must be supplied", call.=FALSE)
+}
+
+# #-- SET THE WD --#
+# setwd('jheem_analyses')
+# 
+# #-- SOURCE THE RELEVANT FILES --#
+# print("Loading Source Files...")
+# source('applications/EHE/ehe_specification.R')
+
+version = args[1]
+location = args[2]
+calibration.code = args[3]
+
+print(typeof(version))
+print(typeof(location))
+print(typeof(calibration.code))
+
+# clear.calibration.cache(version=version,
+#                         location=location,
+#                         calibration.code=calibration.code,
+#                         allow.remove.incomplete = T)
+# set.up.calibration(version=version,
+#                    location=location,
+#                    calibration.code=calibration.code,
+#                    cache.frequency = 250)
