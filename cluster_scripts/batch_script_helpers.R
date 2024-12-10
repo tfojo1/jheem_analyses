0c2316d250bd62aa7cd0af632b9724b2fa1ee26a
A-Zalesak <azalesak@alumni.unc.edu>
2024-12-10 15:10:20 -0500

    fix again

--- cluster_scripts/batch_script_helpers.R
+++ cluster_scripts/batch_script_helpers.R
@@ -60,5 +60,5 @@ make.setup.script <- function(location,
                        partition = 'shared',
                        time.hours = 48,
                        account=account,
-                       commands= paste("Rscript set_up_calibration.r", "ehe", "C.12580", "init.pop.ehe"))
+                       commands= paste("Rscript cluster_scripts/set_up_calibration.r", "ehe", "C.12580", "init.pop.ehe"))
 }
