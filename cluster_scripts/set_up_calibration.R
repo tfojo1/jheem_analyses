772e1bb2650d3b51aae3f1124525071b87d0f71f
A-Zalesak <azalesak@alumni.unc.edu>
2024-12-11 10:50:35 -0500

    change working directory

--- cluster_scripts/set_up_calibration.R
+++ cluster_scripts/set_up_calibration.R
@@ -1,12 +1,12 @@
 #-- CHECK ARGUMENTS --#
 args = commandArgs(trailingOnly=TRUE)
-if (length(args)<1) {
-    stop("One argument must be supplied", call.=FALSE)
+if (length(args)<3) {
+    stop("Three arguments must be supplied", call.=FALSE)
 }
 
 #-- SET THE WD --#
-print(getwd())
-# setwd('jheem_analyses')
+# print(getwd())
+setwd('scr4_pkasaie1/azalesak/jheem/code/jheem_analyses')
 
 #-- SOURCE THE RELEVANT FILES --#
 print("Loading Source Files...")
@@ -16,15 +16,15 @@ version = args[1]
 location = args[2]
 calibration.code = args[3]
 
-print(typeof(version))
-print(typeof(location))
-print(typeof(calibration.code))
+# print(typeof(version))
+# print(typeof(location))
+# print(typeof(calibration.code))
 
-# clear.calibration.cache(version=version,
-#                         location=location,
-#                         calibration.code=calibration.code,
-#                         allow.remove.incomplete = T)
-# set.up.calibration(version=version,
-#                    location=location,
-#                    calibration.code=calibration.code,
-#                    cache.frequency = 250)
+clear.calibration.cache(version=version,
+                        location=location,
+                        calibration.code=calibration.code,
+                        allow.remove.incomplete = T)
+set.up.calibration(version=version,
+                   location=location,
+                   calibration.code=calibration.code,
+                   cache.frequency = 250)
