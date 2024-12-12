cc859ed1025a419b888c6e7e3b1838ccd05b14c2
A-Zalesak <azalesak@alumni.unc.edu>
2024-12-12 14:10:53 -0500

    make run scripts

---
+++ cluster_scripts/run_scripts/run_C.35620_1.bat
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH --job-name=setup_C.35620_1
+#SBATCH --mem=16G
+#SBATCH --output=../jheem_analyses/cluster_scripts/run_C.35620_1.out
+#SBATCH --time=168:00:00
+#SBATCH --partition=unlimited
+#SBATCH --account=pkasaie1
+
+ml gfbf/2023b
+ml R/4.3.3-gfbf-2023b
+export R_LIBS_USER=$HOME/rlibs/R-4.3.3
+Rscript cluster_scripts/run_calibration.R ehe C.35620 init.pop.ehe 1
