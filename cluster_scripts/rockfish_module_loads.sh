964aa7fd6e2daa322d3b4f38bf55a05a5efdf8cd
A-Zalesak <azalesak@alumni.unc.edu>
2025-01-03 11:25:21 -0500

    add mkdir to module load script

--- cluster_scripts/rockfish_module_loads.sh
+++ cluster_scripts/rockfish_module_loads.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/bash
 
-echo "First ml"
+mkdir -p $HOME/rlibs/R-4.3.3
 ml gfbf/2023b
-echo "Second ml"
 ml R/4.3.3-gfbf-2023b
-echo "Export"
 export R_LIBS_USER=$HOME/rlibs/R-4.3.3
-echo "Complete"
