11c9c07e31da71f85c9a168b312b94426cbcc5f9
Melissa Schnure <mschnur3@jh.edu>
2025-01-03 12:48:01 -0500

    updated STI ratio cv to 1.35 and removed old code

--- applications/EHE/ehe_likelihoods.R
+++ applications/EHE/ehe_likelihoods.R
@@ -640,53 +640,12 @@ gonorrhea.year.on.year.change.likelihood.instructions =
                                              error.variance.type = 'cv',
                                              correlation.different.years = 0.5,
                                              
-                                             ratio.cv = 1.5,
+                                             ratio.cv = 1.35,
                                              # ratio.correlation = , # NULL will enter default of 0
                                              
                                              weights = (1*TOTAL.WEIGHT),
                                              equalize.weight.by.year = T 
   )
-
-
-# gonorrhea.year.on.year.change.likelihood.instructions = 
-#   create.time.lagged.comparison.likelihood.instructions.with.included.multiplier(outcome.for.data = "gonorrhea.ratio", 
-#                                                                                  outcome.for.sim = "sexual.transmission.rates", 
-#                                                                                  # (2020 gon diagnoses / 2019 gon diagnoses) proportional to 
-#                                                                                  # (2020 sexual transmisson/2019 sexual transmission)
-#                                                                                  levels.of.stratification = c(0,1), 
-#                                                                                  dimensions = c("sex","race","age"),
-#                                                                                  from.year = 2018, 
-#                                                                                  observation.correlation.form = 'compound.symmetry', 
-#                                                                                  error.variance.term = 0.03, # pick a smarter one
-#                                                                                  error.variance.type = 'cv',
-#                                                                                  correlation.different.years = 0.5,
-#                                                                                  
-#                                                                                  # ratio of HIV transmission (year-on-year) is proportional 
-#                                                                                  # to ratio of gonorrhea transmission (year-on-year) 
-#                                                                                  # with best estimate of 1 but can be off by X%
-#                                                                                  included.multiplier = 1,
-#                                                                                  included.multiplier.sd = 0.1232992, # see applications/EHE/ehe_sti_included_multiplier.R
-#                                                                                  included.multiplier.correlation = 0.5,
-#                                                                                  
-#                                                                                  weights = list(1), 
-#                                                                                  equalize.weight.by.year = T 
-#   )
-
-# race.gonorrhea.year.on.year.change.likelihood.instructions = 
-#   create.time.lagged.comparison.likelihood.instructions(outcome.for.data = "gonorrhea.ratio", # zoe changing 0-14 to 13-14, throw out 'unknown'
-#                                                         outcome.for.sim = "sexual.transmission.rates", 
-#                                                         # (2020 gon diagnoses / 2019 gon diagnoses) proportional to 
-#                                                         # (2020 sexual transmisson/2019 sexual transmission)
-#                                                         levels.of.stratification = c(0,1), 
-#                                                         dimensions = c("race"),
-#                                                         from.year = 2008, 
-#                                                         observation.correlation.form = 'compound.symmetry', 
-#                                                         error.variance.term = 0.03, # pick a smarter one
-#                                                         error.variance.type = 'cv',
-#                                                         correlation.different.years = 0.5,
-#                                                         weights = list(1), 
-#                                                         equalize.weight.by.year = T 
-#   )
 
 #-- YEAR-ON-YEAR SYPHILIS CHANGE ----
 ps.syphilis.year.on.year.change.likelihood.instructions = 
@@ -702,52 +661,12 @@ ps.syphilis.year.on.year.change.likelihood.instructions =
                                              error.variance.type = 'cv',
                                              correlation.different.years = 0.5,
                                              
-                                             ratio.cv = 1.5,
+                                             ratio.cv = 1.35,
                                              # ratio.correlation = , # NULL will enter default of 0
                                              
                                              weights = (1*TOTAL.WEIGHT),
                                              equalize.weight.by.year = T 
   )
-
-# ps.syphilis.year.on.year.change.likelihood.instructions = 
-#   create.time.lagged.comparison.likelihood.instructions.with.included.multiplier(outcome.for.data = "ps.syphilis.ratio", 
-#                                                                                  outcome.for.sim = "sexual.transmission.rates", 
-#                                                                                  # (2020 ps diagnoses / 2019 ps diagnoses) proportional to 
-#                                                                                  # (2020 sexual transmisson/2019 sexual transmission)
-#                                                                                  levels.of.stratification = c(0,1), 
-#                                                                                  dimensions = c("sex","race","age"),
-#                                                                                  from.year = 2018, 
-#                                                                                  observation.correlation.form = 'compound.symmetry', 
-#                                                                                  error.variance.term = 0.03, # pick a smarter one
-#                                                                                  error.variance.type = 'cv',
-#                                                                                  correlation.different.years = 0.5,
-#                                                                                  
-#                                                                                  # ratio of HIV transmission (year-on-year) is proportional 
-#                                                                                  # to ratio of ps transmission (year-on-year) 
-#                                                                                  # with best estimate of 1 but can be off by X%
-#                                                                                  included.multiplier = 1,
-#                                                                                  included.multiplier.sd = 0.1232992, # see applications/EHE/ehe_sti_included_multiplier.R
-#                                                                                  included.multiplier.correlation = 0.5,
-#                                                                                  
-#                                                                                  weights = list(1), 
-#                                                                                  equalize.weight.by.year = T 
-#   )
-
-# race.ps.syphilis.year.on.year.change.likelihood.instructions = 
-#   create.time.lagged.comparison.likelihood.instructions(outcome.for.data = "ps.syphilis.ratio", # zoe working on this
-#                                                         outcome.for.sim = "sexual.transmission.rates", # we have to define this outcome
-#                                                         # (2020 ps diagnoses / 2019 ps diagnoses) proportional to 
-#                                                         # (2020 sexual transmisson/2019 sexual transmission)
-#                                                         levels.of.stratification = c(0,1), 
-#                                                         dimensions = c("race"),
-#                                                         from.year = 2008, 
-#                                                         observation.correlation.form = 'compound.symmetry', 
-#                                                         error.variance.term = 0.03, # pick a smarter one
-#                                                         error.variance.type = 'cv',
-#                                                         correlation.different.years = 0.2,
-#                                                         weights = list(1), 
-#                                                         equalize.weight.by.year = T 
-#   )
 
 #-- JOIN THE POPULATION-RELATED LIKELIHOODS --#  ----
 joint.pop.migration.total.trans.likelihood.instructions = 
