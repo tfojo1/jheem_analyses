d74fe7fbf97eb4a529bb92fb210d99cb8d8cd9eb
Zoe Dansky <zoedansky@gmail.com>
2025-02-06 16:20:49 -0500

    adding msa to viral suppression

--- data_processing/ryan.white.data.manager/ryan.white.viral.suppression.R
+++ data_processing/ryan.white.data.manager/ryan.white.viral.suppression.R
@@ -31,6 +31,31 @@ ryan.white.suppression.clean = lapply(ryan.white.suppression, function(file){
       data$location = as.character(data$location)
   }
   
+  if(grepl("_msa", filename)) {
+    data$location = locations::get.location.code(data$`ema.tga`, 'CBSA')
+    
+    data<- data %>%
+      mutate(location = ifelse(`ema.tga` == "Austin", "C.12420", location))%>%
+      mutate(location = ifelse(`ema.tga` == "Charlotte", "C.16740", location))%>%
+      mutate(location = ifelse(`ema.tga` == "Cleveland", "C.17460", location))%>%
+      mutate(location = ifelse(`ema.tga` == "Miami", "C.33100", location))%>%
+      mutate(location = ifelse(`ema.tga` == "Norfolk", "C.47260", location))%>%
+      mutate(location = ifelse(`ema.tga` == "Philadelphia", "C.37980", location))%>%
+      mutate(location = ifelse(`ema.tga` == "Portland", "C.38900", location))%>%
+      mutate(location = ifelse(`ema.tga` == "Washington", "C.47900", location))%>%
+      mutate(location = ifelse(`ema.tga` == "Riverside", "C.40140", location))%>%
+      mutate(location = ifelse(`ema.tga` == "Jacksonville", "C.27260", location))%>%
+      mutate(location = ifelse(`ema.tga` == "Tampa", "C.45300", location))%>%
+      mutate(location = ifelse(`ema.tga` == "Minneapolis", "C.33460", location))%>%
+      mutate(location = ifelse(`ema.tga` == "Las Vegas", "C.29820", location))%>%
+      mutate(location = ifelse(`ema.tga` == "Columbus", "C.18140", location))%>%
+      mutate(location = ifelse(`ema.tga` == "Ft. Worth", "C.49380", location))
+    
+    data$location.check = locations::is.location.valid(data$location)
+    data = subset(data, data$location.check == "TRUE")
+    data$location = as.character(data$location)
+  }
+  
  data$outcome = "non.adap.viral.suppression"
  data$value = (data$value/100)
  data$year = gsub("suppression percent", "", data$year)
