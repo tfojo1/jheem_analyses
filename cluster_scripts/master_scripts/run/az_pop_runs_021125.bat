094affa192dc77f9f027863819408eee59bc1165
A-Zalesak <azalesak@alumni.unc.edu>
2025-02-11 09:38:53 -0500

    make master scripts for failed cities

---
+++ cluster_scripts/master_scripts/run/az_pop_runs_021125.bat
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sbatch cluster_scripts/run_scripts/ehe/C.31080/run_init.pop.ehe_1.bat
+sbatch cluster_scripts/run_scripts/ehe/C.41860/run_init.pop.ehe_1.bat
+sbatch cluster_scripts/run_scripts/ehe/C.38060/run_init.pop.ehe_1.bat
+sbatch cluster_scripts/run_scripts/ehe/C.40140/run_init.pop.ehe_1.bat
+sbatch cluster_scripts/run_scripts/ehe/C.29820/run_init.pop.ehe_1.bat
+sbatch cluster_scripts/run_scripts/ehe/C.41740/run_init.pop.ehe_1.bat
+sbatch cluster_scripts/run_scripts/ehe/C.32820/run_init.pop.ehe_1.bat
+sbatch cluster_scripts/run_scripts/ehe/C.12940/run_init.pop.ehe_1.bat
+sbatch cluster_scripts/run_scripts/ehe/C.40900/run_init.pop.ehe_1.bat
---
+++ cluster_scripts/master_scripts/setup/az_pop_runs_021125.bat
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sbatch cluster_scripts/setup_scripts/ehe/C.31080/setup_init.pop.ehe.bat
+sbatch cluster_scripts/setup_scripts/ehe/C.41860/setup_init.pop.ehe.bat
+sbatch cluster_scripts/setup_scripts/ehe/C.38060/setup_init.pop.ehe.bat
+sbatch cluster_scripts/setup_scripts/ehe/C.40140/setup_init.pop.ehe.bat
+sbatch cluster_scripts/setup_scripts/ehe/C.29820/setup_init.pop.ehe.bat
+sbatch cluster_scripts/setup_scripts/ehe/C.41740/setup_init.pop.ehe.bat
+sbatch cluster_scripts/setup_scripts/ehe/C.32820/setup_init.pop.ehe.bat
+sbatch cluster_scripts/setup_scripts/ehe/C.12940/setup_init.pop.ehe.bat
+sbatch cluster_scripts/setup_scripts/ehe/C.40900/setup_init.pop.ehe.bat
