3bf86ffb96ed1ecb7a554dfc335715acdf0f6b1a
A-Zalesak <azalesak@alumni.unc.edu>
2025-02-13 14:08:49 -0500

    make run master script for four remaining cities

---
+++ cluster_scripts/master_scripts/run/az_pop_runs_021325.bat
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sbatch cluster_scripts/run_scripts/ehe/C.29820/run_init.pop.ehe_1.bat
+sbatch cluster_scripts/run_scripts/ehe/C.41740/run_init.pop.ehe_1.bat
+sbatch cluster_scripts/run_scripts/ehe/C.32820/run_init.pop.ehe_1.bat
+sbatch cluster_scripts/run_scripts/ehe/C.12940/run_init.pop.ehe_1.bat
