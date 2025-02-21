e24f6ba307ad4f5bdda0b1c5d3efa2fdbc4c6112
A-Zalesak <azalesak@alumni.unc.edu>
2025-02-21 10:02:04 -0500

    run a few cities that didn't finish

---
+++ cluster_scripts/master_scripts/run/az_trans_runs_022125.bat
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sbatch cluster_scripts/run_scripts/ehe/C.19100/run_init.transmission.ehe_1.bat
+sbatch cluster_scripts/run_scripts/ehe/C.12420/run_init.transmission.ehe_1.bat
+sbatch cluster_scripts/run_scripts/ehe/C.12940/run_init.transmission.ehe_1.bat
+sbatch cluster_scripts/run_scripts/ehe/C.40900/run_init.transmission.ehe_1.bat
