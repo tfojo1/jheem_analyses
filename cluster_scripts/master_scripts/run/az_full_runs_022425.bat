56d7855f69e5da450b04b8ba318bc4aa8f662000
A-Zalesak <azalesak@alumni.unc.edu>
2025-02-24 13:30:34 -0500

    make new cluster scripts

---
+++ cluster_scripts/master_scripts/run/az_full_runs_022425.bat
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sbatch cluster_scripts/run_scripts/ehe/C.12420/run_full.with.covid2_1.bat
+sbatch cluster_scripts/run_scripts/ehe/C.12940/run_full.with.covid2_1.bat
+sbatch cluster_scripts/run_scripts/ehe/C.19100/run_full.with.covid2_1.bat
+sbatch cluster_scripts/run_scripts/ehe/C.40900/run_full.with.covid2_1.bat
+sbatch cluster_scripts/run_scripts/ehe/C.16980/run_full.with.covid2_1.bat
+sbatch cluster_scripts/run_scripts/ehe/C.18140/run_full.with.covid2_1.bat
+sbatch cluster_scripts/run_scripts/ehe/C.47900/run_full.with.covid2_1.bat
+sbatch cluster_scripts/run_scripts/ehe/C.12060/run_full.with.covid2_1.bat
+sbatch cluster_scripts/run_scripts/ehe/C.26420/run_full.with.covid2_1.bat
+sbatch cluster_scripts/run_scripts/ehe/C.45300/run_full.with.covid2_1.bat
+sbatch cluster_scripts/run_scripts/ehe/C.40140/run_full.with.covid2_1.bat
+sbatch cluster_scripts/run_scripts/ehe/C.19820/run_full.with.covid2_1.bat
---
+++ cluster_scripts/master_scripts/setup/az_setup_runs_022425.bat
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sbatch cluster_scripts/setup_scripts/ehe/C.12420/setup_full.with.covid2.bat
+sbatch cluster_scripts/setup_scripts/ehe/C.12940/setup_full.with.covid2.bat
+sbatch cluster_scripts/setup_scripts/ehe/C.19100/setup_full.with.covid2.bat
+sbatch cluster_scripts/setup_scripts/ehe/C.40900/setup_full.with.covid2.bat
+sbatch cluster_scripts/setup_scripts/ehe/C.16980/setup_full.with.covid2.bat
+sbatch cluster_scripts/setup_scripts/ehe/C.18140/setup_full.with.covid2.bat
+sbatch cluster_scripts/setup_scripts/ehe/C.47900/setup_full.with.covid2.bat
