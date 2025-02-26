1473d0e92c6498a1b6c88261406bdaed30fd04cc
A-Zalesak <azalesak@alumni.unc.edu>
2025-02-25 21:28:10 -0500

    make new run scripts with runtime 72 hrs

--- cluster_scripts/run_scripts/ehe/C.12580/run_full.with.covid2_1.bat
+++ cluster_scripts/run_scripts/ehe/C.12580/run_full.with.covid2_1.bat
@@ -3,7 +3,7 @@
 #SBATCH --job-name=run_ehe_C.12580_full.with.covid2_1
 #SBATCH --mem=16G
 #SBATCH --output=/scratch4/pkasaie1/azalesak/jheem/code/jheem_analyses/cluster_scripts/outputs/ehe/C.12580/run_full.with.covid2_1.out
-#SBATCH --time=12:00:00
+#SBATCH --time=72:00:00
 #SBATCH --partition=shared
 #SBATCH --account=tfojo1
 
--- cluster_scripts/run_scripts/ehe/C.31080/run_full.with.covid2_1.bat
+++ cluster_scripts/run_scripts/ehe/C.31080/run_full.with.covid2_1.bat
@@ -3,7 +3,7 @@
 #SBATCH --job-name=run_ehe_C.31080_full.with.covid2_1
 #SBATCH --mem=16G
 #SBATCH --output=/scratch4/pkasaie1/azalesak/jheem/code/jheem_analyses/cluster_scripts/outputs/ehe/C.31080/run_full.with.covid2_1.out
-#SBATCH --time=12:00:00
+#SBATCH --time=72:00:00
 #SBATCH --partition=shared
 #SBATCH --account=tfojo1
 
