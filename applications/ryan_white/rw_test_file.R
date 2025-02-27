f625a275ebaca4ecf39b71b316e4a2c6a79a8bd9
Ryan Forster <ryanforster@macbookair.mynetworksettings.com>
2025-02-27 15:31:43 -0500

    Added likelihoods

--- applications/ryan_white/rw_test_file.R
+++ applications/ryan_white/rw_test_file.R
@@ -5,6 +5,7 @@ RW.DATA.MANAGER = load.data.manager('../../cached/ryan.white.data.manager.rdata'
 source('applications/ryan_white/ryan_white_specification.R')
 source('applications/ryan_white/ryan_white_mcmc.R')
 
+simset = rerun.simulations(simset)
 
 transmuter = create.jheem.transmuter(simset, to.version='rw')
 
@@ -12,7 +13,21 @@ sim = get.fitted.rw.simulation(sim.transmuter = transmuter,
                                sim.index = simset$n.sim)
 
 simplot(sim, 'non.adap.clients', data.manager = RW.DATA.MANAGER)
+simplot(sim, 'oahs.clients', data.manager = RW.DATA.MANAGER)
+simplot(sim, 'oahs.suppression', data.manager = RW.DATA.MANAGER)
+simplot(sim, 'adap.suppression', data.manager = RW.DATA.MANAGER)
+simplot(sim, 'adap.proportion', data.manager = RW.DATA.MANAGER)
 
+non.adap.lk = rw.non.adap.likelihood.instructions$instantiate.likelihood('rw', sim$location, data.manager = RW.DATA.MANAGER)
+oahs.lk = rw.oahs.likelihood.instructions$instantiate.likelihood('rw', sim$location, data.manager = RW.DATA.MANAGER)
+oahs.suppression.lk = rw.oahs.suppression.likelihood.instructions$instantiate.likelihood('rw', sim$location, data.manager = RW.DATA.MANAGER)
+adap.lk = rw.adap.likelihood.instructions$instantiate.likelihood("rw",sim$location, data.manager = RW.DATA.MANAGER)
+adap.suppression.lk = rw.adap.suppression.likelihood.instructions$instantiate.likelihood("rw",sim$location, data.manager = RW.DATA.MANAGER)
+
+non.adap.lk$compute(sim)
+oahs.lk$compute(sim)
+oahs.suppression.lk$compute(sim)
+adap.lk$compute(sim)
 
 
 # QUICK AND DIRTY UNTIL ZOE GETS DATA UP
---
+++ applications/ryan_white/ryan_white_likelihoods.R
@@ -0,0 +1,166 @@
+source('applications/EHE/ehe_likelihoods.R')
+rw.non.adap.likelihood.instructions = create.basic.likelihood.instructions(
+  outcome.for.data = "non.adap.clients", 
+  outcome.for.sim = "non.adap.clients",
+  dimensions = c("age","sex","race","risk"),
+  levels.of.stratification = c(0,1,2), # 0 = totals, 1 = 1-way stratification
+  from.year = 2010,
+  correlation.different.years = 0.5, # this is the default
+  correlation.different.strata = 0.1, # this is the default
+  correlation.different.sources = 0.3, # default
+  correlation.same.source.different.details = 0.3, # default
+  
+  observation.correlation.form = 'compound.symmetry', 
+  
+  error.variance.term = 0.05,
+  error.variance.type = 'cv',
+  
+  weights = 1,
+  
+  # if there are more datapoints for certain years, this will normalize
+  # e.g., if there are a few years with only the totals 
+  # before the stratifications are available
+  equalize.weight.by.year = T
+)
+
+
+
+rw.oahs.likelihood.instructions = create.basic.likelihood.instructions(
+  outcome.for.data = "oahs.clients", 
+  outcome.for.sim = "oahs.clients",
+  dimensions = c("age","sex","race","risk"),
+  levels.of.stratification = c(0,1,2), # 0 = totals, 1 = 1-way stratification
+  from.year = 2010,
+  correlation.different.years = 0.5, # this is the default
+  correlation.different.strata = 0.1, # this is the default
+  correlation.different.sources = 0.3, # default
+  correlation.same.source.different.details = 0.3, # default
+  
+  observation.correlation.form = 'compound.symmetry', 
+  
+  error.variance.term = 0.05,
+  error.variance.type = 'cv',
+  
+  weights = 1,
+  
+  # if there are more datapoints for certain years, this will normalize
+  # e.g., if there are a few years with only the totals 
+  # before the stratifications are available
+  equalize.weight.by.year = T
+)
+
+rw.oahs.suppression.likelihood.instructions = create.basic.likelihood.instructions(
+  outcome.for.data = "oahs.suppression", 
+  outcome.for.sim = "oahs.suppression",
+  dimensions = c("age","sex","race","risk"),
+  levels.of.stratification = c(0,1,2), # 0 = totals, 1 = 1-way stratification
+  from.year = 2010,
+  correlation.different.years = 0.5, # this is the default
+  correlation.different.strata = 0.1, # this is the default
+  correlation.different.sources = 0.3, # default
+  correlation.same.source.different.details = 0.3, # default
+  
+  observation.correlation.form = 'compound.symmetry', 
+  
+  error.variance.term = 0.05,
+  error.variance.type = 'cv',
+  
+  weights = 1,
+  
+  # if there are more datapoints for certain years, this will normalize
+  # e.g., if there are a few years with only the totals 
+  # before the stratifications are available
+  equalize.weight.by.year = T
+)
+
+
+rw.oahs.suppression.likelihood.instructions = create.basic.likelihood.instructions(
+  outcome.for.data = "oahs.suppression", 
+  outcome.for.sim = "oahs.suppression",
+  dimensions = c("age","sex","race","risk"),
+  levels.of.stratification = c(0,1,2), # 0 = totals, 1 = 1-way stratification
+  from.year = 2010,
+  correlation.different.years = 0.5, # this is the default
+  correlation.different.strata = 0.1, # this is the default
+  correlation.different.sources = 0.3, # default
+  correlation.same.source.different.details = 0.3, # default
+  
+  observation.correlation.form = 'compound.symmetry', 
+  
+  error.variance.term = 0.05,
+  error.variance.type = 'cv',
+  
+  weights = 1,
+  
+  # if there are more datapoints for certain years, this will normalize
+  # e.g., if there are a few years with only the totals 
+  # before the stratifications are available
+  equalize.weight.by.year = T
+)
+
+
+rw.adap.likelihood.instructions = 
+  create.nested.proportion.likelihood.instructions(outcome.for.data = "adap.proportion",
+                                                   outcome.for.sim = "adap.proportion",
+                                                   denominator.outcome.for.data = 'non.adap.clients',
+                                                   
+                                                   location.types = c('STATE','CBSA'), 
+                                                   minimum.geographic.resolution.type = 'COUNTY',
+                                                   location.stratum.keep.threshold = 2, # default
+                                                   
+                                                   dimensions = c("age","sex","race","risk"),
+                                                   
+                                                   levels.of.stratification = c(0,1), 
+                                                   from.year = 2008, 
+                                                   
+                                                   p.bias.inside.location = 0, 
+                                                   p.bias.outside.location = 0,
+                                                   p.bias.sd.inside.location = 0.05,
+                                                   p.bias.sd.outside.location = 0.05,
+                                                   
+                                                   within.location.p.error.correlation = 0.5,
+                                                   within.location.n.error.correlation = 0.5,
+                                                   
+                                                   observation.correlation.form = 'compound.symmetry', 
+                                                   p.error.variance.term = 0.05, # from calculating_error_terms_for_ehe_likelihoods.R
+                                                   p.error.variance.type = 'sd',
+                                                   
+                                                   partitioning.function = EHE.PARTITIONING.FUNCTION, 
+                                                   
+                                                   weights = 1,
+                                                   equalize.weight.by.year = T
+  )
+
+rw.adap.suppression.likelihood.instructions = 
+  create.nested.proportion.likelihood.instructions(outcome.for.data = "adap.suppression",
+                                                   outcome.for.sim = "adap.suppression",
+                                                   denominator.outcome.for.data = 'adap.clients',
+                                                   
+                                                   location.types = c('STATE','CBSA'), 
+                                                   minimum.geographic.resolution.type = 'COUNTY',
+                                                   location.stratum.keep.threshold = 2, # default
+                                                   
+                                                   dimensions = c("age","sex","race","risk"),
+                                                   
+                                                   levels.of.stratification = c(0,1), 
+                                                   from.year = 2008, 
+                                                   
+                                                   p.bias.inside.location = suppression.bias.estimates$in.mean, 
+                                                   p.bias.outside.location = suppression.bias.estimates$out.mean,
+                                                   p.bias.sd.inside.location = suppression.bias.estimates$in.sd,
+                                                   p.bias.sd.outside.location = suppression.bias.estimates$out.sd,
+                                                   
+                                                   within.location.p.error.correlation = 0.5,
+                                                   within.location.n.error.correlation = 0.5,
+                                                   
+                                                   observation.correlation.form = 'compound.symmetry', 
+                                                   p.error.variance.term = 0.05, # from calculating_error_terms_for_ehe_likelihoods.R
+                                                   p.error.variance.type = 'sd',
+                                                   
+                                                   partitioning.function = EHE.PARTITIONING.FUNCTION, 
+                                                   
+                                                   weights = 1,
+                                                   equalize.weight.by.year = T
+  )
+
+
