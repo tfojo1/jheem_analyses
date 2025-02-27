ff7a563f534391c7e2a32e2338a4dccc806fb5e7
Todd Fojo <tfojo1@jh.edu>
2025-02-27 14:16:52 -0500

    updates to cache.objects.for.version - don't need to have version registered now

--- commoncode/cache_object_for_version_functions.R
+++ commoncode/cache_object_for_version_functions.R
@@ -14,19 +14,37 @@ cache.object.for.version <- function(object, name, version, overwrite=F)
 get.cached.object.for.version <- function(name, version)
 {
     BASE.FILE.PATH = '../jheem_analyses/commoncode/object_for_version_cache'
-    for (version.to.try in c(version, get.prior.versions(version)))
-    {
-        error.prefix = "Error getting cached object for version: "
-        FILE.PATH = paste0(BASE.FILE.PATH, "/", version.to.try, "_", name, ".Rdata")
     
-        if (file.exists(FILE.PATH))
-        {
-            load(file=FILE.PATH) # will be called "object"
-            return (object)
-        }
+    error.prefix = "Error getting cached object for version: "
+    FILE.PATH = paste0(BASE.FILE.PATH, "/", version, "_", name, ".Rdata")
+    
+    if (file.exists(FILE.PATH))
+    {
+        load(file=FILE.PATH) # will be called "object"
+        return (object)
     }
     
-    # if we got here, we didn't find anything
-    stop(paste0("Cannot get cached '", name, "' for version '", version, 
-                "' - no object by this name has been cached for the version or any ancestor versions"))
+    if (version.has.been.registered(version))
+    {
+        for (version.to.try in get.prior.versions(version))
+        {
+            FILE.PATH = paste0(BASE.FILE.PATH, "/", version.to.try, "_", name, ".Rdata")
+        
+            if (file.exists(FILE.PATH))
+            {
+                load(file=FILE.PATH) # will be called "object"
+                return (object)
+            }
+        }
+    
+        # if we got here, we didn't find anything
+        stop(paste0("Cannot get cached '", name, "' for version '", version, 
+                    "' - no object by this name has been cached for the version or any ancestor versions"))
+    }
+    else
+    {
+      stop(paste0("Cannot get cached '", name, "' for version '", version, 
+                  "' - no object by this name has been cached for the version. HOWEVER, '",
+                  version, "' has NOT been registered, so we cannot check whether '", name, "' is present in prior versions"))
+    }
 }
