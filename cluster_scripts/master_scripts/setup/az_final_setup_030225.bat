d37ab003e7680138ca5382bfffa50a95cabdb52e
A-Zalesak <azalesak@alumni.unc.edu>
2025-03-02 10:53:16 -0500

    make new setup script for final

---
+++ cluster_scripts/master_scripts/setup/az_final_setup_030225.bat
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sbatch cluster_scripts/setup_scripts/ehe/C.33100/setup_final.bat
+sbatch cluster_scripts/setup_scripts/ehe/C.12060/setup_final.bat
+sbatch cluster_scripts/setup_scripts/ehe/C.19100/setup_final.bat
+sbatch cluster_scripts/setup_scripts/ehe/C.36740/setup_final.bat
+sbatch cluster_scripts/setup_scripts/ehe/C.29820/setup_final.bat
+sbatch cluster_scripts/setup_scripts/ehe/C.14460/setup_final.bat
+sbatch cluster_scripts/setup_scripts/ehe/C.16740/setup_final.bat
+sbatch cluster_scripts/setup_scripts/ehe/C.32820/setup_final.bat
+sbatch cluster_scripts/setup_scripts/ehe/C.42660/setup_final.bat
+sbatch cluster_scripts/setup_scripts/ehe/C.12420/setup_final.bat
+sbatch cluster_scripts/setup_scripts/ehe/C.18140/setup_final.bat
