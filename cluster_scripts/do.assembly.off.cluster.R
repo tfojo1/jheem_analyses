f474ccc4935101e696ad42d79a95e72615eee62e
A-Zalesak <azalesak@alumni.unc.edu>
2025-03-04 16:26:27 -0500

    make assembly loop do try catch

--- cluster_scripts/do.assembly.off.cluster.R
+++ cluster_scripts/do.assembly.off.cluster.R
@@ -12,20 +12,20 @@ do.assembly.off.cluster <- function(output.dir,
     source(register.calibration.path)
     
     for (location in locations) {
-        
-        print(paste0("Assembling mcmc for '", version, "', '", location, "', '", calibration.code, "'"))
-        simset = assemble.simulations.from.calibration(version = version,
-                                                       location = location,
-                                                       calibration.code = calibration.code)
-        
-        print(paste0("Burning/thinning..."))
-        if (burn.keep!=0)
-            simset = simset$burn(keep = burn.keep) #0.5
-        if (thin.keep!=0)
-            simset = simset$thin(keep = thin.keep)
-        
-        print(paste0("Saving..."))
-        save(simset,file=paste0("../../files/run_results/",output.dir, "/",calibration.code,"_simset_",Sys.Date(),"_",location,".Rdata"))
+        tryCatch({print(paste0("Assembling mcmc for '", version, "', '", location, "', '", calibration.code, "'"))
+            simset = assemble.simulations.from.calibration(version = version,
+                                                           location = location,
+                                                           calibration.code = calibration.code)
+            
+            print(paste0("Burning/thinning..."))
+            if (burn.keep!=0)
+                simset = simset$burn(keep = burn.keep) #0.5
+            if (thin.keep!=0)
+                simset = simset$thin(keep = thin.keep)
+            
+            print(paste0("Saving..."))
+            save(simset,file=paste0("../../files/run_results/",output.dir, "/",calibration.code,"_simset_",Sys.Date(),"_",location,".Rdata"))
+        }, error=function(e) {print(paste0("Error: skipping location '", location, "'"))})
         
     }
     print("Done with all locations!")
