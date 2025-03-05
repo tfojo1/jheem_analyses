a2584d91619d2ec7c6573430ddcae08bca606ccf
Andrew Zalesak <azalesa1@jh.edu>
2025-03-05 11:58:23 -0500

    add documentation

--- cluster_scripts/do.assembly.off.cluster.R
+++ cluster_scripts/do.assembly.off.cluster.R
@@ -1,8 +1,8 @@
-#' @param output.dir Directory within "files/run_results"
+#' @param output.dir Directory within "files/run_results". "run_results" may need to be created.
 do.assembly.off.cluster <- function(output.dir,
                                     locations,
-                                    version="ehe",
                                     calibration.code,
+                                    version="ehe",
                                     burn.keep=150,
                                     thin.keep=0,
                                     specification.path="../jheem_analyses/applications/EHE/ehe_specification.R",
@@ -31,6 +31,9 @@ do.assembly.off.cluster <- function(output.dir,
     print("Done with all locations!")
 }
 
+#' @details You probably need to source "../jheem2/R/FILE_MANAGER_file_manager.R" and call set.jheem.root.directory before using this.
+#' @param dir Directory within "files/run_results" where the assembled simsets exist locally.
+#' @param date The same format as Sys.date(), which was the date the simsets were assembled.
 do.save.simsets <- function(locations,
                             version="ehe",
                             calibration.code="full.with.covid2",
@@ -41,10 +44,12 @@ do.save.simsets <- function(locations,
         
         tryCatch({
             print(paste0("Saving location '", location, "'"))
-            filename = paste0("../../files/run_results/",calibration.code,"_simset_",Sys.Date(),"_",location,".Rdata")
+            filename = paste0("../../files/",dir, "/",calibration.code,"_simset_",date,"_",location,".Rdata")
             simset = get(load(filename))
             simset$save()
-        }, error=function(e) {print(paste0("Error: skipping location '", location, "'"))})
+        },
+        # }, error=function(e) {browser()})
+        error=function(e) {print(paste0("Error: skipping location '", location, "'"))})
         
     }
     print("All done!")
