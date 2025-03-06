d844274504d6466755f1cd39f225044a3746e1dc
Zoe Dansky <zoedansky@gmail.com>
2025-03-06 10:44:04 -0500

    adding race mapping for ryan white data

--- applications/EHE/ehe_ontology_mappings.R
+++ applications/EHE/ehe_ontology_mappings.R
@@ -172,3 +172,17 @@ register.ontology.mapping('aidsvu.to.jheem.race',
                           mappings = rbind(c('black', 'black'),
                                            c('hispanic', 'hispanic'),
                                            c('white', 'other')))
+
+#-----------------#
+#-- For RYAN WHITE --#
+#-----------------#
+
+register.ontology.mapping('ryan.white.to.jheem.race',
+                          from.dimensions = 'race',
+                          to.dimensions = 'race',
+                          mappings = rbind(c('black', 'black'),
+                                           c('hispanic', 'hispanic'),
+                                           c('white', 'other'),
+                                           c('american indian alaska native', 'other'),
+                                           c('native hawaiian pacific islander', 'other'),
+                                           c('asian', 'other')))
