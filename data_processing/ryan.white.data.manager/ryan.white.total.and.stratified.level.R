ce3abcde862562a396d3278ddb04238d6314fb24
Zoe Dansky <zoedansky@gmail.com>
2025-03-13 13:21:20 -0400

    correcting error for dallas- removing 2022 not 2019

--- data_processing/ryan.white.data.manager/ryan.white.total.and.stratified.level.R
+++ data_processing/ryan.white.data.manager/ryan.white.total.and.stratified.level.R
@@ -488,7 +488,7 @@ ryan.white.stratified.msa = lapply(ryan.white.pdf.reports.stratified.msa, functi
     
     #Manually remove Dallas MSA (they did not report data for 2022 by race but Fort Worth did)
     data<-data%>%
-      mutate(drop_var = ifelse(location == "C.19100" & year == "2019", "1", "0"))%>%
+      mutate(drop_var = ifelse(location == "C.19100" & year == "2022", "1", "0"))%>%
       filter(drop_var != "1")
   }
 
