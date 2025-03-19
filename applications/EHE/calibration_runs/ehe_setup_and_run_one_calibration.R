38916d42fd1cbc4ad2d9238ecd4159f0f1b3db5e
Todd Fojo <tfojo1@jh.edu>
2025-03-19 13:43:17 -0400

    updates to set up and run script

--- applications/EHE/calibration_runs/ehe_setup_and_run_one_calibration.R
+++ applications/EHE/calibration_runs/ehe_setup_and_run_one_calibration.R
@@ -1,15 +1,16 @@
+LOCATION.INDEX = 26
+
 source('../jheem_analyses/applications/EHE/calibration_runs/ehe_register_calibrations.R')
 source('../jheem_analyses/commoncode/locations_of_interest.R')
 
 LOCATION.LIST = setdiff(MSAS.OF.INTEREST, c('C.17140','C.44180','C.12580'))
-LOCATION.INDEX = 15
 LOCATION = LOCATION.LIST[LOCATION.INDEX]
 print(paste0("Running for ", LOCATION, " - ", locations::get.location.name(LOCATION)))
 CALIBRATION.CODES.TO.RUN = c(CALIBRATION.CODE.POPULATION, # 1
                              CALIBRATION.CODE.TRANSMISSION, # 2
                              CALIBRATION.CODE.FULL.PLUS.COVID, # 3
                              CALIBRATION.CODE.EHE.FINAL # 4
-                             )[c(3,4)]
+                             )[1:3]
 
 #CALIBRATION.CODE.TO.RUN = CALIBRATION.CODES.TO.RUN
 
