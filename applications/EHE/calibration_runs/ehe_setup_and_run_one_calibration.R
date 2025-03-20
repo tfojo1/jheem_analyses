cafd14172b9c10f403003a70fb787f93d57ed0b3
Todd Fojo <tfojo1@jh.edu>
2025-03-19 23:36:14 -0400

    changed setup and run code to resume first instead of restarting it from beginning

--- applications/EHE/calibration_runs/ehe_setup_and_run_one_calibration.R
+++ applications/EHE/calibration_runs/ehe_setup_and_run_one_calibration.R
@@ -1,41 +1,48 @@
-LOCATION.INDEX = 1#23
+LOCATION.INDEX = 28
 
 source('../jheem_analyses/applications/EHE/calibration_runs/ehe_register_calibrations.R')
 source('../jheem_analyses/commoncode/locations_of_interest.R')
 
-#LOCATION.LIST = setdiff(MSAS.OF.INTEREST, c('C.17140','C.44180','C.12580'))
-LOCATION.LIST = c(MIAMI.MSA, BALTIMORE.MSA, NYC.MSA, ATLANTA.MSA, HOUSTON.MSA, CHICAGO.MSA)
-LOCATION = LOCATION.LIST[LOCATION.INDEX]
+LOCATION.LIST = setdiff(MSAS.OF.INTEREST, c('C.17140','C.44180','C.12580'))
+#LOCATION.LIST = c(MIAMI.MSA, BALTIMORE.MSA, NYC.MSA, ATLANTA.MSA, HOUSTON.MSA, CHICAGO.MSA)
+#LOCATION = LOCATION.LIST[LOCATION.INDEX]
+
 print(paste0("Running for ", LOCATION, " - ", locations::get.location.name(LOCATION)))
 CALIBRATION.CODES.TO.RUN = c(CALIBRATION.CODE.POPULATION, # 1
                              CALIBRATION.CODE.TRANSMISSION, # 2
                              CALIBRATION.CODE.FULL.PLUS.COVID, # 3
                              CALIBRATION.CODE.EHE.FINAL # 4
-                             )[3]
+                             )[2:3]
+
+RESUME.FIRST = T
 
 #CALIBRATION.CODE.TO.RUN = CALIBRATION.CODES.TO.RUN
 
 for (CALIBRATION.CODE.TO.RUN in CALIBRATION.CODES.TO.RUN)
 {   
-    simset = NULL
-
-    gc()
+    if (!RESUME.FIRST)
+    {
+        simset = NULL
     
-    set.seed(12345)
-    
-    print(paste0("Setting up ", LOCATION, " (", locations::get.location.name(LOCATION), ")"))
-    
-    clear.calibration.cache(version='ehe',
-                            location=LOCATION,
-                            calibration.code = CALIBRATION.CODE.TO.RUN,
-                            allow.remove.incomplete = T)
-    
-    set.up.calibration(version='ehe',
-                       location=LOCATION,
-                       calibration.code = CALIBRATION.CODE.TO.RUN,
-                       cache.frequency = 250)  
-    
-    print(paste0("DONE setting up ", LOCATION, " (", locations::get.location.name(LOCATION), ")"))
+        gc()
+        
+        set.seed(12345)
+        
+        print(paste0("Setting up ", LOCATION, " (", locations::get.location.name(LOCATION), ")"))
+        
+        clear.calibration.cache(version='ehe',
+                                location=LOCATION,
+                                calibration.code = CALIBRATION.CODE.TO.RUN,
+                                allow.remove.incomplete = T)
+        
+        set.up.calibration(version='ehe',
+                           location=LOCATION,
+                           calibration.code = CALIBRATION.CODE.TO.RUN,
+                           cache.frequency = 250)  
+        
+        print(paste0("DONE setting up ", LOCATION, " (", locations::get.location.name(LOCATION), ")"))
+    }
+  
     start.time = Sys.time()
     
     print(ggplot2::qplot(1,1) + 
