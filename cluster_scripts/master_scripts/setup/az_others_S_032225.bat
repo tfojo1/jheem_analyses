0195abf23a37c71d8ff88c4b68128ac7abd0cec7
A-Zalesak <azalesak@alumni.unc.edu>
2025-03-22 11:57:57 -0400

    make master scripts for other cities

---
+++ cluster_scripts/master_scripts/setup/az_others_S_032225.bat
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sbatch cluster_scripts/setup_scripts/ehe/C.33100/setup_final.ehe.bat
+sbatch cluster_scripts/setup_scripts/ehe/C.31080/setup_final.ehe.bat
+sbatch cluster_scripts/setup_scripts/ehe/C.40140/setup_final.ehe.bat
+sbatch cluster_scripts/setup_scripts/ehe/C.19820/setup_final.ehe.bat
+sbatch cluster_scripts/setup_scripts/ehe/C.29820/setup_final.ehe.bat
+sbatch cluster_scripts/setup_scripts/ehe/C.14460/setup_final.ehe.bat
+sbatch cluster_scripts/setup_scripts/ehe/C.41740/setup_final.ehe.bat
+sbatch cluster_scripts/setup_scripts/ehe/C.16740/setup_final.ehe.bat
+sbatch cluster_scripts/setup_scripts/ehe/C.41700/setup_final.ehe.bat
