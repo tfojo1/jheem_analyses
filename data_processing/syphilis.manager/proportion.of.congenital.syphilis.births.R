66c119cbedac858c30e9fcdcf6fe31ec47bcde85
Zoe Dansky <zoedansky@gmail.com>
2025-03-26 14:08:40 -0400

    pushing updates for congenital syphilis

--- data_processing/syphilis.manager/proportion.of.congenital.syphilis.births.R
+++ data_processing/syphilis.manager/proportion.of.congenital.syphilis.births.R
@@ -1,23 +1,75 @@
 #This code pulls LHD and Atlas Plus data from Section 2 and birth data from Section 3 to create a proportion of congenital syphilis births
-
 #This is available at 2 geographies: state and MSA
-
 #For state data, the proportion = (# state level congenital syphilis births from Atlas Plus) / (# state level births)
-
 #For MSA data, the proportion = (# MSA level congenital syphilis births from LHD data) / (# MSA level births)
 
+#Create outcome for births:
+data.manager$register.outcome(
+  'births.denominator.for.congenital.syphilis.proportion',
+  metadata = create.outcome.metadata(
+    scale = 'non.negative.number',
+    display.name = 'Births- Denominator for Congenital Syphilis Proportion',
+    axis.name = 'Births- Denominator for Congenital Syphilis Proportion',
+    units = 'births',
+    description = "Births- Denominator for Congenital Syphilis Proportion"))
 
-# First add birth data (this is from CDC Wonder, by county, state, --------
+
+# First add birth data (this is from CDC Wonder, by county, state, this is the Denominator) --------
 
 DATA.DIR.BIRTHS="../../data_raw/syphilis.manager/births.for.congenital.syphilis.proportion"
 
 births.files <- Sys.glob(paste0(DATA.DIR.BIRTHS, '/*.xlsx'))
 
 birth.data <- lapply(births.files, function(x){
-  list(filename=x, data=read_excel(x))
-})
+  list(filename=x, data=read_excel(x))})
+
 
 birth.data.clean = lapply(birth.data, function(file){
+  
+  data=file[["data"]]
+  filename = file[["filename"]]
+  
+  if(grepl("national", filename)) {
+    data$location = "US"
+  }
+  
+  if(grepl("state", filename)) {
+    data$location <- state.abb[match(data$State, state.name)]
+  }
+  
+  if(grepl("county", filename)) {
+    data$location= str_pad(data$`County Code`, width=5, side="left", pad="0")
+    data$location = as.character(data$location)
+  }
+  
+  data$outcome = 'births.denominator.for.congenital.syphilis.proportion'
+  data$year = as.character(data$Year)
+  data$value = data$Births
+  
+  data <- data %>% select(year, location, outcome, value)
+  
+  data = as.data.frame(data)
+  
+  list(filename, data) 
+  
+})
+  
+birth.data.clean.put = lapply(birth.data.clean, `[[`, 2)  
+
+for (data in lhd.data.clean.put) {
+  
+  data.manager$put.long.form(
+    data = data,
+    ontology.name = 'cdc.fertility',
+    source = 'cdc.wonder.natality',
+    dimension.values = list(),
+    url = 'na',
+    details = 'https://wonder.cdc.gov/natality.html')
+}
+
+# LHD Data Processing (For MSA Level, numerator) --------------------------
+
+lhd.data.clean = lapply(lhd.data, function(file){
   
   data=file[["data"]]
   filename = file[["filename"]]
