1d0178da7b54b507d827106294d5f4d8e71d77b9
Zoe Dansky <zoedansky@gmail.com>
2025-04-02 17:19:08 -0400

    updating for lhd data and proportion of congenital syphilis births

--- data_processing/syphilis.manager/local.health.department.syphilis.data.R
+++ data_processing/syphilis.manager/local.health.department.syphilis.data.R
@@ -1,5 +1,8 @@
 #This code processes data that was manually pulled from health department websites:
 
+#I'm removing any LHDs that did not report what counties make up their data points
+#I'm combining primary and secondary syphilis into ps.syphilis
+
 DATA.DIR.LHD="../../data_raw/syphilis.manager/lhd"
 
 lhd.files <- Sys.glob(paste0(DATA.DIR.LHD, '/*.xlsx'))
@@ -18,11 +21,14 @@ lhd.data.clean = lapply(lhd.data, function(file){
   data <- data %>%
     mutate(year = as.character(year))%>%
     filter(LHD.Value != "Not Available")%>%
+      filter(`Counties Included in MSA (per the data)` != 'Not stated')%>% #remove counties that don't report what counties their data represents
     mutate(value = as.numeric(LHD.Value))%>%
-    filter(outcome != "cns.syphilis.diagnoses")%>%  #removing these for now
-    filter(outcome != "primary.syphilis.diagnoses")%>%
-    filter(outcome != "secondary.syphilis.diagnoses")
- 
+    filter(outcome != "cns.syphilis.diagnoses")%>%  #removing neurosyphilis for now
+    select(outcome, location, year, value)%>%
+    mutate(outcome = ifelse(outcome == "primary.syphilis.diagnoses" | outcome == "secondary.syphilis.diagnoses", "ps.syphilis.diagnoses", outcome))%>% #combining primary+secondary diagnoses into ps.syphilis
+    group_by(year, location, outcome)%>%
+     mutate(regrouped.value = sum(value))
+      
    data = as.data.frame(data)
   
    list(filename, data) 
@@ -43,3 +49,4 @@ for (data in lhd.data.clean.put) {
     url = 'na',
     details = 'Data pulled from Local Health Department websites')
 }
+
--- data_processing/syphilis.manager/proportion.of.congenital.syphilis.births.R
+++ data_processing/syphilis.manager/proportion.of.congenital.syphilis.births.R
@@ -1,10 +1,14 @@
 #This code pulls LHD and Atlas Plus data from Section 2 and birth data from Section 3 to create a proportion of congenital syphilis births
 #This is available at 2 geographies: state and MSA
+
 #For state data, the proportion = (# state level congenital syphilis births from Atlas Plus) / (# state level births)
 #For MSA data, the proportion = (# MSA level congenital syphilis births from LHD data) / (# MSA level births)
 
-#Create outcome for births:
-data.manager$register.outcome(
+#source = cdc.sti (state) ; lhd (msa)
+#ontology =  cdc.sti (this doesn't really matter bc it's not stratified data)
+
+# Register New Outcomes and Sources ---------------------------------------
+syphilis.manager$register.outcome(
   'births.denominator.for.congenital.syphilis.proportion',
   metadata = create.outcome.metadata(
     scale = 'non.negative.number',
@@ -12,6 +16,15 @@ data.manager$register.outcome(
     axis.name = 'Births- Denominator for Congenital Syphilis Proportion',
     units = 'births',
     description = "Births- Denominator for Congenital Syphilis Proportion"))
+
+syphilis.manager$register.outcome(
+    'proportion.of.congenital.syphilis.births',
+    metadata = create.outcome.metadata(
+        scale = 'proportion',
+        display.name = 'Proportion of Congenital Syphilis Births',
+        axis.name = 'Proportion of Congenital Syphilis Births',
+        units = '%',
+        description = "Proportion of Congenital Syphilis Births"), denominator.outcome = 'births.denominator.for.congenital.syphilis.proportion')
 
 
 # First add birth data (this is from CDC Wonder, by county, state, this is the Denominator) --------
@@ -35,11 +48,15 @@ birth.data.clean = lapply(birth.data, function(file){
   
   if(grepl("state", filename)) {
     data$location <- state.abb[match(data$State, state.name)]
+    data$location = ifelse(data$State == "District of Columbia", "DC", data$location)
   }
   
   if(grepl("county", filename)) {
     data$location= str_pad(data$`County Code`, width=5, side="left", pad="0")
     data$location = as.character(data$location)
+    data$flag = ifelse(grepl("Unidentified", data$County, ignore.case = T), "1", "0") #Creating flag to identify the 'unidentified counties'
+    data = subset(data, data$flag != "1")#removing unidentified counties
+    data = subset(data, data$location != "12025") #removing invalid location
   }
   
   data$outcome = 'births.denominator.for.congenital.syphilis.proportion'
@@ -56,84 +73,76 @@ birth.data.clean = lapply(birth.data, function(file){
   
 birth.data.clean.put = lapply(birth.data.clean, `[[`, 2)  
 
-for (data in lhd.data.clean.put) {
+for (data in birth.data.clean.put) {
   
-  data.manager$put.long.form(
+  syphilis.manager$put.long.form(
     data = data,
     ontology.name = 'cdc.fertility',
     source = 'cdc.wonder.natality',
     dimension.values = list(),
-    url = 'na',
-    details = 'https://wonder.cdc.gov/natality.html')
+    url = 'https://wonder.cdc.gov/natality.html',
+    details = 'CDC Wonder Fertility Data')
 }
+
+
+# Aggregate births to MSA level -------------------------------------------
+source('commoncode/locations_of_interest.R') #Source locations of interest to create MSA vectors
+source('../jheem2/R/HELPERS_array_helpers.R') 
+
+source('data_processing/put_msa_data_as_new_source_script.R') #This aggregates county level data to other locations
+
+put.msa.data.as.new.source(outcome = 'births.denominator.for.congenital.syphilis.proportion',
+                           from.source.name= 'cdc.wonder.natality',
+                           to.source.name = 'cdc.aggregated.county',
+                           to.locations = MSAS.OF.INTEREST,
+                           geographic.type.from = 'COUNTY',
+                           geographic.type.to = 'CBSA',
+                           details.for.new.data = 'estimated from county data',
+                           aggregate.counts.with.whatever.we.have = T,
+                           data.manager= syphilis.manager)
+
 
 # LHD Data Processing (For MSA Level, numerator) --------------------------
 
-lhd.data.clean = lapply(lhd.data, function(file){
-  
-  data=file[["data"]]
-  filename = file[["filename"]]
-  
-  data <- data %>%
-    mutate(year = as.character(year))%>%
-    filter(LHD.Value != "Not Available")%>%
-    mutate(value = as.numeric(LHD.Value))%>%
-    filter(outcome != "cns.syphilis.diagnoses")%>%  #removing these for now
-    filter(outcome != "primary.syphilis.diagnoses")%>%
-    filter(outcome != "secondary.syphilis.diagnoses")
-  
-  data = as.data.frame(data)
-  
-  list(filename, data) 
-  
-})
+msa.congenital = as.data.frame.table(syphilis.manager$data$congenital.syphilis.diagnoses$estimate$lhd$cdc.sti$year__location, stringsAsFactors = F)%>% rename(msa.congenital.cases = Freq)%>% filter(!is.na(msa.congenital.cases))
 
-lhd.data.clean.put = lapply(lhd.data.clean, `[[`, 2)  
+msa.births = as.data.frame.table(syphilis.manager$data$births.denominator.for.congenital.syphilis.proportion$estimate$cdc.aggregated.county$cdc.fertility$year__location)%>% rename(count.msa.births = Freq)
 
-for (data in lhd.data.clean.put) {
-  
-  data.manager$put.long.form(
-    data = data,
-    ontology.name = 'cdc.sti',
-    source = 'lhd',
-    dimension.values = list(),
-    url = 'na',
-    details = 'Data pulled from Local Health Department websites')
-}
+all.msa.congenital.data = left_join(msa.congenital, msa.births, by = c("location", "year"))
+
+all.msa.congenital.data <- all.msa.congenital.data %>%
+    mutate(value = msa.congenital.cases/count.msa.births)%>%
+    mutate(outcome = 'births.denominator.for.congenital.syphilis.proportion')
 
 
-# Register New Outcomes and Sources ---------------------------------------
-data.manager$register.outcome(
-  'proportion.of.congenital.syphilis.births', 
-  metadata = create.outcome.metadata(
-    scale = 'proportion',
-    display.name = 'Proportion of Congenital Syphilis Births',
-    axis.name = 'Proportion of Congenital Syphilis Births',
-    units = '%',
-    description = "Proportion of Congenital Syphilis Births"), denominator.outcome = 'FILL IN THIS PART') 
+# Data Processing, pull existing data (For State Level, numerator)  ---------------------------------------------------------------
 
-# Pull data ---------------------------------------------------------------
-#STATE
-state.congenital = as.data.frame.table(syphilis.manager$data$congenital.syphilis.diagnoses$estimate$cdc.sti$cdc.sti$year__location)%>% rename(state.congenital.cases = Freq)
+state.congenital = as.data.frame.table(syphilis.manager$data$congenital.syphilis.diagnoses$estimate$cdc.sti$cdc.sti$year__location, stringsAsFactors = F)%>% rename(state.congenital.cases = Freq)
 
-state.births = as.data.frame.table()
+state.births = as.data.frame.table(syphilis.manager$data$births.denominator.for.congenital.syphilis.proportion$estimate$cdc.wonder.natality$cdc.fertility$year__location, stringsAsFactors = F)%>% rename(count.state.births = Freq)
 
-#MSA
-msa.congenital = as.data.frame.table()
+all.state.congenital.data = left_join(state.congenital, state.births, by = c("location", "year"))
 
-msa.births = as.data.frame.table(syphilis.manager$data)
-
-
-# Calculate Proportion ----------------------------------------------------
-
+all.state.congenital.data <- all.state.congenital.data %>%
+    mutate(value = state.congenital.cases/count.state.births)%>%
+    mutate(outcome = 'births.denominator.for.congenital.syphilis.proportion')
 
 
 # Put ---------------------------------------------------------------------
+#I'm going to put the MSA data separate from the state data since they have different sources
 
-# WHAT SHOULD THESE DETAILS BE?
+# Put state
 syphilis.manager$put.long.form(
-  data = data,
+  data = all.state.congenital.data,
   ontology.name = 'cdc.sti',
-  source = '',
-  url = '',
-  details = '')
+  source = 'cdc.sti',
+  url = 'https://www.cdc.gov/nchhstp/about/atlasplus.html',
+  details = 'Congenital Syphilis cases by state is from Atlas Plus (numerator of proportion); births are from CDC Wonder (denominator for proportion)')
+
+#Put msa
+syphilis.manager$put.long.form(
+    data = all.msa.congenital.data,
+    ontology.name = 'cdc.sti',
+    source = 'lhd',
+    url = 'https://health.maryland.gov/phpa/OIDPCS/CSTIP/Pages/sti-data-statistics.aspx', #There's not a single URL for the LHD data
+    details = 'Congenital Syphilis cases by MSA is from various local health department websites (numerator of proportion); births are from CDC Wonder (denominator for proportion)')
