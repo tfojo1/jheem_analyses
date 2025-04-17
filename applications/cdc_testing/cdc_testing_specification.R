edf05ddb4f76d11007cbd5d9ece82038eeb7bec7
ruchitab1997 <rbalasubramanian@g.harvard.edu>
2025-04-17 19:48:35 -0400

    Updated specifications

--- applications/cdc_testing/cdc_testing_specification.R
+++ applications/cdc_testing/cdc_testing_specification.R
@@ -8,6 +8,25 @@ CDCT.SPECIFICATION = create.jheem.specification(version='cdct',
                                               parent.version = 'ehe')
 
 
+register.model.quantity(CDCT.SPECIFICATION, name = "testing.of.undiagnosed",
+                        value = expression(cdc.funded.testing.of.undiagnosed*cdc.effect+cdc.nonfunded.testing.of.undiagnosed))
+
+register.model.element(CDCT.SPECIFICATION, name = "cdc.effect",
+                       value = 1,
+                       scale = "proportion")
+
+register.model.quantity(CDCT.SPECIFICATION, name = "cdc.funded.testing.of.undiagnosed",
+                       value = expression(super.testing.of.undiagnosed*fraction.diagnoses.from.cdc))
+
+register.model.quantity(CDCT.SPECIFICATION, name = "cdc.nonfunded.testing.of.undiagnosed",
+                        value = expression(super.testing.of.undiagnosed*(1-fraction.diagnoses.from.cdc)))
+
+register.model.element(CDCT.SPECIFICATION, name = "fraction.diagnoses.from.cdc",
+                       value = create.logistic.linear.functional.form(intercept = log(0.25)-log(0.75), slope = 0, anchor.year = 2020, parameters.are.on.logit.scale = TRUE),
+                       scale = "proportion")
+
+
+
 ##-- REGISTER IT! --##
 
 register.model.specification(CDCT.SPECIFICATION)
--- use_jheem2_package_setting.R
+++ use_jheem2_package_setting.R
@@ -2,4 +2,4 @@
 # This file just provides a quick switch for whether we should use
 # JHEEM2 code from the package or from the source code
 
-USE.JHEEM2.PACKAGE = T
+USE.JHEEM2.PACKAGE = F
