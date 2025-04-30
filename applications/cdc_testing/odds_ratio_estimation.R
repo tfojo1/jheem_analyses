66a9cd837eec62374319e31d7d172c9a02d04bb9
ruchitab24 <ruchita.balasubramanian@gmail.com>
2025-04-30 16:27:22 -0400

    Updated odds ratio logistic regression

--- applications/cdc_testing/odds_ratio_estimation.R
+++ applications/cdc_testing/odds_ratio_estimation.R
@@ -4,8 +4,8 @@ df <- read.csv("cdc_diagnoses.csv")
 
 #Set reference categories
 
-df$Race <- relevel(factor(df$Race), ref = "all1")
-df$Age <- relevel(factor(df$Age), ref = "all2")
+df$Race <- relevel(factor(df$Race), ref = "all")
+df$Age <- relevel(factor(df$Age), ref = "all")
 
 #Logistic regression
 oddsratio_model <- glm(Diagnoses ~ as.factor(Race) + as.factor(Age) + as.factor(Year), data = df, weights = Number, family = binomial(link = "logit"))
