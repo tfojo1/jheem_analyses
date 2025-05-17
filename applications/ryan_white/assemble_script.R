7d70cbdd40c3fa306f78f9acf8c59a39d42a9bda
Todd Fojo <tfojo1@jh.edu>
2025-05-16 22:51:50 -0400

    assemble script

---
+++ applications/ryan_white/assemble_script.R
@@ -0,0 +1,19 @@
+loc = 'MS'
+
+full.simset = assemble.simulations.from.calibration('ehe',loc,'final.ehe.state')
+
+simplot(full.simset, 'new')
+
+new.2030 = full.simset$get('new', year=2030)
+mean(new.2030<250)
+
+# simset = full.simset[new.2030<300]
+# simset$n.sim
+
+simset = full.simset$burn(1000)
+
+simplot(simset, 'new')
+
+
+
+simset$save()
