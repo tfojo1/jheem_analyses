713242af2345161ca5c58bca09019a5d6115391f
Parastu <pkasaie@jhu.edu>
2025-05-23 11:23:17 -0400

    excluding calib params for 1970

--- applications/SHIELD/shield_calib_parameters.R
+++ applications/SHIELD/shield_calib_parameters.R
@@ -109,8 +109,8 @@ AGING.PARAMETERS.PRIOR=join.distributions(
 
 ## TRANSMISSION.PARAMETERS.PRIOR ----
 TRANSMISSION.PARAMETERS.PRIOR=join.distributions( 
-  initial.infection.multiplier.1970.early = Lognormal.Distribution(meanlog = 0, sdlog = 0.5*log(2)), #ps and EL
-  initial.infection.multiplier.1970.late = Lognormal.Distribution(meanlog = 0, sdlog = 0.5*log(2)), #ll and tertiary
+  # initial.infection.multiplier.1970.early = Lognormal.Distribution(meanlog = 0, sdlog = 0.5*log(2)), #ps and EL
+  # initial.infection.multiplier.1970.late = Lognormal.Distribution(meanlog = 0, sdlog = 0.5*log(2)), #ll and tertiary
   
   ## Transmission
   global.transmission.rate = Lognormal.Distribution(meanlog = log(3.5), sdlog = 0.5*log(2)), #directly used in specification (will need sth uch larger) 
@@ -482,9 +482,9 @@ SHIELD.AGING.SAMPLING.BLOCKS = list(
 
 ## SHIELD.TRANSMISSION.SAMPLING.BLOCKS ----
 SHIELD.TRANSMISSION.SAMPLING.BLOCKS = list(
-  initial.infections = c(
-    "initial.infection.multiplier.1970.early",
-    "initial.infection.multiplier.1970.late"), 
+  # initial.infections = c(
+  #   "initial.infection.multiplier.1970.early",
+  #   "initial.infection.multiplier.1970.late"), 
   #
   global.transmission.rate=c("global.transmission.rate"),
   #
