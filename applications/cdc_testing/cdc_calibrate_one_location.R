afc36985f2549f94a56f77553c899f70a9e27fa9
ruchitab24 <62656133+ruchitab24@users.noreply.github.com>
2025-06-02 14:30:17 -0400

    Add files via upload
    update cdc testing

---
+++ applications/cdc_testing/cdc_calibrate_one_location.R
@@ -0,0 +1,16 @@
+
+source("applications/cdc_testing/cdc_testing_main.R")
+
+LOCATION = "AL"
+
+set.up.transmute.calibration('cdct', 
+                             from.calibration.code = 'final.ehe.state', 
+                             location = LOCATION, 
+                             n.chunks = N.CHUNKS,
+                             n.sim = N.SIMS,
+                             allow.overwrite.cache = TRUE,
+                             return.simulations = F)
+
+run.transmute.calibration("cdct",location = LOCATION, n.sim = N.SIMS, chunks = 1:N.CHUNKS,verbose =  T)
+
+
