cece5cd48a75f9243a905a02e2f5aab74fcadaaf
Lauren Zalla <zalla@unc.edu>
2025-06-03 11:48:06 -0400

    save results

--- applications/ehe_disparities/ehe_disparities_analysis.R
+++ applications/ehe_disparities/ehe_disparities_analysis.R
@@ -27,7 +27,7 @@ source("../jheem_analyses/applications/ehe_disparities/ehe_disparities_intervent
 #load_simset(date="2025_01-31", msa="35620") #NYC
 
 source("../jheem_analyses/commoncode/locations_of_interest.R")
-MSAS.OF.INTEREST #list of MSAs
+#MSAS.OF.INTEREST
 
 CALIBRATION.CODE="final.ehe"
 LOCATIONS=MSAS.OF.INTEREST[c(1:27,29:32)] #skip Cincinnati and St Louis
@@ -57,17 +57,21 @@ for (i in 1:31) {
 
 #################################
 
-#Output incidence and population estimates
+#Output incidence and population estimates for 2035 and 2025
+#results = collection$get(outcomes = c("new", "incidence", "population"),
+#                         dimension.values = list(year=2035),
+#                         keep.dimensions = c("race"))
 
-results = collection$get(outcomes = c("new", "incidence", "population"),
-                         dimension.values = list(year=2035),
-                         keep.dimensions = c("race"))
-results
-dim(results)
+#baseline = collection$get(outcomes = c("new", "incidence", "population"),
+#                         dimension.values = list(year=2025),
+#                         keep.dimensions = c("race"))
 
-results = collection$get(outcomes = c("new", "incidence", "population"),
-                         dimension.values = list(year=2025),
-                         keep.dimensions = c("race"))
+#save(results, baseline, file=file.path(get.jheem.root.directory(),"results","ehe_disparities","results.Rdata"))
+load(file.path(get.jheem.root.directory(),"results","ehe_disparities","results.Rdata"))
+
+
+#resample from simulations without NAs
+
 
 #################################
 
@@ -121,7 +125,8 @@ fpl <- distinct(left_join(fpl_msa, fpl_state, join_by(statename==NAME))) %>%
 
 #Sum incidence and population estimates across all MSAs
 total_results <- apply(results, setdiff(names(dim(results)), "location"), sum)
-#total_results
+#dim(total_results)
+#total_results[,,,2]
 dim.names = dimnames(results)
 #dim.names
 dim.names$location = c(dim.names$location, "Total")
@@ -136,7 +141,6 @@ pct_black <- apply(results["black", , "incidence", , ], c("location", "intervent
 pct_hisp <- apply(results["hispanic", , "incidence", , ], c("location", "intervention"), sum) / apply(results[ , , "incidence", , ], c("location", "intervention"), sum)
 pct_other <- apply(results["other", , "incidence", , ], c("location", "intervention"), sum) / apply(results[ , , "incidence", , ], c("location", "intervention"), sum)
 
-
 #Calculate incidence rates
 ir_black <- apply(results["black", , "incidence", , ], c("sim", "location", "intervention"), sum) / apply(results["black", , "population", , ], c("sim", "location", "intervention"), sum) 
 ir_hisp <- apply(results["hispanic", , "incidence", , ], c("sim", "location", "intervention"), sum) / apply(results["hispanic", , "population", , ], c("sim", "location", "intervention"), sum) 
@@ -145,29 +149,29 @@ ir_total <- apply(results[ , , "incidence", , ], c("sim", "location", "intervent
 
 #Take the median, 2.5th and 97.5th percentile of the IRs across simulations
 ir_black_median = apply(ir_black, c("location", "intervention"), median)
-ir_black_lower = apply(ir_black, c("location", "intervention"), quantile, probs=0.025)
-ir_black_upper = apply(ir_black, c("location", "intervention"), quantile, probs=0.975)
+ir_black_lower = apply(ir_black, c("location", "intervention"), quantile, probs=0.025, na.rm=TRUE) #exclude simulations with no results
+ir_black_upper = apply(ir_black, c("location", "intervention"), quantile, probs=0.975, na.rm=TRUE)
 ir_black_medianci = paste0(round(ir_black_median*100000, 0), " (", round(ir_black_lower*100000, 0), ", ", round(ir_black_upper*100000, 0), ")")
 dim(ir_black_medianci) = dim(ir_black_median)
 dimnames(ir_black_medianci) = dimnames(ir_black_median)
 
 ir_hisp_median = apply(ir_hisp, c("location", "intervention"), median)
-ir_hisp_lower = apply(ir_hisp, c("location", "intervention"), quantile, probs=0.025)
-ir_hisp_upper = apply(ir_hisp, c("location", "intervention"), quantile, probs=0.975)
+ir_hisp_lower = apply(ir_hisp, c("location", "intervention"), quantile, probs=0.025, , na.rm=TRUE)
+ir_hisp_upper = apply(ir_hisp, c("location", "intervention"), quantile, probs=0.975, , na.rm=TRUE)
 ir_hisp_medianci = paste0(round(ir_hisp_median*100000, 0), " (", round(ir_hisp_lower*100000, 0), ", ", round(ir_hisp_upper*100000, 0), ")")
 dim(ir_hisp_medianci) = dim(ir_hisp_median)
 dimnames(ir_hisp_medianci) = dimnames(ir_hisp_median)
 
 ir_other_median = apply(ir_other, c("location", "intervention"), median)
-ir_other_lower = apply(ir_other, c("location", "intervention"), quantile, probs=0.025)
-ir_other_upper = apply(ir_other, c("location", "intervention"), quantile, probs=0.975)
+ir_other_lower = apply(ir_other, c("location", "intervention"), quantile, probs=0.025, na.rm=TRUE)
+ir_other_upper = apply(ir_other, c("location", "intervention"), quantile, probs=0.975, na.rm=TRUE)
 ir_other_medianci = paste0(round(ir_other_median*100000, 0), " (", round(ir_other_lower*100000, 0), ", ", round(ir_other_upper*100000, 0), ")")
 dim(ir_other_medianci) = dim(ir_other_median)
 dimnames(ir_other_medianci) = dimnames(ir_other_median)
 
 ir_total_median = apply(ir_total, c("location", "intervention"), median)
-ir_total_lower = apply(ir_total, c("location", "intervention"), quantile, probs=0.025)
-ir_total_upper = apply(ir_total, c("location", "intervention"), quantile, probs=0.975)
+ir_total_lower = apply(ir_total, c("location", "intervention"), quantile, probs=0.025, , na.rm=TRUE)
+ir_total_upper = apply(ir_total, c("location", "intervention"), quantile, probs=0.975, , na.rm=TRUE)
 ir_total_medianci = paste0(round(ir_total_median*100000, 0), " (", round(ir_total_lower*100000, 0), ", ", round(ir_total_upper*100000, 0), ")")
 dim(ir_total_medianci) = dim(ir_total_median)
 dimnames(ir_total_medianci) = dimnames(ir_total_median)
@@ -176,34 +180,33 @@ ir_total_medianci
 #Calculate IRD
 ird_black <- ir_black - ir_other
 ird_black_median = apply(ird_black, c("location", "intervention"), median)
-ird_black_lower = apply(ird_black, c("location", "intervention"), quantile, probs=0.025)
-ird_black_upper = apply(ird_black, c("location", "intervention"), quantile, probs=0.975)
+ird_black_lower = apply(ird_black, c("location", "intervention"), quantile, probs=0.025, , na.rm=TRUE)
+ird_black_upper = apply(ird_black, c("location", "intervention"), quantile, probs=0.975, , na.rm=TRUE)
 ird_black_medianci = paste0(round(ird_black_median*100000, 0), " (", round(ird_black_lower*100000, 0), ", ", round(ird_black_upper*100000, 0), ")")
 dim(ird_black_medianci) = dim(ird_black_median)
 dimnames(ird_black_medianci) = dimnames(ird_black_median)
 
 ird_hisp <- ir_hisp - ir_other
 ird_hisp_median = apply(ird_hisp, c("location", "intervention"), median)
-ird_hisp_lower = apply(ird_hisp, c("location", "intervention"), quantile, probs=0.025)
-ird_hisp_upper = apply(ird_hisp, c("location", "intervention"), quantile, probs=0.975)
+ird_hisp_lower = apply(ird_hisp, c("location", "intervention"), quantile, probs=0.025, , na.rm=TRUE)
+ird_hisp_upper = apply(ird_hisp, c("location", "intervention"), quantile, probs=0.975, , na.rm=TRUE)
 ird_hisp_medianci = paste0(round(ird_hisp_median*100000, 0), " (", round(ird_hisp_lower*100000, 0), ", ", round(ird_hisp_upper*100000, 0), ")")
 dim(ird_hisp_medianci) = dim(ird_hisp_median)
 dimnames(ird_hisp_medianci) = dimnames(ird_hisp_median)
 
-#change change in IRR in each simulation
 #Calculate IRR
 log_irr_black <- log(ir_black / ir_other)
 log_irr_black_median = apply(log_irr_black, c("location", "intervention"), median)
-log_irr_black_lower = apply(log_irr_black, c("location", "intervention"), quantile, probs=0.025)
-log_irr_black_upper = apply(log_irr_black, c("location", "intervention"), quantile, probs=0.975)
+log_irr_black_lower = apply(log_irr_black, c("location", "intervention"), quantile, probs=0.025, na.rm=TRUE)
+log_irr_black_upper = apply(log_irr_black, c("location", "intervention"), quantile, probs=0.975, na.rm=TRUE)
 irr_black_medianci = paste0(round(exp(log_irr_black_median), 1), " (", round(exp(log_irr_black_lower), 1), ", ", round(exp(log_irr_black_upper), 1), ")")
 dim(irr_black_medianci) = dim(log_irr_black_median)
 dimnames(irr_black_medianci) = dimnames(log_irr_black_median)
 
 log_irr_hisp <- log(ir_hisp / ir_other)
 log_irr_hisp_median = apply(log_irr_hisp, c("location", "intervention"), median)
-log_irr_hisp_lower = apply(log_irr_hisp, c("location", "intervention"), quantile, probs=0.025)
-log_irr_hisp_upper = apply(log_irr_hisp, c("location", "intervention"), quantile, probs=0.975)
+log_irr_hisp_lower = apply(log_irr_hisp, c("location", "intervention"), quantile, probs=0.025, na.rm=TRUE)
+log_irr_hisp_upper = apply(log_irr_hisp, c("location", "intervention"), quantile, probs=0.975, na.rm=TRUE)
 irr_hisp_medianci = paste0(round(exp(log_irr_hisp_median), 1), " (", round(exp(log_irr_hisp_lower), 1), ", ", round(exp(log_irr_hisp_upper), 1), ")")
 dim(irr_hisp_medianci) = dim(log_irr_hisp_median)
 dimnames(irr_hisp_medianci) = dimnames(log_irr_hisp_median)
