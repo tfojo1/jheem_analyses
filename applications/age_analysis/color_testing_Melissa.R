7474a40d0a6247248d85f69129d57476adc8c775
mschnure <mschnur3@jhmi.edu>
2025-07-01 09:14:52 -0400

    updated with options

--- applications/age_analysis/color_testing_Melissa.R
+++ applications/age_analysis/color_testing_Melissa.R
@@ -10,4 +10,11 @@ write.shaded.table(table_contents,
                    file = "../jheem_analyses/applications/age_analysis/shaded_table_test.xlsx",
                    color.by = table_color_by,
                    thresholds = c(-1, 0, 1),
-                   colors = c("#006d2c", "white", "#e6550d"),)
+                   colors = c("#74c476", "white", "#fd8d3c"),)
+
+# option 1 (original): c("yellow", "white", "blue")
+# option 2 (also yellow to blue): c("#fee08b", "white", "#3288bd") 
+# option 3 (green to orange): c("#74c476", "white", "#fd8d3c")
+# option 4 (deeper green to orange): c("#006d2c", "white", "#e6550d")
+# option 5 (light blue to light orange): c("#6baed6", "white", "#fdcc8a")
+# option 6 (green to purple): c("#33a02c", "white", "#984ea3")
