cfa6f862f79bfc387fb9955e8c82eda4498b6a37
mschnure <mschnur3@jhmi.edu>
2025-07-01 08:52:54 -0400

    new set of colors

--- applications/age_analysis/color_testing_Melissa.R
+++ applications/age_analysis/color_testing_Melissa.R
@@ -1,10 +1,13 @@
 # For Melissa
 # 6/30/2025
 
+source("../jheem_analyses/applications/EHE/ehe_specification.R")
+source("../jheem_analyses/presentation/make_pretty_table.R")
+
 table_contents <- get(load("../jheem_analyses/applications/age_analysis/table1.R"))
 table_color_by <- get(load("../jheem_analyses/applications/age_analysis/table1_colors.R"))
 write.shaded.table(table_contents,
                    file = "../jheem_analyses/applications/age_analysis/shaded_table_test.xlsx",
                    color.by = table_color_by,
                    thresholds = c(-1, 0, 1),
-                   colors = c("yellow", "white", "blue"),)
+                   colors = c("#006d2c", "white", "#e6550d"),)
