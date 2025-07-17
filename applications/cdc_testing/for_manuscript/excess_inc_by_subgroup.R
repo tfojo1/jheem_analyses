3294ea76a921d1d32a38d20227a34478dcd28a3e
Todd Fojo <tfojo1@jh.edu>
2025-07-16 23:08:16 -0400

    additional processing of cdc testing results

---
+++ applications/cdc_testing/for_manuscript/excess_inc_by_subgroup.R
@@ -0,0 +1,45 @@
+
+YEARS = as.character(2025:2030)
+
+total.inc.by.age = apply(full.incidence[YEARS,,,,,,,], c('age','sim','intervention'), sum)
+total.abs.excess.inc.by.age.end = total.inc.by.age[,,'cdct.end'] - total.inc.by.age[,,'noint']
+total.rel.excess.inc.by.age.end = total.abs.excess.inc.by.age.end / total.inc.by.age[,,'noint']
+
+cbind(
+    mean = apply(total.rel.excess.inc.by.age.end, 'age', mean),
+    lower = apply(total.rel.excess.inc.by.age.end, 'age', quantile, probs=0.025),
+    upper = apply(total.rel.excess.inc.by.age.end, 'age', quantile, probs=0.975)
+)
+
+total.rel.excess.inc.under.35 = colSums(total.inc.by.age[1:2,,'cdct.end'] - total.inc.by.age[1:2,,'noint']) / colSums(total.inc.by.age[1:2,,'noint'])
+total.rel.excess.inc.over.35 = colSums(total.inc.by.age[3:5,,'cdct.end'] - total.inc.by.age[3:5,,'noint']) / colSums(total.inc.by.age[1:2,,'noint'])
+
+rbind(
+    under.35 = c(mean=mean(total.rel.excess.inc.under.35),
+                 lower=quantile(total.rel.excess.inc.under.35, probs=0.025),
+                 upper=quantile(total.rel.excess.inc.under.35, probs=0.975)),
+    over.35 = c(mean=mean(total.rel.excess.inc.over.35),
+                 lower=quantile(total.rel.excess.inc.over.35, probs=0.025),
+                 upper=quantile(total.rel.excess.inc.over.35, probs=0.975))
+)
+
+total.inc.by.race = apply(full.incidence[YEARS,,,,,,,], c('race','sim','intervention'), sum)
+total.abs.excess.inc.by.race.end = total.inc.by.race[,,'cdct.end'] - total.inc.by.race[,,'noint']
+total.rel.excess.inc.by.race.end = total.abs.excess.inc.by.race.end / total.inc.by.race[,,'noint']
+
+cbind(
+    mean = apply(total.rel.excess.inc.by.race.end, 'race', mean),
+    lower = apply(total.rel.excess.inc.by.race.end, 'race', quantile, probs=0.025),
+    upper = apply(total.rel.excess.inc.by.race.end, 'race', quantile, probs=0.975)
+)
+
+
+total.inc.by.sex = apply(full.incidence[YEARS,,,,,,,], c('sex','sim','intervention'), sum)
+total.abs.excess.inc.by.sex.end = total.inc.by.sex[,,'cdct.end'] - total.inc.by.sex[,,'noint']
+total.rel.excess.inc.by.sex.end = total.abs.excess.inc.by.sex.end / total.inc.by.sex[,,'noint']
+
+cbind(
+    mean = apply(total.rel.excess.inc.by.sex.end, 'sex', mean),
+    lower = apply(total.rel.excess.inc.by.sex.end, 'sex', quantile, probs=0.025),
+    upper = apply(total.rel.excess.inc.by.sex.end, 'sex', quantile, probs=0.975)
+)
---
+++ applications/cdc_testing/for_manuscript/tests_saved_per_excess_infection.R
@@ -0,0 +1,27 @@
+
+load("Q:results/cdc_testing/cdc_testing_results_2025-07-16.Rdata")
+
+YEARS = as.character(2025:2030)
+
+abs.excess.inc.end = apply(total.results[YEARS,,'incidence',,'cdct.end'] - total.results[YEARS,,'incidence',,'noint'], c('sim', 'location'), sum)
+abs.excess.cdc.tests.end = apply(total.results[YEARS,,'cdc.funded.tests',,'noint'] - total.results[YEARS,,'cdc.funded.tests',,'cdct.end'], c('sim', 'location'), sum)
+
+tests.per.excess.inf.end = abs.excess.cdc.tests.end / abs.excess.inc.end
+sort(apply(tests.per.excess.inf.end, 'location', mean))
+
+
+abs.excess.inc.bintr = apply(total.results[YEARS,,'incidence',,'cdct.bintr'] - total.results[YEARS,,'incidence',,'noint'], c('sim', 'location'), sum)
+abs.excess.cdc.tests.bintr = apply(total.results[YEARS,,'cdc.funded.tests',,'noint'] - total.results[YEARS,,'cdc.funded.tests',,'cdct.bintr'], c('sim', 'location'), sum)
+
+tests.per.excess.inf.bintr = abs.excess.cdc.tests.bintr / abs.excess.inc.bintr
+sort(apply(tests.per.excess.inf.bintr, 'location', mean))
+
+
+
+abs.excess.inc = apply(total.results[YEARS,,'incidence',,] - as.numeric(total.results[YEARS,,'incidence',,'noint']), c('sim', 'location','intervention'), sum)
+abs.excess.cdc.tests = apply(as.numeric(total.results[YEARS,,'cdc.funded.tests',,'noint']) - total.results[YEARS,,'cdc.funded.tests',,], c('sim', 'location','intervention'), sum)
+tests.per.excess.inf = abs.excess.cdc.tests / abs.excess.cdc.tests
+
+apply(tests.per.excess.inf, c('location','intervention'), mean)
+
+
