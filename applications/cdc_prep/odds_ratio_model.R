f827e733c88f596b737d28ec1f1f18649764fe20
ruchitab24 <62656133+ruchitab24@users.noreply.github.com>
2025-08-18 15:08:51 -0400

    updated odds ratio model

--- applications/cdc_prep/odds_ratio_model.R
+++ applications/cdc_prep/odds_ratio_model.R
@@ -21,27 +21,70 @@ get.fraction.prep.referred <- function(specification.metadata)
     df$sex <- relevel(factor(df$sex), ref = "all")
     
     #Logistic regression
-    oddsratio_model <- suppressWarnings(glm(diagnoses ~ as.factor(race)*year + as.factor(age)*year + as.factor(sex)*year, data = df, weights = number, family = binomial(link = "logit")))
+    oddsratio_model <- suppressWarnings(glm(referrals ~ as.factor(race)*year + as.factor(age)*year + as.factor(sex)*year, data = df, weights = number, family = binomial(link = "logit")))
     summary(oddsratio_model)
     
     
     dummy.for.intercept = as.data.frame(get.every.combination(dim.names))
     dummy.for.intercept$year = 2021
-    diagnoses.intercept = predict(oddsratio_model,newdata = dummy.for.intercept)
+    referrals.intercept = predict(oddsratio_model,newdata = dummy.for.intercept)
     
     dummy.for.slope = dummy.for.intercept 
     dummy.for.slope$year = 2022
-    diagnoses.slope = predict(oddsratio_model,newdata = dummy.for.slope) - diagnoses.intercept
+    referrals.slope = predict(oddsratio_model,newdata = dummy.for.slope) -referrals.intercept
     
-    dim(diagnoses.intercept) = sapply(dim.names,length)
-    dimnames(diagnoses.intercept) = dim.names
+    dim(referrals.intercept) = sapply(dim.names,length)
+    dimnames(referrals.intercept) = dim.names
     
-    dim(diagnoses.slope) = sapply(dim.names,length)
-    dimnames(diagnoses.slope) = dim.names
+    dim(referrals.slope) = sapply(dim.names,length)
+    dimnames(referrals.slope) = dim.names
     
-    create.logistic.linear.functional.form(intercept = diagnoses.intercept, 
-                                           slope = diagnoses.slope, 
+    create.logistic.linear.functional.form(intercept = referrals.intercept, 
+                                           slope = referrals.slope, 
                                            anchor.year = 2021, 
                                            parameters.are.on.logit.scale = TRUE)
 }
 
+
+get.fraction.prep.eligible <- function(specification.metadata)
+{
+    
+    df <- read.csv("../jheem_analyses/applications/cdc_prep/prep_eligible.csv")
+    
+    #Set reference categories
+    
+    specification.metadata = get.specification.metadata("cdcp","MD")
+    dim.names = specification.metadata$dim.names[c("age","race","sex")]
+    
+    df$race <- relevel(factor(df$race), ref = "all")
+    age.mapping = dim.names$age
+    names(age.mapping) = paste0("cat",1:5)
+    age.mapping = c(age.mapping,all = "all")
+    df$age = age.mapping[df$age]
+    df$age <- relevel(factor(df$age), ref = "all")
+    df$sex <- relevel(factor(df$sex), ref = "all")
+    
+    #Logistic regression
+    oddsratio_model <- suppressWarnings(glm(eligible ~ as.factor(race)*year + as.factor(age)*year + as.factor(sex)*year, data = df, weights = number, family = binomial(link = "logit")))
+    summary(oddsratio_model)
+    
+    
+    dummy.for.intercept = as.data.frame(get.every.combination(dim.names))
+    dummy.for.intercept$year = 2021
+    eligible.intercept = predict(oddsratio_model,newdata = dummy.for.intercept)
+    
+    dummy.for.slope = dummy.for.intercept 
+    dummy.for.slope$year = 2022
+    eligible.slope = predict(oddsratio_model,newdata = dummy.for.slope) - eligible.intercept
+    
+    dim(eligible.intercept) = sapply(dim.names,length)
+    dimnames(eligible.intercept) = dim.names
+    
+    dim(eligible.slope) = sapply(dim.names,length)
+    dimnames(eligible.slope) = dim.names
+    
+    create.logistic.linear.functional.form(intercept = eligible.intercept, 
+                                           slope = eligible.slope, 
+                                           anchor.year = 2021, 
+                                           parameters.are.on.logit.scale = TRUE)
+}
