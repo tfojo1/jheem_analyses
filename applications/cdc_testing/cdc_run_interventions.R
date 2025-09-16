806c601afdc3ace84bb577344f094e8ba2dd85eb
Todd Fojo <tfojo1@jh.edu>
2025-09-16 09:10:44 -0400

    again tweaks to CDCT run interventions

--- applications/cdc_testing/cdc_run_interventions.R
+++ applications/cdc_testing/cdc_run_interventions.R
@@ -11,9 +11,18 @@ print(paste0("Running interventions for locations: ", paste0(LOCATIONS, collapse
 coll = create.simset.collection('cdct', 
                                 calibration.code = 'final.ehe.state',
                                 locations = LOCATIONS, 
-                                interventions = CDC.TESTING.INTERVENTION.CODES,
+                                interventions = setdiff(CDC.TESTING.INTERVENTION.CODES, 'noint'),
                                 n.sim = N.SIMS)
 
 coll$run(start.year=CDC.TESTING.ANCHOR.YEAR, keep.from.year = CDC.TESTING.ANCHOR.YEAR-1, end.year=CDC.TESTING.ANCHOR.YEAR+10, verbose = T, overwrite.prior = T, stop.for.errors = T)
+
+
+coll.noint = create.simset.collection('cdct', 
+                                calibration.code = 'final.ehe.state',
+                                locations = LOCATIONS, 
+                                interventions = 'noint',
+                                n.sim = N.SIMS)
+
+coll$run(start.year=2034, keep.from.year = 1970, end.year=CDC.TESTING.ANCHOR.YEAR+10, verbose = T, overwrite.prior = T, stop.for.errors = T)
 
 source('applications/cdc_testing/cdc_testing_extract_results.R')
