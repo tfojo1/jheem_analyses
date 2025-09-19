4e3de8624e0887ea5f28f2645f5723991c677be1
Todd Fojo <tfojo1@jh.edu>
2025-09-19 15:54:04 -0400

    tweak to cd rw timeline

--- applications/ryan_white/ryan_white_cd_interventions.R
+++ applications/ryan_white/ryan_white_cd_interventions.R
@@ -1,39 +1,42 @@
+
+CD.START.YEAR = 2026 + 1/12
+
 # Complete Loss of OAHS
 cd.oahs.cessation.expansion.effect = create.intervention.effect(quantity.name = 'oahs.suppression.expansion.effect',
-                                                                start.time = START.YEAR,
+                                                                start.time = CD.START.YEAR,
                                                                 effect.values = expression(1-lose.oahs.expansion.effect*fraction.oahs.on.cd),
                                                                 apply.effects.as = 'value',
                                                                 scale = 'proportion',
-                                                                times = START.YEAR + LOSS.LAG,
+                                                                times = CD.START.YEAR + LOSS.LAG,
                                                                 allow.values.less.than.otherwise = T,
                                                                 allow.values.greater.than.otherwise = F )
 
 cd.oahs.cessation.nonexpansion.effect = create.intervention.effect(quantity.name = 'oahs.suppression.nonexpansion.effect',
-                                                                   start.time = START.YEAR,
+                                                                   start.time = CD.START.YEAR,
                                                                    effect.values = expression(1-lose.oahs.nonexpansion.effect*fraction.oahs.on.cd),
                                                                    apply.effects.as = 'value',
                                                                    scale = 'proportion',
-                                                                   times = START.YEAR + LOSS.LAG,
+                                                                   times = CD.START.YEAR + LOSS.LAG,
                                                                    allow.values.less.than.otherwise = T,
                                                                    allow.values.greater.than.otherwise = F )
 
 
 # Complete Loss of RW support
 cd.rw.support.cessation.expansion.effect = create.intervention.effect(quantity.name = 'rw.support.suppression.expansion.effect',
-                                                                      start.time = START.YEAR,
+                                                                      start.time = CD.START.YEAR,
                                                                       effect.values = expression(1-lose.rw.support.expansion.effect*fraction.rw.support.on.cd),
                                                                       apply.effects.as = 'value',
                                                                       scale = 'proportion',
-                                                                      times = START.YEAR + LOSS.LAG,
+                                                                      times = CD.START.YEAR + LOSS.LAG,
                                                                       allow.values.less.than.otherwise = T,
                                                                       allow.values.greater.than.otherwise = F )
 
 cd.rw.support.cessation.nonexpansion.effect = create.intervention.effect(quantity.name = 'rw.support.suppression.nonexpansion.effect',
-                                                                         start.time = START.YEAR,
+                                                                         start.time = CD.START.YEAR,
                                                                          effect.values = expression(1-lose.rw.support.nonexpansion.effect*fraction.rw.support.on.cd),
                                                                          apply.effects.as = 'value',
                                                                          scale = 'proportion',
-                                                                         times = START.YEAR + LOSS.LAG,
+                                                                         times = CD.START.YEAR + LOSS.LAG,
                                                                          allow.values.less.than.otherwise = T,
                                                                          allow.values.greater.than.otherwise = F )
 sampled.cd.fraction = rnorm(1000, mean=0.2, sd=0.025)
