54c62499fbfdfc1fc03e717b1abc6d5b7df42e95
Andrew Zalesak <azalesa1@jh.edu>
2025-10-20 10:55:00 -0400

    allow figure to be made for 11-state version

--- applications/age_analysis/generate_stacked_area_chart.R
+++ applications/age_analysis/generate_stacked_area_chart.R
@@ -15,14 +15,23 @@ load("../jheem_analyses/applications/age_analysis/Rdata Objects/age_results.Rdat
 load(file="../jheem_analyses/applications/age_analysis/Rdata Objects/state_order.Rdata")
 load(file="../jheem_analyses/applications/age_analysis/Rdata Objects/state_order_names.Rdata")
 
+original_eleven <- c("AL", "CA", "FL", "GA", "IL", "LA", "MO", "MS", "NY", "TX", "WI")
+# ONLY_ELEVEN <- F
+ONLY_ELEVEN <- T
+
+if (ONLY_ELEVEN) {
+    state_order <- c(intersect(state_order, original_eleven), "total")
+    state_order_names <- state_order_names[state_order]
+    age_results <- age_results[,,,,original_eleven,,drop=F]
+}
+my_states <- setdiff(sort(state_order), "total")
+
 # Stacked Area Plots----
 stacked_area_total_arr <- apply(age_results[as.character(2025:2040),,,"diagnosed.prevalence",,], c("year", "age", "sim"), sum)
 stacked_area_raw_arr <- array(c(age_results[as.character(2025:2040),,,"diagnosed.prevalence",,],
                                 stacked_area_total_arr),
                               dim=c(year=length(2025:2040), age=5, sim=1000, location=length(state_order)),
                               dimnames=list(year=2025:2040, age=dimnames(age_results)[["age"]], sim=1:1000, location=c(my_states, "total")))
-
-my_states <- setdiff(sort(state_order), "total")
 
 stacked_area_plot_data <- reshape2::melt(get_stats(stacked_area_raw_arr,
                                                    keep.dimensions = c('year', 'age', 'location'))) %>%
@@ -50,7 +59,13 @@ make_stacked_area_plots <- function(states, type=c("absolute", "percentage")[1])
 }
 
 plot <- make_stacked_area_plots(c(my_states, "total"))
-ggsave(filename = "../jheem_analyses/applications/age_analysis/stacked_area_chart.png",
-       plot=plot,width = 10, height=10)
+
+if (ONLY_ELEVEN) {
+    ggsave(filename = "../jheem_analyses/applications/age_analysis/Figures/HIV & Aging/stacked_area_chart.png",
+           plot=plot,width = 7, height=6)
+} else {
+    ggsave(filename = "../jheem_analyses/applications/age_analysis/Figures/Manuscript/stacked_area_chart.png",
+           plot=plot,width = 10, height=10)
+}
 
 # Do I want this saved in a particular size? Number of pixels? ?
