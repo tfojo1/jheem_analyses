58c1df693e73e78d52466fe1e188387d7f128600
Todd Fojo <tfojo1@jh.edu>
2025-11-12 21:41:02 -0500

    bug in simplot showing up with RW plotting

---
+++ test/simplot_bug_rw.R
@@ -0,0 +1,15 @@
+
+
+simset = retrieve.simulation.set('rw', 'C.12580', 'final.ehe', n.sim=80, intervention.code = 'noint', sub.version='w')
+
+# This gives sim in the jheem sex ontology (msm, female, heterosexual_male)
+simplot(simset,
+        split.by='sex',
+        data.manager = RW.DATA.MANAGER,
+        'non.adap.clients', dimension.values = list(year=2015:2025)) 
+
+# Gives an error
+simplot(simset,
+        split.by='risk',
+        data.manager = RW.DATA.MANAGER,
+        'non.adap.clients', dimension.values = list(year=2015:2025)) 
