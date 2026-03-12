cb91e7e642c34fa887281da6d0a4b470fe377da1
Andrew Zalesak <azalesa1@jh.edu>
2026-03-12 12:25:22 -0400

    add calib 3-12 stage 1 and 3-10 stage 0

--- applications/SHIELD/shield_calib_register_new.R
+++ applications/SHIELD/shield_calib_register_new.R
@@ -15,6 +15,30 @@ par.aliases.transmission = list(
 )
 
 # STAGE 0 Demographics thin 50
+
+register.calibration.info("calib.3.12.stage1.az",
+                          preceding.calibration.codes = 'calib.2.19.stage0.az',
+                          likelihood.instructions = lik.inst.stage1,
+                          data.manager = SURVEILLANCE.MANAGER,
+                          end.year = 2030,
+                          parameter.names = c(TRANSMISSION.PARAMETERS.PRIOR@var.names,
+                                              STI.TESTING.PARAMETERS.PRIOR@var.names),
+                          n.iter = N_ITER, thin = 50, is.preliminary = T, max.run.time.seconds = 30, description = "NA"
+                          
+                          )
+
+register.calibration.info("calib.3.10.stage0.az",
+                          likelihood.instructions = lik.inst.stage0,
+                          data.manager = SURVEILLANCE.MANAGER,
+                          end.year = 2030,
+                          fixed.initial.parameter.values = c("global.transmission.rate"=2.05), # median is 2.2
+                          parameter.names = c(POPULATION.PARAMETERS.PRIOR@var.names,
+                                              AGING.PARAMETERS.PRIOR@var.names,
+                                              "global.transmission.rate"),
+                          parameter.aliases = par.aliases.transmission,
+                          n.iter = N_ITER, thin = 50, is.preliminary = T, max.run.time.seconds = 30, description = "NA"
+)
+
 register.calibration.info("calib.2.19.stage0.az",
                           likelihood.instructions = lik.inst.stage0,
                           data.manager = SURVEILLANCE.MANAGER,
@@ -41,27 +65,6 @@ register.calibration.info('calib.3.2.stage1.az',
 register.calibration.info('calib.3.3.stage2.woFT',
                           preceding.calibration.codes = 'calib.3.2.stage1.az',
                           likelihood.instructions = lik.inst.stage2,
-                          data.manager = SURVEILLANCE.MANAGER,
-                          end.year = 2030,
-                          parameter.names = c(TRANSMISSION.PARAMETERS.PRIOR@var.names,
-                                              STI.TESTING.PARAMETERS.PRIOR@var.names),
-                          n.iter = N_ITER, thin = 50, is.preliminary = T, max.run.time.seconds = 30, description = "NA"
-)
-# with future trend ----
-#STAGE1  
-register.calibration.info('calib.3.2.stage1.pk',
-                          preceding.calibration.codes = 'calib.2.19.stage0.az',
-                          likelihood.instructions = lik.inst.stage1.futureTrend,
-                          data.manager = SURVEILLANCE.MANAGER,
-                          end.year = 2030,
-                          parameter.names = c(TRANSMISSION.PARAMETERS.PRIOR@var.names,
-                                              STI.TESTING.PARAMETERS.PRIOR@var.names),
-                          n.iter = N_ITER, thin = 50, is.preliminary = T, max.run.time.seconds = 30, description = "NA"
-)
-#STAGE2 
-register.calibration.info('calib.3.3.stage2.wFT',
-                          preceding.calibration.codes = 'calib.3.2.stage1.pk',
-                          likelihood.instructions = lik.inst.stage2.futureTrend,
                           data.manager = SURVEILLANCE.MANAGER,
                           end.year = 2030,
                           parameter.names = c(TRANSMISSION.PARAMETERS.PRIOR@var.names,
