e2c2dc73b1ce20808bd9ab27a22889f2aebae471
Melissa Schnure <mschnur3@jh.edu>
2026-03-26 15:01:25 -0400

    added new intervention to source

--- applications/ryan_white/ryan_white_main.R
+++ applications/ryan_white/ryan_white_main.R
@@ -18,6 +18,7 @@ source('../jheem_analyses/applications/ryan_white/read_rw_costs.R')
 source('../jheem_analyses/applications/ryan_white/ryan_white_nanb_interventions.R')
 source('../jheem_analyses/applications/ryan_white/ryan_white_partb_interventions.R')
 source('../jheem_analyses/applications/ryan_white/rw_adap_biktarvy_interventions.R')
+source('../jheem_analyses/applications/ryan_white/ryan_white_50_adap_interventions.R')
 
 
 # To pull what we have calibrated
