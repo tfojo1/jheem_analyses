cde62f3bf79a619fd32d64dced228cae96eebb6d
Ryan Forster <rfor1111@gmail.com>
2026-03-31 11:11:30 -0400

    removed noint and symbolic link

--- applications/ryan_white/run_rw_costing_interventions.R
+++ applications/ryan_white/run_rw_costing_interventions.R
@@ -1,53 +1,13 @@
 
 library(jheem2)
 
-.real_sim_root <- "/Volumes/jheem$/simulations"
-
-.rewrite_q_path <- function(path) {
-    if (is.character(path)) sub("^Q:simulations", .real_sim_root, path) else path
-}
-
-file.exists <- local({
-    f <- base::file.exists
-    function(...) {
-        args <- lapply(list(...), .rewrite_q_path)
-        do.call(f, args)
-    }
-})
-
-dir.exists <- local({
-    f <- base::dir.exists
-    function(paths) f(.rewrite_q_path(paths))
-})
-
-list.files <- local({
-    f <- base::list.files
-    function(path = ".", ...) f(path = .rewrite_q_path(path), ...)
-})
-
-list.dirs <- local({
-    f <- base::list.dirs
-    function(path = ".", ...) f(path = .rewrite_q_path(path), ...)
-})
-
-normalizePath <- local({
-    f <- base::normalizePath
-    function(path, ...) f(.rewrite_q_path(path), ...)
-})
-
-Sys.glob <- local({
-    f <- base::Sys.glob
-    function(paths, ...) f(.rewrite_q_path(paths), ...)
-})
-
-
 print("Sourcing code prior to running interventions")
 
 source('../jheem_analyses/applications/ryan_white/ryan_white_main.R')
 
 LOCATIONS = RW.LOCATIONS # 'FL' 
 FORCE.OVERWRITE = T #FORCE.REDO
-INTERVENTION.CODES = c('adap.100.end.26', "noint") 
+INTERVENTION.CODES = c('adap.100.end.26') 
 
 print(paste0("Doing locations: ",
              paste0(LOCATIONS, collapse=', ')))
