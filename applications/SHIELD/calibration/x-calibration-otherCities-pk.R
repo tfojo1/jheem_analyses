64c91bdd81a2d3dfa96ef6d759631cb20a49d631
pkasaie1 <pkasaie1@jhu.edu>
2026-04-05 20:38:06 -0400

    x
    "rebasing"

--- applications/SHIELD/calibration/x-calibration-otherCities-pk.R
+++ applications/SHIELD/calibration/x-calibration-otherCities-pk.R
@@ -12,12 +12,17 @@ stratum.style.manager  = create.style.manager(color.data.by = "stratum")
 # READ STATUS of ongoing runs
 for(x in names(msa_var_names)[msa_var_names %in% c("P","B","M","A","H","C","L","N")] ){
     print(MSAS.OF.INTEREST[MSAS.OF.INTEREST==x])
-    status<-get.calibration.progress('shield', locations = x, calibration.code = "calib.4.3.stage1.pk")
+# <<<<<<< HEAD
+#     status<-get.calibration.progress('shield', locations = x, calibration.code = "calib.4.3.stage1.pk")
+# =======
+    status<-get.calibration.progress('shield', locations = x, calibration.code = "calib.4.3.stage2.pk")
+# >>>>>>> 32239611 (x)
     print(status)
 }
 #names(msa_var_names)[msa_var_names %in% c("P","B","M","A","H","C","L","N")]
 #    
 
+# <<<<<<< HEAD
 # Reads completed runs
 assign_simset_vars(names(msa_var_names)[msa_var_names %in% c("P","B","M","A","H","C","L","N")],calibration.codes = "calib.4.3.stage0.pk",n.sim = 300,sim.id = 0)
 assign_simset_vars(names(msa_var_names)[msa_var_names %in% c("B","M","A","H","C","L","N")],calibration.codes = "calib.4.3.stage1.pk",n.sim = 300,sim.id = 1)
@@ -32,6 +37,50 @@ assign_simset_vars(names(msa_var_names)[msa_var_names %in% c("B","M","A","H","C"
 #     paste("read ",x," ")
 # }
 
+# =======
+# Reads complete runs simSet0 Andrew
+# assign_simset_vars(names(msa_var_names)[msa_var_names %in% c("P","B","M","A","H","C","L","N")],
+# calibration.codes = "calib.4.3.stage0.pk",n.sim = 300,sim.id = 0)
+
+# STAGE0
+for ( x in c("B","M","A","H","C","L","N")){
+    location= names(msa_var_names)[msa_var_names %in% x]
+    simset<-assemble.simulations.from.calibration(version = 'shield',
+                                                  location = location,
+                                                  calibration.code = "calib.4.3.stage0.pk",
+                                                  allow.incomplete = T)
+    # filename=paste0(get.jheem.root.directory(),"/shield/",CALIBRATION.NAME,"_simset_",Sys.Date(),"_",LOCATION,".Rdata")
+    # save(simset,file =filename )
+    save.simulation.set(get(paste0("simset",x,0)))
+    
+    # assign(
+    #     paste0("simset",x,0),
+    #     retrieve.simulation.set('shield',location = location,calibration.code = "calib.4.3.stage0.pk",n.sim = 300))
+    paste("read ",x," ")
+}
+
+# STAGE1
+for ( x in c("A","H","C","L")){  
+    location= names(msa_var_names)[msa_var_names %in% x]
+    assign(
+        paste0("simset",x,1),
+        retrieve.simulation.set('shield',location = location,calibration.code = "calib.3.30.stage1.pk",n.sim = 300))
+    paste("read ",x," ")
+}
+
+# Simset2 PK
+for ( x in c("A","H","C","L")){ 
+    location= names(msa_var_names)[msa_var_names %in% x]
+    assign(
+        paste0("simset",x,2),
+        retrieve.simulation.set('shield',location = location,calibration.code = "calib.3.30.stage2.pk",n.sim = 300))
+    paste("read ",x," ")
+}
+
+# #
+# simM2<-extract.last.simulation.from.calibration('shield', location = "C.33100", calibration.code = "calib.3.30.stage1.pk",allow.incomplete = T,include.first.sim = T)
+
+# >>>>>>> 32239611 (x)
 
 simplot(
     # simsetB1$last.sim(),  simsetB2$last.sim(),
@@ -51,7 +100,12 @@ simplot(
     # facet.by = "age"
     # ,split.by="race"
     # ,split.by="sex"
+# <<<<<<< HEAD
     # dimension.values = list(year=c(1970:2025)),
+# =======
+    ,split.by="race"
+    # ,plot.which="sim.only"
+# >>>>>>> 32239611 (x)
 )    
 
 lik1=lik.inst.stage1$instantiate.likelihood(VERSION,LOCATION)
