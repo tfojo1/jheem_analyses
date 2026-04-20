a80816db0b9d178732db434eba2de70f3cda0a16
Zoe Dansky <zoedansky@gmail.com>
2026-04-20 13:48:28 -0400

    fixing error

--- data_processing/syphilis.manager/proportion.of.congenital.syphilis.births.R
+++ data_processing/syphilis.manager/proportion.of.congenital.syphilis.births.R
@@ -86,7 +86,7 @@ for (data in birth.data.clean.put) {
 
 
 # Aggregate births to MSA level -------------------------------------------
-source('applications/SHIELD/R/shield_locations_of_interest.r') #Source locations of interest to create MSA vectors
+source('applications/SHIELD/R/shield_locations_of_interest.R') #Source locations of interest to create MSA vectors
 source('../jheem2/R/HELPERS_array_helpers.R') 
 
 source('data_processing/aggregate_county_to_msa_new.R') #This aggregates county level data to other locations
--- data_processing/syphilis.manager/syphilis.processing.for.merging/section3.processing.R
+++ data_processing/syphilis.manager/syphilis.processing.for.merging/section3.processing.R
@@ -245,7 +245,7 @@ source('data_processing/syphilis.manager/cdc.wonder.deaths.R')
 
 #Aggregate Outcomes to MSA 
 syphilis.manager = data.manager
-source('applications/SHIELD/R/shield_locations_of_interest.r') #Source locations of interest to create MSA vectors
+source('applications/SHIELD/R/shield_locations_of_interest.R') #Source locations of interest to create MSA vectors
 #source('commoncode/additional_locations_of_interest.R') #Additional locations of interest
 source('../jheem2/R/HELPERS_array_helpers.R') 
 source('data_processing/put_msa_data_as_new_source_script.R') #This aggregates county level data to other locations
