d08f7a49b6ff53620460d9c1cdf32af3575850c8
Andrew Zalesak <azalesa1@jh.edu>
2026-04-22 11:03:55 -0400

    rename null intervention

--- applications/SHIELD/doxy_interventions_april.R
+++ applications/SHIELD/doxy_interventions_april.R
@@ -1,7 +1,7 @@
 # New Doxy Intervention code
 # 4-14-2026
 
-source("applications/SHIELD/shield_specification.R")
+# source("applications/SHIELD/shield_specification.R")
 
 # (COPIED FROM ORIGINAL) ----
 
@@ -78,14 +78,16 @@ doxyPEP_10 <- create.intervention(
     code = "doxyPEP.10"
 )
 
+no_intervention = get.null.intervention()
+
 
 # NEW ----
-if (1==1)
-{simset <- retrieve.simulation.set(version = "shield",location = "C.12580", calibration.code = "calib.4.8.stage2.az", 300)}
+if (1==2)
+{simset <- retrieve.simulation.set(version = "shield",location = "C.12580", calibration.code = "calib.4.8.stage2.az", 300)
 
-no.intervention = get.null.intervention()
+
 
 sim_doxy10  <- doxyPEP_10$run(sim,  start.year = 2022, end.year = 2030)
-simplot(sim, sim_doxy10, "diagnosis.ps")
+simplot(sim, sim_doxy10, "diagnosis.ps")}
 # Make an intervention by supplying a target population and an intervention effect.
 # 
