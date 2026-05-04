5eff0d44a73a64a2fad219bfe6c097e87be2a985
Parastu <pkasaie@jhu.edu>
2026-05-04 11:41:45 -0400

    modeling rapid scaleup in Seattle

---
+++ applications/SHIELD/intervention/rapidScaleup-seattle.R
@@ -0,0 +1,142 @@
+# Modeling a rapid scale up in Seattle according to DOXYPEP Trial 
+
+# enrolled in 2020–2022 across San Francisco (UCSF) and Seattle (UW).  
+# Enrollment occurred largely in 2020–2021 with 12 months of follow‑up per participant.
+# Enrollment start: August 19, 2020
+# Enrollment stopped: May 13, 2022 (early stop for efficacy)
+# The first main results were presented in 2022 (notably at the International AIDS Conference, AIDS 2022, in July 2022
+# Published paper: The pivotal results were published in the New England Journal of Medicine on April 6, 2023 (Molina et al. for the French trial; Luetkemeyer et al. for the UCSF/UW DoxyPEP trial also in NEJM 2023
+
+# When trial enrollment was halted in May 2022, we offered all current trial participants doxy-PEP as part of the open-label extension phase, and later that year, we offered all past trial participants doxy-PEP prescriptions through the SHC. By March 2023, we began offering doxy-PEP prescriptions to patients who did not participate in the trial and tracking doxy-PEP use.
+#uptake: 
+# https://academic.oup.com/cid/advance-article/doi/10.1093/cid/ciag209/8541760
+# From May 2023 to June 2024:  the proportion of MSM who reported doxy-PEP use in the prior month increased from 12% to 45%
+# self-reported doxy-PEP use in the prior month increased from 15% to 31% among MSM
+
+# Single target population for all MSM
+WHOLE.POPULATION = create.target.population(name = 'Whole Population') #MSM?
+
+# Timing
+INTERVENTION.START <- 2020 + 8/12       # August 19, 2020. DoxyPEP trial begins Uptake0
+# INTERVENTION.END   <- 2022 + 5/12      # May 13, 2022. DoxyPEP trial ends 
+# INTERVENTION.END   <- 2023 + 5/12      # May , 2023 guideline dropped Uptake 12-15%
+INTERVENTION.END   <- 2024 + 6/12      # June, 2024 last interview  Uptake 31-45%
+
+# DOXY-PEP EFFICATY (Studies report: RR: Rate Ratio of incident syphilis cases in doxy vs no-doxy arms per person-time)
+# we have pooled estiamtes from clinical trials to estimate the meanlog and sdlog
+# Draw RR samples from the final lognormal distribution to use in the model
+# log(HR) ~ Normal(mean,sd) >>> HR=lognormal(meanlog,sdlog)
+draw_rr_lnorm <- function(n, 
+                          rr_meanlog,rr_sdlog, # mean and sd log for a lognormal dist
+                          cap_at_one = TRUE) {
+    rr_samples <- rlnorm(n, meanlog = rr_meanlog, sdlog = rr_sdlog) 
+    #
+    if (cap_at_one) rr_samples <- pmin(rr_samples, 1)
+    rr_samples
+}
+
+# generate 1000 values of Doxy effectiveness
+rr_samples <- draw_rr_lnorm(n = 1000, rr_meanlog = -1.540424,rr_sdlog =0.2510223,cap_at_one = T )
+effectiveness_samples=1-rr_samples
+
+# draw_effectiveness_uniform<-function(n){
+#     persistence_samples = runif(n,1,1)
+#     persistence_samples
+# }
+# rr_samples<-draw_effectiveness_uniform(1000)
+# effectiveness_samples=rr_samples
+
+
+# PERSISTANCE and DISCONTINUATION RATE #----
+# P: proportion of population, taking Doxy at the end of the year
+# Discontinuation rate= -log(Peristance)
+
+draw_persistance_uniform<-function(n){
+    persistence_samples = runif(n,1,1)
+    persistence_samples
+}
+persistence_samples<-draw_persistance_uniform(10000)
+discont_rate_samples = -log(persistence_samples)
+
+# Putting them together ----
+DOXY.PARAMS <- rbind(effectiveness_samples,discont_rate_samples)
+rownames(DOXY.PARAMS) <- c("doxy.effectiveness","doxy.discontinuationRate")
+
+
+# INTERVENTION ----
+# intervnetion controls the uptake among eligible population >>> 10% from 2022-2030
+# we know C=U/(1+r) > in the model, we will calculate the coverage
+# r is discontinuation rate
+# U is the proportion of eligible population filling a prescription for doxy (regardless app having been on Doxy before)
+# C is the proportion of eligible population receiving doxyPep by the end of the year?
+
+# Intervention control uptake levels
+clear.interventions() 
+for (uptake in c(30,45)){
+    uptake.effect =  create.intervention.effect(
+        quantity.name    = "doxy.uptake",
+        effect.values    = uptake/100,
+        start.time       = INTERVENTION.START,# when scale up begins
+        times            = INTERVENTION.END, # when scale up ends
+        scale            = "proportion",
+        apply.effects.as = "value",
+        allow.values.less.than.otherwise  = FALSE,
+        allow.values.greater.than.otherwise = TRUE
+    )
+    
+    uptake_intervention <- create.intervention(
+        uptake.effect,
+        parameters = DOXY.PARAMS,
+        WHOLE.POPULATION, 
+        code = paste0("doxypep.",uptake)
+    )
+}
+
+noint = get.null.intervention()
+
+# ============================================================================
+# DoxyPEP Intervention Analysis
+# ============================================================================
+source('../jheem_analyses/applications/SHIELD/shield_specification.R')
+source('../jheem_analyses/commoncode/locations_of_interest.R')
+
+# =============================================================================
+# SECTION 1: Configuration
+# =============================================================================
+# Just for SEATTLE
+LOCATIONS        <- SHIELD.TEN.MSAS[10]     # Named vector: names = city, values = codes
+
+CALIBRATION.CODE <- "calib.4.24.stage2.az"
+N.SIM <- 300
+FIRST.YEAR <- 2000
+LAST.YEAR <- 2040
+
+BASE.PATH <- paste0(ROOT.DIR,"/simulations/shield")
+
+INTERVENTION.LABELS <- c(
+    noint        = "No Doxy-PEP Intervention",
+    doxypep.30.rapid   = "30% Doxy-PEP Coverage (2020-2024)",
+    doxypep.45.rapid   = "45% Doxy-PEP Coverage (2020-2024)"
+)
+INTERVENTION.CODES <- names(INTERVENTION.LABELS)
+
+if (1==1){
+    sim.collection <- create.simset.collection(
+        version = "shield",
+        calibration.code = CALIBRATION.CODE,
+        locations = LOCATIONS,
+        interventions = INTERVENTION.CODES,
+        n.sim = N.SIM
+    )
+    #
+    FORCE.OVERWRITE<- FALSE
+    #
+    sim.collection$run(
+        FIRST.YEAR,
+        LAST.YEAR,
+        verbose = T,
+        stop.for.errors = FALSE,
+        overwrite.prior = FORCE.OVERWRITE,
+        keep.from.year = FIRST.YEAR
+    )
+}
