f67ef7adeb6e390b8248f0db0b898b27047cc01a
Zoe Dansky <zoedansky@gmail.com>
2026-07-27 15:47:07 -0400

    adding ps diag rate among msm to syphilis manager

--- data_processing/syphilis.manager/ps.diag.rate.among.msm.R
+++ data_processing/syphilis.manager/ps.diag.rate.among.msm.R
@@ -6,7 +6,9 @@
 #This will need to happen within the 'syphilis.manager.merge' code since MSM and syphilis data come from different sections of the manager
 
 #I'm going to calculate the count by county and then aggregate that to state and MSA#
+
 #==================================================================
+
 #Apply Emory's Proportion MSM Estimate to MSA/State Data
 
 #==================================================================
@@ -32,10 +34,9 @@ all.male.data <- all.male.data%>%
     mutate(estimated.count.msm = round(male.population.count * proprtion.msm))%>%
     mutate(year = as.character(year))%>%
     mutate(location = as.character(location))%>%
-    mutate(sex = as.character(sex))%>%
     rename(value = estimated.count.msm)%>%
     mutate(outcome = "estimated.count.msm")%>%
-    select(year, location, outcome, sex, value)
+    select(year, location, outcome, -sex, value)
 
 #Register the outcome and Source:
 syphilis.manager$register.outcome(
@@ -47,19 +48,18 @@ syphilis.manager$register.outcome(
         units = 'count',
         description = "Estimated Count of MSM"))
 
-syphilis.manager$register.source('emory.aggregated', parent.source= "ACS", full.name = "Aggregated Data from Emory Proportion Estimates", short.name='emory') #child
-
-
 #Put count of MSM:
     syphilis.manager$put.long.form(
         data = all.male.data,
         ontology.name = 'emory',
         source = 'emory',
-        dimension.values = list(sex = "male"),
+        #dimension.values = list(sex = "male"),
         url = 'https://prismhealth.emory.edu/estimating-the-population-sizes-of-men-who-have-sex-with-men-in-us-states-and-counties-using-data-from-the-american-community-survey/',
         details = 'Estimated Count of MSM Using Emorys Proportion of MSM data from 2013')
 
 #Then Aggregate:
+    syphilis.manager$register.source('emory.aggregated', parent.source= "ACS", full.name = "Aggregated Data from Emory Proportion Estimates", short.name='emory') #child
+    
     #MSA
 put.msa.data.as.new.source.NEW(outcome = 'estimated.count.msm',
                                from.source.name = 'emory',
@@ -87,3 +87,49 @@ put.msa.data.as.new.source.NEW(outcome = 'estimated.count.msm',
                                outcome.for.relative.contribution = 'population',
                                source.for.relative.contribution = 'census.population',
                                ontology.for.relative.contribution = 'census')
+
+#==================================================================
+
+#Create Outcome ps.diag.rate.among.msm
+
+#State Level
+#==================================================================
+syphilis.manager$register.outcome(
+    'ps.diag.rate.among.msm',
+    metadata = create.outcome.metadata(
+        scale = 'rate',
+        display.name = 'Diagnosis Rate of PS Syphilis Among MSM',
+        axis.name = 'Diagnosis Rate of PS Syphilis Among MSM',
+        units = 'cases',
+        description = "Diagnosis Rate of PS Syphilis Among MSM"), denominator.outcome = 'estimated.count.msm')
+
+#Calculated as : (MSM PS Diagnoses / MSM Population Size)
+
+#msm ps dx: (Note: there isnt ps cases by MSM data from Atlas Plus so we are pulling from old CDC Reports)
+msm.ps.dx <- as.data.frame.table(syphilis.manager$data$ps.syphilis.diagnoses$estimate$cdc.sti.surveillance.reports$cdc.pdf.report$year__location__sex)%>% filter(sex =='msm')%>%filter(location %in% STATE)%>%filter(Freq != "NaN") %>%rename(msm.ps.dx = Freq)
+
+#msm pop size:
+msm.population <- as.data.frame.table(syphilis.manager$data$estimated.count.msm$estimate$emory.aggregated$emory$year__location)%>% filter(location %in% STATE)%>%rename(msm.population.count = Freq)
+
+#join
+all.msm.data.for.rate.calc <- inner_join(msm.ps.dx, msm.population, by = c("year", "location"))
+
+#calculate rate:
+all.msm.data.for.rate.calc <- all.msm.data.for.rate.calc%>%
+    group_by(year, location)%>%
+    mutate(value = msm.ps.dx / msm.population.count)%>%
+    mutate(year = as.character(year))%>%
+    mutate(location = as.character(location))%>%
+    mutate(outcome = "ps.diag.rate.among.msm")%>%
+    select(-sex)%>%
+    mutate(value = as.numeric(value))
+    
+#Put Rate (data is calculated from data from Emory and Data from CDC PDF Reports)
+all.msm.data.for.rate.calc.put = as.data.frame(all.msm.data.for.rate.calc)
+syphilis.manager$put.long.form(
+    data = all.msm.data.for.rate.calc.put,
+    ontology.name = 'cdc.pdf.report',
+    source = 'cdc.sti.surveillance.reports',
+    url = 'https://www.cdc.gov/sti-statistics/media/pdfs/2024/07/1997-Surveillance-Report.pdf',
+    details = 'CDC STI Surveillance Reports')
+
